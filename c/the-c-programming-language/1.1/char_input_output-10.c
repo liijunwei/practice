2e3aa93f5d7c3e108a438c0a08872f797dd4615e
lijunwei <ljw532344863@sina.com>
2021-11-11 00:06:54 +0800

    Commit manually.

--- c/the-c-programming-language/1.1/char_input_output-10.c
+++ c/the-c-programming-language/1.1/char_input_output-10.c
@@ -4,8 +4,6 @@
 
 // 将输入复制到输出, 并将其中连续的多个空格用一个空格代替
 
-// https://blog.csdn.net/go_to_break/article/details/76944018?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link
-
 // 变量.1 char          是/不是 ' '
 // 变量.2 space_counter 是/不是 0
 // 两两组合, 四种情况
@@ -17,17 +15,22 @@ int main(){
   int space_counter = 0;
 
   while((c = getchar()) != EOF){
-    if(c != CONST_CHAR_SPACE){
-      space_counter = 0;
-      putchar(c);
-    }
-
     if(c == CONST_CHAR_SPACE && space_counter == 0){
       putchar(c);
       space_counter += 1;
-    } else {
+    }
+
+    if(c == CONST_CHAR_SPACE && space_counter != 0){
       // do nothing
     }
 
+    if(c != CONST_CHAR_SPACE && space_counter == 0){
+      putchar(c);
+    }
+
+    if(c != CONST_CHAR_SPACE && space_counter != 0){
+      space_counter = 0;
+      putchar(c);
+    }
   }
 }
