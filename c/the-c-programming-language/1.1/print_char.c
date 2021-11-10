75277c042644df1b95957307a2be48fe0f849a01
lijunwei <ljw532344863@sina.com>
2021-11-10 23:37:16 +0800

    Commit manually.

--- c/the-c-programming-language/1.1/print_char.c
+++ c/the-c-programming-language/1.1/print_char.c
@@ -9,7 +9,18 @@ int main(){
   printf("char('e') -> number: %d\n", 'e');
   printf("char('f') -> number: %d\n", 'f');
   printf("char('g') -> number: %d\n", 'g');
+  printf("\n");
+
+  printf("char('A') -> number: %d\n", 'A');
+  printf("char('B') -> number: %d\n", 'B');
+  printf("char('C') -> number: %d\n", 'C');
+  printf("char('D') -> number: %d\n", 'D');
+  printf("char('E') -> number: %d\n", 'E');
+  printf("char('F') -> number: %d\n", 'F');
+  printf("char('G') -> number: %d\n", 'G');
+  printf("\n");
 
   printf("char('/') -> number: %d\n", '/');
   printf("char('*') -> number: %d\n", '*');
+  printf("char('\\n') -> number: %d\n", '\n');
 }
