f5ffe894e2f26f81bb49c10a13e6d882992153b5
lijunwei <ljw532344863@sina.com>
2021-11-10 23:21:48 +0800

    Commit manually.

---
+++ c/the-c-programming-language/1.1/print_char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// 对字符进行计数
+int main(){
+  printf("char('a') -> number: %d\n", 'a');
+  printf("char('b') -> number: %d\n", 'b');
+  printf("char('c') -> number: %d\n", 'c');
+  printf("char('d') -> number: %d\n", 'd');
+  printf("char('e') -> number: %d\n", 'e');
+  printf("char('f') -> number: %d\n", 'f');
+  printf("char('g') -> number: %d\n", 'g');
+
+  printf("char('/') -> number: %d\n", '/');
+  printf("char('*') -> number: %d\n", '*');
+}
