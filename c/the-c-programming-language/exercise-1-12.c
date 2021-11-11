574e19326c0f7fc8cf79a64f9c63c0d71e6c35b4
lijunwei <ljw532344863@sina.com>
2021-11-11 23:34:13 +0800

    Commit manually.

---
+++ c/the-c-programming-language/exercise-1-12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define CONST_IN  1 // 在单词内
+#define CONST_OUT 0 // 在单词外
+
+#define CONST_CHAR_TAB       '\t'
+#define CONST_CHAR_NEWLINE   '\n'
+#define CONST_CHAR_SPACE     ' '
+
+
+int main(){
+  int c;
+  int state = CONST_OUT;
+
+  printf("please enter chars, `ctrl+d` to exit\n");
+
+  while((c = getchar()) != EOF){
+
+
+  }
+
+}
