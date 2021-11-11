d402145764682eedfd84978110abc20330f22e76
lijunwei <ljw532344863@sina.com>
2021-11-11 23:43:02 +0800

    Commit manually.

--- c/the-c-programming-language/exercise-1-12.c
+++ c/the-c-programming-language/exercise-1-12.c
@@ -17,8 +17,21 @@ int main(){
   printf("please enter chars, `ctrl+d` to exit\n");
 
   while((c = getchar()) != EOF){
+    if(c == CONST_CHAR_NEWLINE){
+      putchar(c);
+    }
 
-
+    if(c == CONST_CHAR_SPACE || c == CONST_CHAR_TAB){
+      state = CONST_OUT;
+      printf("\n");
+    }
+    else if(state == CONST_OUT){
+      state = CONST_IN;
+      putchar(c);
+    }
+    else{
+      putchar(c);
+    }
   }
 
 }
