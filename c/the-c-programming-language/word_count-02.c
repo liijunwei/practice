279d35dd5a99c8af6314e33505b2884fb669bb30
lijunwei <ljw532344863@sina.com>
2021-11-11 23:25:39 +0800

    Commit manually.

--- c/the-c-programming-language/word_count-02.c
+++ c/the-c-programming-language/word_count-02.c
@@ -10,6 +10,14 @@
 // 统计输入的 行数/单词数/字符数
 
 // TODO.1 如何验证程序的正确性?
+/*
+PASS 测试用例.1 无输入, 应该输出 num_line: 0 num_word: 0 num_char: 0
+测试用例.2 输入1个字符, 应该输出 num_line: 1 num_word: 1 num_char: 2
+测试用例.3 输入2个字符组成一个单词, 应该输出 num_line: 1 num_word: 1 num_char: 3
+测试用例.4 输入2个字符组成两个单词, 在同一行, 应该输出 num_line: 1 num_word: 2 num_char: 4
+测试用例.5 输入2个字符组成两个单词, 在不同行, 应该输出 num_line: 2 num_word: 2 num_char: 4
+*/
+
 // TODO.2 如果程序存在错误, 那么什么类型的输入最可能发现这类错误呢?
 // TODO.3 编写程序, 以每行一个单词的形式, 打印其输入
 
