c6447133ca8d4389b9e88ce678fd5e662b52a876
lijunwei <ljw532344863@sina.com>
2021-11-12 13:40:57 +0800

    Commit manually.

---
+++ go/const-enum-iota.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const (
+	monday = 0
+	tuesday = 1
+	wednesday = 2
+)
+
+// gofmt -w main.go
+func main() {
+	fmt.Println("monday    ", monday)
+	fmt.Println("tuesday   ", tuesday)
+	fmt.Println("wednesday ", wednesday)
+}
