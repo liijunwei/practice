7be82e81c67ebaad772f3237322099b5bd09bff7
lijunwei <ljw532344863@sina.com>
2021-11-12 08:16:32 +0800

    Commit manually.

---
+++ c/the-c-programming-language/functions/power-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int power(int m, int n);
+
+int main(){
+  int i;
+
+  for(i = 0; i < 10; ++i){
+    printf("%d %d %d\n", i, power(2, i), power(-3, i));
+  }
+
+  return 0;
+}
+
+int powe())))))))))))))))))))))))))))))
