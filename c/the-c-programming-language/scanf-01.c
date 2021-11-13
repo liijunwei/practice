5d03c84b35064032b1b6de0472f45ad05e559d39
lijunwei <ljw532344863@sina.com>
2021-11-13 14:38:43 +0800

    Commit manually.

--- c/the-c-programming-language/scanf-01.c
+++ c/the-c-programming-language/scanf-01.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-#define CONST_CHAR_SPACE ' '
-
-// 将输入复制到输出, 并将其中连续的多个空格用一个空格代替
-
-// 变量.1 char          是/不是 ' '
-// 变量.2 space_counter 是/不是 0
-// 两两组合, 四种情况
-
 int main(){
 
   int a,b,c;
