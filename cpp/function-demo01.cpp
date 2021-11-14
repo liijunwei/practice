4ea0631a169371c50f7549441a3f8deb86e18f8c
lijunwei <ljw532344863@sina.com>
2021-11-14 13:13:53 +0800

    Commit manually.

--- cpp/function-demo01.cpp
+++ cpp/function-demo01.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int days;
 int get_dayofweek();
 int get_year();
-int get_month(int m);
+int get_month(int leap_year);
 int get_leap_year(int y);
 
 int main(){
@@ -39,6 +39,60 @@ int main(){
   }
 
   return 0;
+}
+
+int get_dayofweek(){
+  int dayofweek;
+  dayofweek = days % 7;
+
+  return dayofweek
+}
+
+int get_year(){
+  int i = 2000;
+  int leap_year;
+  while(true){
+    leap_year = get_leap_year(i);
+    if(leap_year == 1 && days >= 366){
+      days = days - 366;
+      i++;
+      continue;
+    }
+    else if(leap_year == 0 && days > 365){
+      days = days - 365;
+      i++;
+      continue;
+    }
+    else{
+      break;
+    }
+
+  }
+
+  return i;
+}
+
+int int get_month(int leap_year){
+  int pmonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int rmonth[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  int j = 0;
+
+  while(true){
+    if(leap_year == 1 && days >= rmonth[j]){
+      days = days - rmonth[j];
+      j++;
+    }
+    else if(leap_year -- 0 && days >= pmonth[j]){
+      days = days - pmonth[j];
+      j++;
+    }
+    else{
+      break;
+    }
+  }
+
+  return ++j;
 }
 
 int get_leap_year(int y){
