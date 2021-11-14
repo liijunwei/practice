767a424a8294282cee453539eaaae82dd3837671
lijunwei <ljw532344863@sina.com>
2021-11-14 13:45:16 +0800

    Commit manually.

--- cpp/tmp.cpp
+++ cpp/tmp.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int get_leap_year(int y){
-  return (y % 4 == 0 && y % 100 != 0 || y % 400 == 0);
+  return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));
 }
 
 int main(){
+  cout << "get_leap_year(1700) " << get_leap_year(1700) << endl;
+  cout << "get_leap_year(1800) " << get_leap_year(1800) << endl;
+  cout << "get_leap_year(1900) " << get_leap_year(1900) << endl;
+  cout << "get_leap_year(2100) " << get_leap_year(2100) << endl;
 
+  cout << "get_leap_year(1600) " << get_leap_year(1600) << endl;
   cout << "get_leap_year(2000) " << get_leap_year(2000) << endl;
-  cout << "get_leap_year(2001) " << get_leap_year(2001) << endl;
-  cout << "get_leap_year(2002) " << get_leap_year(2002) << endl;
+  cout << "get_leap_year(2400) " << get_leap_year(2400) << endl;
   cout << endl;
   return 0;
 }
