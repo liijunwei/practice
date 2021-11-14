12fcb50d525f4b01e73d56d391266e2b096fe99d
lijunwei <ljw532344863@sina.com>
2021-11-14 12:53:01 +0800

    Commit manually.

--- cpp/function-array-01.cpp
+++ cpp/function-array-01.cpp
@@ -5,7 +5,12 @@ using namespace std;
 void exchange(int a[]);
 
 int main(){
+  /* 注意
+    数组的名字是一个常量
+    数组的名字不是一个变量
+  */
   int a[2] = {3, 5};
+
   cout << a[0] << " " << a[1] << endl;
 
   exchange(a);
