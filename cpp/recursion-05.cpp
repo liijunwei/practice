87eabefd57288dd4da722e06b18788119d21c463
lijunwei <ljw532344863@sina.com>
2021-11-14 18:02:51 +0800

    Commit manually.

---
+++ cpp/recursion-05.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// 二进制转换
+
+void convert_to_binary(int n);
+
+int main(){
+  convert_to_binary(40);
+
+  cout << endl;
+  return 0;
+}
+
+void convert_to_binary(int n){
+  if((n / 2) != 0){
+    convert_to_binary(n / 2);
+    cout << (n % 2);
+  }
+  else{
+    cout << n;
+  }
+}
