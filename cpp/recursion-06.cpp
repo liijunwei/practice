ebf40fa597ca3c9da2c2dd743019b285a9192cd9
lijunwei <ljw532344863@sina.com>
2021-11-14 18:30:07 +0800

    Commit manually.

--- cpp/recursion-06.cpp
+++ cpp/recursion-06.cpp
@@ -14,15 +14,30 @@ move(n, a, b, c)     // move n   disks from a to c via b    (target)
 */
 
 // 将 N 个盘子, 从 A 经过 B 移动到 C
-void move(int n, char x, char y, char z);
+void move(int n, char from, char to, char via);
 
 int main(){
-
+  cout << "1 个盘子: " << endl;
+  move(1, 'A', 'C', 'B');
+  cout << endl;
+  cout << "2 个盘子: " << endl;
+  move(2, 'A', 'C', 'B');
+  cout << endl;
+  cout << "3 个盘子: " << endl;
+  move(3, 'A', 'C', 'B');
+  cout << endl;
   return 0;
 }
 
-void move(int n, char x, char y, char z){
-
+void move(int n, char from, char to, char via){
+  if(n == 1){
+    cout << "把一个盘子从 " << from << " 移动到 " << to << endl;
+  }
+  else{
+    move(n - 1, from, via, to);
+    cout << "把一个盘子从 " << from << " 移动到 " << to << endl;
+    move(n - 1, via, from, to);
+  }
 
 }
 
