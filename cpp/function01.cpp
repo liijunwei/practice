e99b3540ed5fbf0371163a135728e332cbd6fcdc
lijunwei <ljw532344863@sina.com>
2021-11-14 09:14:19 +0800

    Commit manually.

--- cpp/function01.cpp
+++ cpp/function01.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // g++ ./function01.cpp && ./a.out
+// cat "$HOME/Library/Application Support/Sublime Text 3/Packages/User/CPP-Custom.sublime-build"
 int absolute(int n){
   if(n < 0){
     return (-n);
