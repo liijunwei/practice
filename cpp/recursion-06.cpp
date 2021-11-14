3c00fef84663e805b5f17e8d22a74150090c4e00
lijunwei <ljw532344863@sina.com>
2021-11-14 18:17:29 +0800

    Commit manually.

---
+++ cpp/recursion-06.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+汉诺塔
+move(n, a, b, c)     // move n   disks from a to c via b    (target)
+  move(n-1, a, c, b) // move n-1 disks from a to b via c    (step 1)
+                     // move 1   disk  from a to c directly (step 2)
+  move(n-1, b, a, c) // move n-1 disks from b to c via a    (step 3)
+
+  repeat.
+
+*/
+
+void convert_to_binary(int n);
+
+int main(){
+
+  return 0;
+}
+
+void convert_to_binary(int n){
+
+
+}
+
+
