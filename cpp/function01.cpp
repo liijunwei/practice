a13a14f77e1a758d36ddc1fd8c5eb79a8cf5b44f
lijunwei <ljw532344863@sina.com>
2021-11-14 08:58:08 +0800

    Add first C++ demo

--- cpp/function01.cpp
+++ cpp/function01.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+// g++ ./function01.cpp && ./a.out
+int absolute(int n){
+  if(n < 0){
+    return (-n);
+  }
+  else{
+    return n;
+  }
+}
+
 int main(){
-  cout << "demo01" << endl;
+  int m = -123;
+  int result = 0;
+
+  result = absolute(m);
+  cout << "The absolute value of " << m << " is " << result << endl;
+
+  return 0;
 }
