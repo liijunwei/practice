fb23aa1cec2829cff6a2173458f193d4b13ac410
lijunwei <ljw532344863@sina.com>
2021-11-14 11:45:41 +0800

    Commit manually.

---
+++ cpp/function04.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void exchange(int *m, int *n);
+
+int main(){
+  int m = 10;
+  int n = 100;
+
+  cout << "Before exchange value: " << m << " " << n << endl;
+  exchange(&m, &n);
+  cout << "After exchange value: " << m << " " << n << endl;
+
+  return 0;
+}
+
+void exchange(int *m, int *n){
+  int p = *m;
+  *m = *n;
+  *n = p;
+}
