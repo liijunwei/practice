9357756d3cd9ada27e9fe57bf43bc80c0fcc4125
lijunwei <ljw532344863@sina.com>
2021-11-14 17:34:01 +0800

    Commit manually.

--- cpp/recursion-01.cpp
+++ cpp/recursion-01.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 // https://www.bilibili.com/video/BV1bs41197KN?p=76
 // "函数可以嵌套调用, 但不能嵌套地定义"
+int fact(int n);
 
 int main(){
+  cout << "fact(4) " << fact(4) << endl;
 
+  cout <<  endl;
+  return 0;
+}
+
+int fact(int n){
+  if(n == 1){
+    return 1;
+  }
+  else {
+    return fact(n - 1) * n;
+  };
 }
