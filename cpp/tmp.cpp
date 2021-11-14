9cb4b39bfcdb308593f9555cfedc030bf0c5047e
lijunwei <ljw532344863@sina.com>
2021-11-14 13:47:41 +0800

    Commit manually.

--- cpp/tmp.cpp
+++ cpp/tmp.cpp
@@ -6,6 +6,10 @@ int get_leap_year(int y){
   return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));
 }
 
+int get_dayofweek(int days){
+  return days % 7;
+}
+
 int main(){
   cout << "get_leap_year(1700) " << get_leap_year(1700) << endl;
   cout << "get_leap_year(1800) " << get_leap_year(1800) << endl;
@@ -15,6 +19,12 @@ int main(){
   cout << "get_leap_year(1600) " << get_leap_year(1600) << endl;
   cout << "get_leap_year(2000) " << get_leap_year(2000) << endl;
   cout << "get_leap_year(2400) " << get_leap_year(2400) << endl;
+
+
+  cout << "get_dayofweek(1) " << get_dayofweek(1) << endl;
+  cout << "get_dayofweek(2) " << get_dayofweek(2) << endl;
+
+
   cout << endl;
   return 0;
 }
