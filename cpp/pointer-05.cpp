09b16e5c060d0bc9af82ebe9b0377b62225b9df2
lijunwei <ljw532344863@sina.com>
2021-11-15 22:56:58 +0800

    Commit manually.

--- cpp/pointer-05.cpp
+++ cpp/pointer-05.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // 用指针变量访问数组元素
 int main(){
 
-  int a[5] = {10, 11, 12, 13, 14};
+  int a[4] = {10, 11, 12, 13};
 
   for(int i = 0; i < sizeof(a) / sizeof(a[0]); i++){
     cout << a[i] << " ";
@@ -18,15 +18,21 @@ int main(){
 
   cout << "pointer:        " << pointer << endl;
   cout << "&a[0]:          " << &a[0] << endl;
+  cout << endl;
 
+  cout << "(pointer + 0)   " << (pointer + 0) << endl;
   cout << "(pointer + 1)   " << (pointer + 1) << endl;
   cout << "(pointer + 2)   " << (pointer + 2) << endl;
   cout << "(pointer + 3)   " << (pointer + 3) << endl;
+  cout << endl;
 
+  cout << "*(pointer + 0)  " << *(pointer + 0) << endl;
   cout << "*(pointer + 1)  " << *(pointer + 1) << endl;
   cout << "*(pointer + 2)  " << *(pointer + 2) << endl;
   cout << "*(pointer + 3)  " << *(pointer + 3) << endl;
+  cout << endl;
 
+  cout << "pointer[0]      " << *(pointer + 0) << endl;
   cout << "pointer[1]      " << *(pointer + 1) << endl;
   cout << "pointer[2]      " << *(pointer + 2) << endl;
   cout << "pointer[3]      " << *(pointer + 3) << endl;
