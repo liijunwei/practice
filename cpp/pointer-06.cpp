8418e4746d2d7d281e974ca55e9810b3b2be3bde
lijunwei <ljw532344863@sina.com>
2021-11-15 23:13:08 +0800

    Commit manually.

--- cpp/pointer-06.cpp
+++ cpp/pointer-06.cpp
@@ -3,12 +3,41 @@
 
 using namespace std;
 
+void iterate_with_index();
+void iterate_with_pointer();
+
 int main(){
+  iterate_with_index();
+  iterate_with_pointer();
+
+  cout << endl;
+  return 0;
+}
+
+
+void iterate_with_index(){
+  cout << "iterate_with_index: ";
+
   int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   for(int i = 9; i >= 0; i--){
     cout << setw(3) << a[i] << " ";
   }
 
   cout << endl;
-  return 0;
 }
+
+// TODO 没看懂, 没打印对
+void iterate_with_pointer(){
+  cout << "iterate_with_pointer: ";
+
+  int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int *p = a;
+
+  for(p--; p >= a;){
+    cout << setw(3) << *p++ << " ";
+  }
+
+  cout << endl;
+}
+
+
