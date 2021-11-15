bb8d354fed3772ae3c59051e25e096911688eac2
lijunwei <ljw532344863@sina.com>
2021-11-15 23:57:35 +0800

    Commit manually.

--- c/the-c-programming-language/array_of_chars/chars-01.c
+++ c/the-c-programming-language/array_of_chars/chars-01.c
@@ -3,7 +3,7 @@
 #define MAXLINE 15
 
 // 将一行读入s中, 并返回其长度
-int getline(char line[], int maxline);
+int custom_getline(char line[], int maxline);
 
 // 将 from 复制到 to, 假定 to 足够大
 void copy(char to[], char from[]);
@@ -15,7 +15,7 @@ int main(){
   char line[MAXLINE];
   char longest[MAXLINE];
 
-  while((len = getline(line, MAXLINE)) > 0){
+  while((len = custom_getline(line, MAXLINE)) > 0){
     if(len > max){
       max = len;
       copy(longest, line);
@@ -29,20 +29,20 @@ int main(){
   return 0;
 }
 
-int getline(char line[], int maxline){
+int custom_getline(char line[], int maxline){
   int c;
   int i;
 
   for(i = 0; (i < maxline && (c = getchar() != EOF) && c != '\n'); ++i){
-    s[i] = c;
+    line[i] = c;
   }
 
   if(c == '\n'){
-    s[i] = c;
+    line[i] = c;
     ++i;
   }
 
-  s[i] = '\n';
+  line[i] = '\n';
 
   return i;
 }
