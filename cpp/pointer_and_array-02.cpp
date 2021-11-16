ef0dd88c7ea09c6c4b8e6ba462f5ba9206b9abbe
lijunwei <ljw532344863@sina.com>
2021-11-16 21:52:26 +0800

    Commit manually.

--- cpp/pointer_and_array-02.cpp
+++ cpp/pointer_and_array-02.cpp
@@ -5,10 +5,10 @@ using namespace std;
 // https://www.bilibili.com/video/BV1bs41197KN?p=86&spm_id_from=pageDriver
 int main(){
   int a[5] = {10, 11, 12, 13, 14};
-  cout << "a " << a << endl;
-  cout << "*a " << *a << endl;
+  cout << "a     " << a << endl;
+  cout << "*a    " << *a << endl;
   cout << "&a[0] " << &a[0] << endl;
-  cout << "a[0] " << a[0] << endl;
+  cout << "a[0]  " << a[0] << endl;
 
   cout << endl;
   return 0;
