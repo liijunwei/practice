c0b61424c548442df8ce9492c6c650032f6b6e97
lijunwei <ljw532344863@sina.com>
2021-11-16 21:52:01 +0800

    Commit manually.

---
+++ cpp/pointer_and_array-02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// https://www.bilibili.com/video/BV1bs41197KN?p=86&spm_id_from=pageDriver
+int main(){
+  int a[5] = {10, 11, 12, 13, 14};
+  cout << "a " << a << endl;
+  cout << "*a " << *a << endl;
+  cout << "&a[0] " << &a[0] << endl;
+  cout << "a[0] " << a[0] << endl;
+
+  cout << endl;
+  return 0;
+}
