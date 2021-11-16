58052ad96eec83a7f0744622a6e51dea4ebf21a6
lijunwei <ljw532344863@sina.com>
2021-11-16 22:51:28 +0800

    Commit manually.

--- cpp/pointer_and_array-05.cpp
+++ cpp/pointer_and_array-05.cpp
@@ -4,12 +4,6 @@ using namespace std;
 
 int main(){
   int a[5] = {10, 11, 12, 13, 14};
-
-  for(int i = 0; i < 5; i++){
-    cout << i << " " << a[i] << endl;
-  }
-  cout << endl;
-
   int *p = &a[3];
 
   cout << *p++ << endl; // 13
