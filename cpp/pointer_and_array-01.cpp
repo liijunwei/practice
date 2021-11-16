68f48698c1b310f046e54754be7d45021f1cd679
lijunwei <ljw532344863@sina.com>
2021-11-16 21:48:34 +0800

    Commit manually.

--- cpp/pointer_and_array-01.cpp
+++ cpp/pointer_and_array-01.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 // https://www.bilibili.com/video/BV1bs41197KN?p=86&spm_id_from=pageDriver
 int main(){
+  int a[5] = {1, 2, 3, 4, 5};
+
+  int *p = &a[3];
+
+  cout << "a " << a << endl;
+  cout << "p " << p << endl;
+  cout << "*p " << *p << endl;
+  cout << "*p++ " << *p++ << endl;
+  cout << "*p " << *p << endl;
+
+  *p = 100;
+  cout << a[3] << endl;
 
   cout << endl;
   return 0;
