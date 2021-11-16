10c6f80aaa4e92355d4fdace932d8a170aabac8a
lijunwei <ljw532344863@sina.com>
2021-11-16 23:57:57 +0800

    Fun Q_rsqrt impl

---
+++ c/Q_rsqrt_demo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// https://www.youtube.com/watch?v=p8u_k2LIZyo
+// https://en.wikipedia.org/wiki/Fast_inverse_square_root
+
+float Q_rsqrt( float number )
+{
+  long i;
+  float x2, y;
+  const float threehalfs = 1.5F;
+
+  x2 = number * 0.5F;
+  y  = number;
+  i  = * ( long * ) &y;                       // evil floating point bit level hacking
+  i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+  y  = * ( float * ) &i;
+  y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+
+  return y;
+}
+
+int main(){
+  float result = 1.0 / Q_rsqrt(2);
+  printf("%f", result);
+}
