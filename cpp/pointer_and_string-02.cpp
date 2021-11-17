2fd883a5c562aa5114c02a4f5ee292ab9c51a000
lijunwei <ljw532344863@sina.com>
2021-11-17 23:27:39 +0800

    Commit manually.

---
+++ cpp/pointer_and_string-02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a = 5;
+  int *pa = &a;
+
+  int b[6] = {1, 2, 3, 4, 5, 6};
+  int *pb = b;
+
+  char c[6] = {'h', 'e', 'l', 'l', 'o', '\0'};
+  char *pc = c;
+
+  cout << a << endl;
+  cout << pa << endl;
+  cout << endl;
+
+  cout << b << endl;
+  cout << pb << endl;
+  cout << endl;
+
+  cout << c << endl;
+  cout << pc << endl;
+  cout << endl;
+
+  cout << endl;
+  return 0;
+}
