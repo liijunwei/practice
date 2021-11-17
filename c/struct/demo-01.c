585b2c3da5c9e1a3f229e0b3eba3a6406f44fd4b
lijunwei <ljw532344863@sina.com>
2021-11-17 13:58:05 +0800

    Commit manually.

---
+++ c/struct/demo-01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// https://www.runoob.com/cprogramming/c-structures.html
+struct Books
+{
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+} book = {"C 语言", "RUNOOB", "编程语言", 123456};
+
+int main(){
+    printf("title:   %s\n", book.title);
+    printf("author:  %s\n", book.author);
+    printf("subject: %s\n", book.subject);
+    printf("book_id: %d\n", book.book_id);
+}
