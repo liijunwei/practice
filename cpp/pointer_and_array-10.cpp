db786741b7dcdbaeb14a58dbcca648961de12a49
lijunwei <ljw532344863@sina.com>
2021-11-18 22:36:55 +0800

    Commit manually.

--- cpp/pointer_and_array-10.cpp
+++ cpp/pointer_and_array-10.cpp
@@ -43,6 +43,11 @@ int main(){
   cout << __LINE__ << "  &a[0][0] + 1 = " << &a[0][0] + 1 << endl;
   cout << endl;
 
+  cout << __LINE__ << "  a[1] = " << a[1] << endl;
+  cout << __LINE__ << "  &a[1] = " << &a[1] << endl;
+  cout << __LINE__ << "  *(a + 1) = " << *(a + 1) << endl;
+  cout << endl;
+
   cout << endl;
   return 0;
 }
