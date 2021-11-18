7ce4a4bf2ec90585826cd3d2f0b32c0f54be18e2
lijunwei <ljw532344863@sina.com>
2021-11-18 08:52:40 +0800

    Commit manually.

---
+++ c/the-c-programming-language/enum-demo-01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+enum escapes {
+  BELL      = '\a',
+  BACKSPACE = '\b',
+  TAB       = '\t',
+  NEWLINE   = '\n',
+  VTAB      = '\v',
+  RETURN    = '\r',
+};
+
+enum months {
+  JAN = 1,
+  FEB,
+  MAR,
+  APR,
+  MAY,
+  JUN,
+  JUL,
+  AUG,
+  SEP,
+  OCT,
+  NOV,
+  DEC,
+};
+
+// TODO
+int main(){
+
+}
