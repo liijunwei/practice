d4129c3c9c1e5637cbdbbd8b0b2b9b898f375520
lijunwei <ljw532344863@sina.com>
2021-11-19 08:43:37 +0800

    Commit manually.

---
+++ cpp/pointer_and_function-02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+// https://www.bilibili.com/video/BV1bs41197KN?p=93
+
+// 数组名作为函数参数
+
+void sum(int *p, int n){
+  int total = 0;
+  for(int i = 0; i < n; i++){
+    total += *p;
+    p++;
+  }
+
+  cout << "total " << total << endl;
+}
+
+int main(){
+  int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  sum(a, 10);
+
+  cout << endl;
+  return 0;
+}
