a6cf94158ac5634f8eb49e493fc75ecfdd846934
lijunwei <ljw532344863@sina.com>
2021-11-19 08:48:48 +0800

    Commit manually.

--- cpp/pointer_and_function-02.cpp
+++ cpp/pointer_and_function-02.cpp
@@ -8,19 +8,40 @@ using namespace std;
 // 数组名相当于指向数组第一个元素的指针
 // 数组名相当于指向数组第一个元素的指针
 
-void sum(int *p, int n){
+void sum_v1(int *p, int n){
+  int total = 0;
+  for(int i = 0; i < n; i++){
+    total += *p++;
+  }
+
+  cout << __LINE__ << " Total: " << total << endl;
+}
+
+void sum_v2(int *p, int n){
+  int total = 0;
+  for(int i = 0; i < n; i++){
+    total += *p;
+    p++;
+  }
+
+  cout << __LINE__ << " Total: " << total << endl;
+}
+
+void sum_v3(int *p, int n){
   int total = 0;
   for(int i = 0; i < n; i++, p++){
     total += *p;
   }
 
-  cout << "total " << total << endl;
+  cout << __LINE__ << " Total: " << total << endl;
 }
 
 int main(){
   int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-  sum(a, 10);
+  sum_v1(a, 10);
+  sum_v2(a, 10);
+  sum_v3(a, 10);
 
   cout << endl;
   return 0;
