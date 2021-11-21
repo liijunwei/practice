7ff9913bb41aa60e4a84948c9f2d96644e516a2e
lijunwei <ljw532344863@sina.com>
2021-11-21 15:52:55 +0800

    Commit manually.

--- cpp/pointer_and_array-03.cpp
+++ cpp/pointer_and_array-03.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-/*
-
-*/
-
 // https://www.bilibili.com/video/BV1bs41197KN?p=87
 int main(){
   int a[5] = {10, 11, 12, 13, 14};
   int *p = NULL;
+  int *q = NULL;
 
   // 数组名相当于指向数组第一个元素的指针, 即 a 指向 a[0], 变量a里存的是 a[0] 的地址
   cout << "a    " << a << endl;
 
   p = a;
+  q = a;
 
   cout << "p    " << p    << endl;
   cout << "*p   " << *p   << endl;
   cout << "     " << *p++ << endl;
   cout << "     " << *p++ << endl;
+  cout << "     " << *++q << endl;
+  cout << "     " << *++q << endl;
 
   cout << endl;
   return 0;
