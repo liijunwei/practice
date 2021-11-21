52442fa5db85cf62cc028d37ac5e1419f117b241
lijunwei <ljw532344863@sina.com>
2021-11-21 17:01:44 +0800

    Commit manually.

--- cpp/pointer_and_array-07.cpp
+++ cpp/pointer_and_array-07.cpp
@@ -17,12 +17,12 @@ int main(){
   int a[5] = {10, 11, 12, 13, 14};
   // int a[3] = {10, 11, 12};
 
-  cout << __LINE__ << " " << a << endl;
-  cout << __LINE__ << " " << (a + 1) << endl;
+  cout << __LINE__ << " " << a << endl;        // 指向a[0]
+  cout << __LINE__ << " " << (a + 1) << endl;  // 指向a[1]
   cout << __LINE__ << " " << &a << endl;       // 指向      整个数组空间
   cout << __LINE__ << " " << (&a + 1) << endl; // 指向 下个 整个数组空间
-  cout << __LINE__ << " " << *(&a) << endl;
-  cout << __LINE__ << " " << (*(&a) + 1) << endl;
+  cout << __LINE__ << " " << *(&a) << endl;    // 指向a[0]
+  cout << __LINE__ << " " << (*(&a) + 1) << endl; // 指向a[1]
 
   cout << endl;
   return 0;
