00bef14a2a085b7873615a2d2722e28de8cebfe4
lijunwei <ljw532344863@sina.com>
2021-11-22 08:37:41 +0800

    结构体数组

---
+++ cpp/struct-and-LinkedList-07.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+// https://www.bilibili.com/video/BV1bs41197KN?p=99
+
+struct SpaceTraveler{
+  int id;
+  char name[20];
+};
+
+// 结构体数组
+int main(){
+  SpaceTraveler ricks[3] = {
+    137, "Rick SancheX",
+    138, "Rick SancheY",
+    139, "Rick SancheZ",
+  };
+
+  SpaceTraveler *one = ricks;
+
+  cout << one->id << " " << one->name << endl;
+  one++;
+
+  cout << one->id << " " << one->name << endl;
+  one++;
+
+  cout << one->id << " " << one->name << endl;
+
+  cout << endl;
+  return 0;
+}
