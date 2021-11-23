fbc9c561c3ca6e175489cd8bf513feff111263bd
lijunwei <ljw532344863@sina.com>
2021-11-23 08:46:15 +0800

    Commit manually.

--- cpp/struct-and-LinkedList-09.cpp
+++ cpp/struct-and-LinkedList-09.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 // https://www.bilibili.com/video/BV1bs41197KN?p=100
+/*
+链表可以动态得创建
 
+**动态地**申请内存空间
+    int *p = new int(1024);
+    delete p;
+
+    int *p = new int[4];
+    delete []p;
+*/
 struct Node{
   head
   data
