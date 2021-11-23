f52b94e2f821c0338dea6ab34d5540d16005adcb
lijunwei <ljw532344863@sina.com>
2021-11-23 22:01:08 +0800

    Commit manually.

--- cpp/struct-and-LinkedList-11.cpp
+++ cpp/struct-and-LinkedList-11.cpp
@@ -3,11 +3,32 @@
 
 // https://www.bilibili.com/video/BV1bs41197KN?p=101
 
-// 遍历链表
-// 删除链表元素
+// 删除链表元素(head)
+// 删除链表元素(tail)
+// 删除链表元素(middle)
+
+Node *deleteHeadNode(Node *p){
+  Node *temp = NULL;
+  temp = p;
+  p = p->next;
+  delete temp;
+  return p;
+}
+
+Node *deleteTailNode(Node *p){
+
+}
+
+Node *deleteMiddleNode(Node *p){
+
+}
+
 int main(){
   Node *list01 = createLinkedList();
   cout << endl;
+  traverseLinkedList(list01);
+
+  list01 = deleteHeadNode(list01);
   traverseLinkedList(list01);
 
   cout << endl;
