858b8817753badec9a18971ccb409e77511263fc
lijunwei <ljw532344863@sina.com>
2021-11-28 17:04:25 +0800

    Commit manually.

---
+++ c/the-c-programming-language/pointer-02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int x = 1;
+  int *p = NULL;
+  p = &x;
+
+  printf("x    is %d \n", x);
+  printf("*p   is %d \n", *p);
+  int y = *p + 1;
+  printf("y    is %d \n", y);
+  printf("=============\n");
+
+  // *p = *p + 1;
+  *p += 1;
+  printf("*p   is %d \n", *p);
+  printf("x    is %d \n", x);
+
+  return 0;
+}
---
+++ c/the-c-programming-language/pointer-03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void swap_01(int a, int b){
+  int temp;
+
+  temp = a;
+  a = b;
+  b = temp;
+}
+
+void swap_02(int *a, int *b){
+  int temp;
+
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int main(){
+  int x = 2;
+  int y = 4;
+
+  printf("x    is %d \n", x);
+  printf("y    is %d \n", y);
+  printf("======swap_01=======\n");
+  swap_01(x, y);
+  printf("x    is %d \n", x);
+  printf("y    is %d \n", y);
+
+  printf("======swap_02=======\n");
+  swap_02(&x, &y);
+  printf("x    is %d \n", x);
+  printf("y    is %d \n", y);
+
+  return 0;
+}
