fb6a983702bad5ad8a9df2f305f5e8b690ae3297
lijunwei <ljw532344863@sina.com>
2021-11-28 16:37:06 +0800

    Commit manually.

---
+++ c/the-c-programming-language/pointer-01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+  int x = 1;
+  int y = 2;
+  int z[10];
+
+  int *ip = NULL;
+
+  printf("x   is %d \n", x);
+  printf("y   is %d \n", y);
+  printf("z[] is %d \n", z[0]);
+
+  ip = &x;
+  printf("ip  is %d \n", ip);
+
+  y = *ip;
+  printf("y   is %d \n", y);
+
+  *ip = 0;
+  printf("x   is %d \n", x);
+  printf("y   is %d \n", y);
+
+  ip = &z[0];
+  printf("ip  is %d \n", ip);
+
+  return 0;
+}
