9de7edb638d983f1ca3e4b0fe525fa141b412f4a
lijunwei <ljw532344863@sina.com>
2021-12-14 00:16:20 +0800

    Commit manually.

--- c/the-c-programming-language/ch1-A-Tutorial-Introduction/char_input_output-3.c
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/char_input_output-3.c
@@ -4,7 +4,11 @@
 // 验证表达式 getchar() != EOF 的值是 0 还是 1
 int main(){
   int c;
-  printf("please enter a char: ");
-  c = (getchar() != EOF);
-  printf("%d", c); // 1
+
+  // != 优先级比 = 高
+  while(c = (getchar() != EOF)){
+    printf("%d\n", c); // 1
+  }
+
+  printf("%d\n", c); // 0
 }
