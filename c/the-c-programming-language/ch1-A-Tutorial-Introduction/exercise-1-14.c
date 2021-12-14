984661fcfe93517157207e124236cb33c2c11317
lijunwei <ljw532344863@sina.com>
2021-12-15 00:21:38 +0800

    Commit manually.

--- c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-14.c
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-14.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
+#include <ctype.h>
 
-// TODO
+// page 17
 // 编写程序, 打印输入中各个字符出现频度的直方图
 
-int main(){
+// 好难懂(TODO)
 
+#define MAXHIST 15
+#define MAXCHAR 128
+
+int main(){
+  int i;
+  int c;
+  int len;
+  int maxvalue;
+  int cc[MAXCHAR];
+
+  for(i = 0; i < MAXCHAR; ++i){
+    cc[i] = 0;
+  }
+
+  while((c = getchar()) != EOF){
+    if(c < MAXCHAR){
+      ++cc[c];
+    }
+  }
+
+  maxvalue = 0;
+
+  for(i = 1; i < MAXCHAR; ++i){
+    if(cc[i] > maxvalue){
+      maxvalue = cc[i];
+    }
+  }
+
+  for(i = 1; i < MAXCHAR; ++i){
+    if(isprint(i)){
+      printf("%5d - %c - %5d : ", i, i, cc[i]);
+    }
+    else{
+      printf("%5d -    - %5d : ", i, cc[i]);
+    }
+
+    if(cc[i] > 0){
+      if((len = cc[i] * MAXHIST / maxvalue) <= 0){
+        len = 1;
+      }
+    }
+    else{
+      len = 0;
+    }
+
+    while(len > 0){
+      putchar('*');
+      --len;
+    }
+
+    putchar('\n');
+  }
 }
