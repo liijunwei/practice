db13d3ad1c5334e7000162fb18c8a99b4c436ed3
lijunwei <ljw532344863@sina.com>
2021-12-18 00:14:22 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-21.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+// page 25
+
+/*
+编写程序entab, 将空格串替换为最少数量的制表符和空格, 但要保持单词之间的间隔不变
+假设制表符终止位的位置与练习1-20的detab程序的情况相同。
+当使用一个制表符或者一个空格都可以到达下一个制表符终止位时, 选用哪种替换字符比较好
+*/
+
+#define TABING 8 // tab increment size
+#define VISIABLE_CHAR '*'
+
+// replace strings of blanks with tabs and blanks
+int main(){
+  int c;
+  int nb  = 0; // number of blanks necessary
+  int nt  = 0; // number of tabs necessary
+  int pos = 1; // position of character in line
+
+
+
+  return 0;
+}
