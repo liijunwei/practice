3cc0d21f93c7fb22cc421f2346843b95c69b9391
lijunwei <ljw532344863@sina.com>
2021-12-18 00:22:51 +0800

    Commit manually.

--- c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-21.c
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-21.c
@@ -9,17 +9,49 @@
 当使用一个制表符或者一个空格都可以到达下一个制表符终止位时, 选用哪种替换字符比较好
 */
 
+// TODO 没明白
+
 #define TABING 8 // tab increment size
-#define VISIABLE_CHAR '*'
+#define CHAR_BLANK 'b' // ' '
+#define CHAR_TAB   't' // '\t'
 
 // replace strings of blanks with tabs and blanks
 int main(){
   int c;
   int nb  = 0; // number of blanks necessary
   int nt  = 0; // number of tabs necessary
-  int pos = 1; // position of character in line
+  int pos;     // position of character in line
 
+  for(pos = 1; (c = getchar()) != EOF; ++pos){
+    if(c == ' '){
+      if(pos % TABING != 0){
+        ++nb;
+      } else {
+        nb = 0;
+        ++nt;
+      }
+    } else {
+      for(; nt > 0; --nt){
+        putchar(CHAR_TAB);
+      }
 
+      if(c == 't'){
+        nb = 0;
+      } else {
+        for(; nb > 0; --nb){
+          putchar(CHAR_BLANK);
+        }
+      }
+
+      putchar(c);
+
+      if(c == '\n'){
+        pos = 0;
+      } else if(c == '\t'){
+        pos = pos + (TABING - (pos - 1) % TABING) - 1;
+      }
+    }
+  }
 
   return 0;
 }
