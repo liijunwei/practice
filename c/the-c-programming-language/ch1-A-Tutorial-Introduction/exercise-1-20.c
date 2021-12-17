e5c9d09d237b312e190fd8b7e0a4c196f451dba7
lijunwei <ljw532344863@sina.com>
2021-12-18 00:03:58 +0800

    Commit manually.

--- c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-20.c
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-20.c
@@ -6,8 +6,30 @@
 // 编写程序detab, 将输入中的制表符替换成适当数目的空格, 使空格充满到下一个制表符终止位的地方
 // 假设制表符终止位的位置是固定的, 比如每个n列就会出现一个制表符终止位, n应该为变量还是符号常量呢?
 
-int main(){
+#define TABING 8 // tab increment size
 
+// replace tabs with the proper number of blanks
+int main(){
+  int c;
+  int nb  = 0; // number of blanks necessary
+  int pos = 1; // position of character in line
+
+  while((c = getchar()) != EOF){
+    if(c == '\t'){
+      nb = TABING - (pos - 1) % TABING;
+      while(nb > 0){
+        putchar(' ');
+        ++pos;
+        --nb;
+      }
+    } else if(c == '\n'){
+      putchar(c);
+      pos = 1;
+    } else {
+      putchar(c);
+      ++pos;
+    }
+  }
 
   return 0;
 }
