35aa32bdfb6c5825de841b4ea0241fdbffb229da
lijunwei <ljw532344863@sina.com>
2021-12-18 12:23:38 +0800

    Commit manually.

--- c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-24.c
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-24.c
@@ -7,6 +7,20 @@
 要正确处理引号(包括单引号、双引号)、转义字符序列与注释。
 */
 
+/*
+测试用的 输入
+
+--- Unbalanced braces
+int main(int argc, char const *argv[]){return 0;
+
+--- Unbalanced brackets
+int main(int argc, char const *argv[){return 0;}
+
+--- Unbalanced parentheses
+int main(int argc, char const *argv[]{return 0;}
+
+*/
+
 int brace; // {}
 int brack; // []
 int paren; // ()
@@ -17,6 +31,43 @@ void search(int c);
 
 // rudimentary(初级的) syntax checker for C programs
 int main(){
+  int c;
+  extern int brace;
+  extern int brack;
+  extern int paren;
+
+  while((c = getchar()) != EOF){
+    if(c == '/'){
+      if((c = getchar()) == '*'){
+        in_comment();
+      } else{
+        search(c);
+      }
+    } else if(c == '\'' || c == '"'){
+      in_quote(c);
+    } else{
+      search(c);
+    }
+
+    if(brace < 0){
+      printf("Unbalanced braces\n");
+      brace = 0;
+    } else if(brack < 0){
+      printf("Unbalanced brackets\n");
+      brack = 0;
+    } else if(paren < 0){
+      printf("Unbalanced parentheses\n");
+      paren = 0;
+    }
+  }
+
+  if(brace > 0){
+    printf("Unbalanced braces\n");
+  } else if(brack > 0){
+    printf("Unbalanced brackets\n");
+  } else if(paren > 0){
+    printf("Unbalanced parentheses\n");
+  }
 
   return 0;
 }
