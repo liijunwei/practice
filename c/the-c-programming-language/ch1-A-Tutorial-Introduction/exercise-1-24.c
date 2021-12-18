f2d91012df7dc2376be871ab6f7cdc30affef18d
lijunwei <ljw532344863@sina.com>
2021-12-18 12:08:58 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-24.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// page 25
+
+/*
+编写一个程序，查找C语言程序中的基本语法错误，如圆括号、方括号以及花括号不配对等。
+要正确处理引号(包括单引号、双引号)、转义字符序列与注释。
+*/
+
+// rudimentary(初级的) syntax checker for C programs
+int main(){
+
+  return 0;
+}
