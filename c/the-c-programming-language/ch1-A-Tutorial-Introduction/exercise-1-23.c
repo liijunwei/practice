ac904fbbd9d1a97d7f1df54b5dd298ee4d89581c
lijunwei <ljw532344863@sina.com>
2021-12-18 12:05:05 +0800

    Commit manually.

--- c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-23.c
+++ c/the-c-programming-language/ch1-A-Tutorial-Introduction/exercise-1-23.c
@@ -8,6 +8,8 @@
 在C语言程序中，注释不允许嵌套
 */
 
+// TODO 有问题: 去不掉以 "// " 开头的注释
+
 void rcomment(int c);
 void in_comment(void);
 void echo_quote(int c);
@@ -23,15 +25,52 @@ int main(){
   return 0;
 }
 
+// 原来去除注释, 使用递归实现的吗?
+
+// read each character, remove the comments
 void rcomment(int c){
+  int d;
 
+  if(c == '/'){
+    if((d = getchar()) == '*'){
+      in_comment(); // beginning of comment
+    } else if(d == '/'){
+      putchar(c); // another slash
+      rcomment(d); // recursively ... ?
+    } else{
+      putchar(c); // not a comment
+      putchar(d);
+    }
+  } else if(c == '\'' || c == '"'){
+    echo_quote(c); // quote begins
+  } else{
+    putchar(c); // not a comment
+  }
 }
 
+// inside of a valid comment
 void in_comment(void){
+  int c = getchar(); // prev character
+  int d = getchar(); // curr character
 
+  while(c != '*' || d != '/'){ // search for end
+    c = d;
+    d = getchar();
+  }
 }
 
+// echo characters within quotes
 void echo_quote(int c){
+  int d;
 
+  putchar(c);
+  while((d = getchar()) != c){ // search for end
+    putchar(d);
+    if(d == '\\'){
+      putchar(getchar()); // ignore escape seq
+    }
+  }
+
+  putchar(d);
 }
 
