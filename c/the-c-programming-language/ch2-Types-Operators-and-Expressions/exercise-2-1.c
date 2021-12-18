e3c746604158be6059d2dcc6988f31700bcb74d8
lijunwei <ljw532344863@sina.com>
2021-12-18 12:53:44 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-1.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-1.c
@@ -21,7 +21,23 @@ int main(int argc, char const *argv[])
 }
 
 void solu_1(){
+  printf("Signed Types\n===============\n");
+  printf("signed char min  = %d\n", SCHAR_MIN);
+  printf("signed char max  = %d\n", SCHAR_MAX);
+  printf("signed short min = %d\n", SHRT_MIN);
+  printf("signed short max = %d\n", SHRT_MAX);
+  printf("signed int min   = %d\n", INT_MIN);
+  printf("signed int max   = %d\n", INT_MAX);
+  printf("signed long min  = %ld\n", LONG_MIN);
+  printf("signed long max  = %ld\n", LONG_MAX);
 
+  printf("\n");
+
+  printf("Unsigned Types\n===============\n");
+  printf("unsigned char max  = %u\n", UCHAR_MAX);
+  printf("unsigned short max = %u\n", USHRT_MAX);
+  printf("unsigned int max   = %u\n", UINT_MAX);
+  printf("unsigned long max  = %lu\n", ULONG_MAX);
 }
 
 void solu_2(){
