11c2e65d25cd4c883dd3428fdefb704e4566a50a
lijunwei <ljw532344863@sina.com>
2021-12-18 13:32:51 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-1.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-1.c
@@ -15,6 +15,8 @@ void solu_2();
 int main(int argc, char const *argv[])
 {
   solu_1();
+  printf("\n\n");
+
   solu_2();
 
   return 0;
@@ -22,7 +24,7 @@ int main(int argc, char const *argv[])
 
 // refer to: https://code.woboq.org/userspace/glibc/include/limits.h.html
 void solu_1(){
-  printf("Signed Types\n===============\n");
+  printf("Signed Types(%s)\n\n", __func__);
   printf("signed char min  = %d\n", SCHAR_MIN);
   printf("signed char max  = %d\n", SCHAR_MAX);
   printf("signed short min = %d\n", SHRT_MIN);
@@ -34,7 +36,7 @@ void solu_1(){
 
   printf("\n");
 
-  printf("Unsigned Types\n===============\n");
+  printf("Unsigned Types(%s)\n\n", __func__);
   printf("unsigned char max  = %u\n", UCHAR_MAX);
   printf("unsigned short max = %u\n", USHRT_MAX);
   printf("unsigned int max   = %u\n", UINT_MAX);
@@ -42,6 +44,22 @@ void solu_1(){
 }
 
 void solu_2(){
+  printf("Signed Types(%s)\n\n", __func__);
+  printf("signed char min   = %d\n", -(char)((unsigned char) ~0 >> 1));
+  printf("signed char max   = %d\n",  (char)((unsigned char) ~0 >> 1));
+  printf("signed short min  = %d\n", -(short)((unsigned short) ~0 >> 1));
+  printf("signed short max  = %d\n",  (short)((unsigned short) ~0 >> 1));
+  printf("signed int min    = %d\n", -(int)((unsigned int) ~0 >> 1));
+  printf("signed int max    = %d\n",  (int)((unsigned int) ~0 >> 1));
+  printf("signed long min   = %ld\n", -(long)((unsigned long) ~0 >> 1));
+  printf("signed long max   = %ld\n",  (long)((unsigned long) ~0 >> 1));
 
+  printf("\n");
+
+  printf("Unsigned Types(%s)\n\n", __func__);
+  printf("unsigned char max   = %u\n",  (unsigned char)  ~0);
+  printf("unsigned short max  = %u\n",  (unsigned short) ~0);
+  printf("unsigned int max   = %u\n",   (unsigned int)   ~0);
+  printf("unsigned long max   = %lu\n", (unsigned long)  ~0);
 }
 
