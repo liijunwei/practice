eda81bbe5887bd650f6a338e55609f46878a21f7
lijunwei <ljw532344863@sina.com>
2021-12-19 10:39:58 +0800

    Commit manually.

---
+++ c/the-c-programming-language/foobar/thread_demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *entry_point(void *value){
+  printf("hello from the 2nd thread :)\n");
+
+  return NULL;
+}
+
+int main(int argc, char const *argv[])
+{
+  pthread_t thread;
+
+  printf("hello from the 1st thread :D\n");
+
+  int num = 9981;
+  pthread_create(&thread, NULL, entry_point, &num);
+  pthread_join(thread, NULL);
+
+  return 0;
+}
