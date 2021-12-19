d102dc6731ed63aa9421bb4c89661010eff1d3c7
lijunwei <ljw532344863@sina.com>
2021-12-19 12:56:41 +0800

    Commit manually.

---
+++ c/the-c-programming-language/foobar/compile_demo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+// gcc -E -o compile_demo_ext.c compile_demo.c
+// gcc -S compile_demo.c
+// gcc -c compile_demo.s -o compile_demo_xx
+
+int main(int argc, char const *argv[])
+{
+  printf("hello em\n");
+  return 0;
+}
