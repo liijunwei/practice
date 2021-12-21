6a10642b8c6037ef2ecd3fda64eb6668ba531378
lijunwei <ljw532344863@sina.com>
2021-12-21 23:29:20 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/atoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// page 33
+
+int custom_atoi(const char s[]){
+  int i;
+  int n = 0;
+
+  for(i = 0; s[i] >= '0' && s[i] <= '9'; i++){
+    n = (10 * n) + (s[i] - '0');
+  }
+
+  return n;
+}
+
+int main(int argc, char const *argv[])
+{
+  char *num = "9527";
+  printf("number: %d\n", custom_atoi(num));
+
+  return 0;
+}
+
