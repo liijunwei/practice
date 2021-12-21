16097616602e6982f09b4d863bb15d2a333d6db5
lijunwei <ljw532344863@sina.com>
2021-12-21 23:39:05 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/lower-02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// page 34
+
+int main(int argc, char const *argv[])
+{
+  printf("isdight: %d\n", isdigit('a'));
+  printf("isdight: %d\n", isdigit(1));
+  printf("isdight: %d\n", isdigit(2));
+
+  printf("tolower: %c\n", tolower('A'));
+  printf("\n");
+  printf("tolower: %c\n", tolower('B'));
+  printf("tolower: %c\n", tolower('M'));
+  printf("tolower: %c\n", tolower('N'));
+  printf("tolower: %c\n", tolower('Y'));
+  printf("tolower: %c\n", tolower('Z'));
+  printf("tolower: %c\n", tolower('g'));
+
+  return 0;
+}
+
