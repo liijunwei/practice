1c2865eb8e4d9519940692ec266a4d9dafb67b7f
lijunwei <ljw532344863@sina.com>
2021-12-21 08:56:05 +0800

    Commit manually.

---
+++ c/game_fizz_buzz-V2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+http://wiki.c2.com/?FizzBuzzTest
+
+高手
+*/
+
+int main() {
+  for( int i=1; i<=100; i++)
+  {
+    if(i%3==0)
+      printf("Fizz");
+    if(i%5==0)
+      printf("Buzz");
+    if(i%3!=0 && i%5!=0)
+      printf("%d",i);
+
+    printf("\n");
+  }
+
+  return 0;
+}
