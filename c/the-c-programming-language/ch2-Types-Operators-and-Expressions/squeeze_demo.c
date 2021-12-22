aa6e08622f6d16122953bb846a68f0d453cd8fe8
lijunwei <ljw532344863@sina.com>
2021-12-23 00:17:37 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/squeeze_demo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// 从字符串s中删除字符c
+void squeeze(char s[], int c){
+  int i = 0;
+  int j = 0;
+
+  for(; s[i] != '\0'; i++){
+    if(s[i] != c){
+      s[j] = s[i];
+      j++;
+    }
+  }
+
+  s[j] = '\0';
+}
+
+int main(int argc, char const *argv[])
+{
+  char str[10] = "hello";
+  printf("char: %s\n", str);
+
+  squeeze(str, 'a');
+  printf("char: %s\n", str);
+  squeeze(str, 'h');
+  printf("char: %s\n", str);
+
+  return 0;
+}
