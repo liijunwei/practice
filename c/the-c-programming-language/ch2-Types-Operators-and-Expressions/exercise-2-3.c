4d48f20d8b5fd81cec8d687c756e133c7eceafad
lijunwei <ljw532344863@sina.com>
2021-12-22 23:30:08 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-3.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-3.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
-#include <limits.h>
+#include <ctype.h>
+#include <assert.h>
 
 /*
 page 37
 
 编写函数htoi(s)，把由十六进制数字组成的字符串(包含可选的前缀0x或0X)转换为与之等价的整型值。字符串中允许包含的数字包括:0~9、a~f以及A~F
 
-TODO 参考: ch2-Types-Operators-and-Expressions/atoi.c
+OK 参考: ch2-Types-Operators-and-Expressions/atoi.c
 */
+
+int custom_htoi(const char s[]){
+  int flag = 1;
+  int n = 0;
+  int hexdigit;
+
+  for(int i = 0; flag == 1; i++){
+    if(s[i] >= '0' && s[i] <= '9'){
+      hexdigit = s[i] - '0';
+    } else if(tolower(s[i]) >= 'a' && tolower(s[i]) <= 'f'){
+      hexdigit = tolower(s[i]) - 'a' + 10;
+    } else {
+      flag = -1;
+    }
+
+    if(flag == 1){
+      n = 16 * n + hexdigit;
+    }
+  }
+
+  return n;
+}
 
 int main(int argc, char const *argv[])
 {
+  printf("assertion started...\n");
+  assert(custom_htoi("1a") == 26);
 
+  printf("assertion pass!\n");
   return 0;
 }
+
--- c/the-c-programming-language/foobar/foo01.c
+++ c/the-c-programming-language/foobar/foo01.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 int foo(){
-  int x;
-  return x == (1 && x);
+  return 'B' - 'A' + 10;
 }
 
 int main(int argc, char const *argv[])
 {
-  printf("%d\n", foo());
+  printf("%d\n\n", foo());
+
+  printf("%d\n", 'A' - 'A' + 10);
+  printf("%d\n", 'B' - 'A' + 10);
+  printf("%d\n", 'C' - 'A' + 10);
+  printf("%d\n", 'D' - 'A' + 10);
+  printf("%d\n", 'E' - 'A' + 10);
+  printf("%d\n", 'F' - 'A' + 10);
+
+  printf("%d\n", 'a' - 'a' + 10);
+  printf("%d\n", 'b' - 'a' + 10);
+  printf("%d\n", 'c' - 'a' + 10);
+  printf("%d\n", 'd' - 'a' + 10);
+  printf("%d\n", 'e' - 'a' + 10);
+  printf("%d\n", 'f' - 'a' + 10);
   return 0;
 }
