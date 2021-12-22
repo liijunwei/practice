f25defba2a739d50a5c6944a8ee07760e89eecb8
lijunwei <ljw532344863@sina.com>
2021-12-22 08:48:17 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/random.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/random.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 /*
 page 36
@@ -13,12 +14,14 @@ void custom_srand(unsigned int seed){
 
 // 返回 0-327667 的伪随机数
 int custom_rand(){
+  custom_srand((unsigned long)time(NULL));
+
   next = next * 1103515245 + 12345;
   return (unsigned int)(next / 65536) % 32768;
 }
 
 int main(int argc, char const *argv[])
 {
-  printf("%d", custom_rand());
+  printf("random number between 0-327667 generated: %d\n", custom_rand());
   return 0;
 }
