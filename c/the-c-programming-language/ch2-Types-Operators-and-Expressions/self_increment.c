62cf8543f41322ffbfb34c40dcd542c40dedd302
lijunwei <ljw532344863@sina.com>
2021-12-22 23:42:00 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/self_increment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int m = 5;
+  int n = 5;
+
+  int x = m++;
+  int y = ++n;
+
+  printf("x: %d\n", x);
+  printf("yd: %d\n", yd);
+  return 0;
+}
