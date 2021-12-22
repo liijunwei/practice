cdb73f4e9b869789d10050ec247f57e20f57ac5b
lijunwei <ljw532344863@sina.com>
2021-12-22 09:23:06 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-3.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-3.c
@@ -5,6 +5,8 @@
 page 37
 
 编写函数htoi(s)，把由十六进制数字组成的字符串(包含可选的前缀0x或0X)转换为与之等价的整型值。字符串中允许包含的数字包括:0~9、a~f以及A~F
+
+TODO 参考: ch2-Types-Operators-and-Expressions/atoi.c
 */
 
 int main(int argc, char const *argv[])
