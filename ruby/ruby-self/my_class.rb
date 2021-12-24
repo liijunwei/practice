f49a09a0dadef301ef8e5270596c8ace6923f799
lijunwei <ljw532344863@sina.com>
2021-12-25 00:15:03 +0800

    Commit manually.

--- ruby/ruby-self/my_class.rb
+++ ruby/ruby-self/my_class.rb
@@ -1,6 +1,9 @@
 
 # "如果你想成为Ruby大师, 你应该在任何时候都知道当前那个对象在充当self的角色"
 class MyClass
+  puts "#{__LINE__} #{self}"
+  puts "#{__LINE__} #{self.class}"
+
   def testing_self
     @var = 10
     my_method()
