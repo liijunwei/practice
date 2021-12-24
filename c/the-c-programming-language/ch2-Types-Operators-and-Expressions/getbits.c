095de103f68868a9d14bd6d69b0f5339d202e380
lijunwei <ljw532344863@sina.com>
2021-12-25 00:03:50 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/getbits.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/getbits.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
-
+unsigned getbits(unsigned x, int p, int n){
+  return (x >> (p + 1 - n) & ~(~0 << n));
+}
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  // 0000 1000
+  printf("%d\n", getbits(100, 4, 5));
+  printf("%d\n", getbits(100, 4, 4));
+  printf("%d\n", getbits(100, 4, 3));
+  printf("%d\n", getbits(100, 4, 2));
+  printf("%d\n", getbits(100, 4, 1));
   return 0;
 }
