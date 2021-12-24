fc1a7ac35b29bc4c4f6b325a30260328cb8f80b2
lijunwei <ljw532344863@sina.com>
2021-12-24 08:35:18 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-5.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-5.c
@@ -12,16 +12,10 @@ int any(char s1[], char s2[]){
   int j;
 
   for(i = 0; s1[i] != '\0'; i++){
-    for(j = 0; s2[j] != '\0' && s1[i] != s2[j]; j++){
-      ;
-    }
-
-    if(s2[j] == '\0'){
-      // do nothing
-    }
-
-    if(s2[j] == s1[i]){
-      return i;
+    for(j = 0; s2[j] != '\0'; j++){
+      if(s2[j] == s1[i]){
+        return i;
+      }
     }
   }
 
