8cb91720abb56a039e857d5ece16e620e3a93de4
lijunwei <ljw532344863@sina.com>
2021-12-25 09:12:39 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/getbits.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/getbits.c
@@ -6,6 +6,12 @@ page 39
 getbits 返回x中从右边第p位开始向右数n为的字段
 这里假设最右边的一位是第0位, n与p都是合理的正值
 例如: getbits(x, 4, 3) 返回x中第 4, 3, 2 三位的值
+
+generate sample data
+--------------------
+toBinary 78  => 0100 1110
+toBinary 89  => 0101 1001
+toBinary 657 => 0010 1001 0001
 */
 unsigned getbits(unsigned x, int p, int n){
   return (x >> (p + 1 - n) & ~(~0 << n));
@@ -13,11 +19,25 @@ unsigned getbits(unsigned x, int p, int n){
 
 int main(int argc, char const *argv[])
 {
-  // 0000 1000
-  printf("%d\n", getbits(100, 4, 5));
-  printf("%d\n", getbits(100, 4, 4));
-  printf("%d\n", getbits(100, 4, 3));
-  printf("%d\n", getbits(100, 4, 2));
-  printf("%d\n", getbits(100, 4, 1));
+  printf("%d\n", getbits(255, 4, 3)); // 7
+  printf("\n");
+
+  printf("%d\n", getbits(78, 4, 3)); // 3
+  printf("%d\n", getbits(78, 4, 2)); // 1
+  printf("%d\n", getbits(78, 4, 1)); // 0
+  printf("%d\n", getbits(78, 4, 0)); // 0 n为0 这个有意义吗? 不确定
+  printf("\n");
+
+  printf("%d\n", getbits(89, 4, 3)); // 6
+  printf("%d\n", getbits(89, 4, 2)); // 3
+  printf("%d\n", getbits(89, 4, 1)); // 1
+  printf("%d\n", getbits(89, 4, 0)); // 0
+  printf("\n");
+
+  printf("%d\n", getbits(657, 6, 3)); // 1
+  printf("%d\n", getbits(657, 6, 2)); // 0
+  printf("%d\n", getbits(657, 6, 1)); // 0
+  printf("%d\n", getbits(657, 6, 0)); // 0
+  printf("\n");
   return 0;
 }
