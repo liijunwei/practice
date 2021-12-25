50090c61b2ee12dc0dc5be8333e058f84ada41db
lijunwei <ljw532344863@sina.com>
2021-12-25 20:55:06 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+page 40
+
+编写一个函数setbits(x, p, n, y)
+该函数返回对x执行下列操作后的结果值: 将x中从第p位开始的n个(二进制)位设置为y中最右边n位的值，x的其余各位保持不变
+*/
+
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
