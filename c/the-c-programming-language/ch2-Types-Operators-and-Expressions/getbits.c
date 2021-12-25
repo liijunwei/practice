2bc4bae0ef259a9270dc2a82adad44660412fe5c
lijunwei <ljw532344863@sina.com>
2021-12-25 08:45:12 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/getbits.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/getbits.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+page 39
+
+getbits 返回x中从右边第p位开始向右数n为的字段
+这里假设最右边的一位是第0位, n与p都是合理的正值
+例如: getbits(x, 4, 3) 返回x中第 4, 3, 2 三位的值
+*/
 unsigned getbits(unsigned x, int p, int n){
   return (x >> (p + 1 - n) & ~(~0 << n));
 }
