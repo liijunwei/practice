75b41d185cb833a074df38b13a4c3093536f701f
lijunwei <ljw532344863@sina.com>
2021-12-25 23:31:39 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/EX2-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+
+int c_tolower(int c){
+  return (c >= 'A' && c <= 'X') ? (c + 'a' - 'A') : c;
+}
+
+int main(int argc, char const *argv[])
+{
+  assert(c_tolower('H') == 'h');
+  assert(c_tolower('e') == 'e');
+  assert(c_tolower('L') == 'l');
+  assert(c_tolower('l') == 'l');
+  assert(c_tolower('O') == 'o');
+  assert(c_tolower('A') == 'a');
+  printf("PASS.\n");
+  return 0;
+}
