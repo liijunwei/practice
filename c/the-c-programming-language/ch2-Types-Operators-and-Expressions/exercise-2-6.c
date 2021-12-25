3736c001a416c9e25b6ee846f11ef5822915d45b
lijunwei <ljw532344863@sina.com>
2021-12-25 21:03:06 +0800

    Commit manually.

--- c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-6.c
+++ c/the-c-programming-language/ch2-Types-Operators-and-Expressions/exercise-2-6.c
@@ -5,6 +5,10 @@ page 40
 
 编写一个函数setbits(x, p, n, y)
 该函数返回对x执行下列操作后的结果值: 将x中从第p位开始的n个(二进制)位设置为y中最右边n位的值，x的其余各位保持不变
+
+1. 把x中的n位 置为0
+2. 把y中的n位 移动到p位
+3. x | y
 */
 
 int main(int argc, char const *argv[])
