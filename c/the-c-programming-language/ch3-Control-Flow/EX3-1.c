b7046c121e29a688a198338c425fcf65be734ade
lijunwei <ljw532344863@sina.com>
2021-12-26 11:04:26 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/EX3-1.c
+++ c/the-c-programming-language/ch3-Control-Flow/EX3-1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
 
 /*
 page 47
@@ -10,9 +11,26 @@ ch3-Control-Flow/binary_search.c 的例子里, while循环语句内共执行了�
 
 "how to do benchmark for c program?"
 https://stackoverflow.com/questions/2349776/how-can-i-benchmark-c-code-easily
+
+benchmark result
+
+lijunwei@bxzy:the-c-programming-language(main)$ uname -a
+Darwin bxzy 20.6.0 Darwin Kernel Version 20.6.0: Tue Oct 12 18:33:42 PDT 2021; root:xnu-7195.141.8~1/RELEASE_X86_64 x86_64
+lijunwei@bxzy:the-c-programming-language(main)$ gcc ch3-Control-Flow/EX3-1.c && ./a.out
+binsearch_v1 costs: 0.547102
+binsearch_v2 costs: 0.657599
+
+root@sanchez:~$ uname -a
+Linux sanchez 3.10.0-1160.25.1.el7.x86_64 #1 SMP Wed Apr 28 21:49:45 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
+root@sanchez:~$ gcc ex3-1.c && ./a.out
+binsearch_v1 costs: 0.781838
+binsearch_v2 costs: 0.844247
+
+说明减少一次循环没啥意义嘛?
 */
 
 #define ARR_SIZE 6
+#define TEST_ROUNDS 10000000
 
 int binsearch_v1(int x, int v[], int n){
   int low = 0;
@@ -54,16 +72,41 @@ int binsearch_v2(int x, int v[], int n){
 int main(int argc, char const *argv[])
 {
   int a[ARR_SIZE] = {1, 2, 4, 665, 876, 988};
+  float start_time;
+  float end_time;
+  float time_elapsed;
 
-  assert(0 == binsearch_v2(1, a, ARR_SIZE));
-  assert(1 == binsearch_v2(2, a, ARR_SIZE));
-  assert(2 == binsearch_v2(4, a, ARR_SIZE));
-  assert(3 == binsearch_v2(665, a, ARR_SIZE));
-  assert(4 == binsearch_v2(876, a, ARR_SIZE));
-  assert(5 == binsearch_v2(988, a, ARR_SIZE));
-  assert(-1 == binsearch_v2(1000, a, ARR_SIZE));
-  assert(-1 == binsearch_v2(-199, a, ARR_SIZE));
-  printf("PASS.\n");
+  start_time = (float)clock()/CLOCKS_PER_SEC;
+  for(int i = 0; i < TEST_ROUNDS; i++){
+    assert(0 == binsearch_v1(1, a, ARR_SIZE));
+    assert(1 == binsearch_v1(2, a, ARR_SIZE));
+    assert(2 == binsearch_v1(4, a, ARR_SIZE));
+    assert(3 == binsearch_v1(665, a, ARR_SIZE));
+    assert(4 == binsearch_v1(876, a, ARR_SIZE));
+    assert(5 == binsearch_v1(988, a, ARR_SIZE));
+    assert(-1 == binsearch_v1(1000, a, ARR_SIZE));
+    assert(-1 == binsearch_v1(-199, a, ARR_SIZE));
+    // printf("PASS.\n");
+  }
+  end_time = (float)clock()/CLOCKS_PER_SEC;
+  time_elapsed = end_time - start_time;
+  printf("binsearch_v1 costs: %f\n", time_elapsed);
+
+  start_time = (float)clock()/CLOCKS_PER_SEC;
+  for(int i = 0; i < TEST_ROUNDS; i++){
+    assert(0 == binsearch_v2(1, a, ARR_SIZE));
+    assert(1 == binsearch_v2(2, a, ARR_SIZE));
+    assert(2 == binsearch_v2(4, a, ARR_SIZE));
+    assert(3 == binsearch_v2(665, a, ARR_SIZE));
+    assert(4 == binsearch_v2(876, a, ARR_SIZE));
+    assert(5 == binsearch_v2(988, a, ARR_SIZE));
+    assert(-1 == binsearch_v2(1000, a, ARR_SIZE));
+    assert(-1 == binsearch_v2(-199, a, ARR_SIZE));
+    // printf("PASS.\n");
+  }
+  end_time = (float)clock()/CLOCKS_PER_SEC;
+  time_elapsed = end_time - start_time;
+  printf("binsearch_v2 costs: %f\n", time_elapsed);
 
   return 0;
 }
