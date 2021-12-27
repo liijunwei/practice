22d09b70c9efa7ad42e22b35aafbfaea3b4a53dc
lijunwei <ljw532344863@sina.com>
2021-12-27 22:29:08 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/EX3-2.c
+++ c/the-c-programming-language/ch3-Control-Flow/EX3-2.c
@@ -3,8 +3,19 @@
 /*
 page 49
 
+编写一个函数 escape(s,t)，将字符串t复制到字符串s中，并在复制过程中将换行符、制表符等不可显示字符分别转换为\n、\t等相应的可显示的转义字符序列。
+要求使用switch语句。
+再编写一个具有相反功能的函数，在复制过程中将转义字符序列转换为实际字符。
 
 */
+
+void escape(s[], t[]){
+
+}
+
+void unescape(s[], t[]){
+
+}
 
 int main(int argc, char const *argv[])
 {
