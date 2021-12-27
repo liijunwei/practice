1b380d9fe97dc438bc61c533015a2a435de51d85
lijunwei <ljw532344863@sina.com>
2021-12-27 08:38:03 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/char_demo.c
+++ c/the-c-programming-language/ch3-Control-Flow/char_demo.c
@@ -5,6 +5,26 @@
 // 统计数字/空白及其他字符
 int main(int argc, char const *argv[])
 {
-  /* code */
+  int c;
+  int i;
+  int nwhite = 0;
+  int nother = 0;
+  int ndight[10];
+
+  for(i = 0; i < 10; i++){
+    ndight[i] = i;
+  }
+
+  while((c = getchar()) != EOF){
+
+  }
+
+  printf("dights =");
+  for(i = 0; i < 10; i++){
+    printf(" %d", ndight[i]);
+  }
+
+  printf(", white space = %d, other = %d\n", nwhite, nother);
+
   return 0;
 }
