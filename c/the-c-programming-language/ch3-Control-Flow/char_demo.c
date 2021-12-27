ad3732bcf4f510855746119963054d3f73235c74
lijunwei <ljw532344863@sina.com>
2021-12-27 08:41:54 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/char_demo.c
+++ c/the-c-programming-language/ch3-Control-Flow/char_demo.c
@@ -12,11 +12,32 @@ int main(int argc, char const *argv[])
   int ndight[10];
 
   for(i = 0; i < 10; i++){
-    ndight[i] = i;
+    ndight[i] = 0;
   }
 
   while((c = getchar()) != EOF){
-
+    switch(c){
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        ndight[c - '0']++;
+        break;
+      case ' ':
+      case '\n':
+      case '\t':
+        nwhite++;
+        break;
+      default:
+        nother++;
+        break;
+    }
   }
 
   printf("dights =");
