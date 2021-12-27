69775cf5c14d5d5be285232eab2968bea6876908
lijunwei <ljw532344863@sina.com>
2021-12-27 23:59:08 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/reverse-demo.c
+++ c/the-c-programming-language/ch3-Control-Flow/reverse-demo.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 #define ARR_SIZE 10
 
 // page 51
 
+// 倒置字符串s中各个字符的位置
 void reverse(char s[]){
+  int c;
+  int i;
+  int j;
 
+  for(i = 0, j = strlen(s) - 1; i < j; i++, j--){
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
 }
 
 int main(int argc, char const *argv[])
 {
-  char str[ARR_SIZE] = {'b', 'x', 'z', 'y', 'x', 'y', 'z', '\0'};
+  char str[ARR_SIZE] = "abcdefg";
 
   printf("before: %s\n", str);
   reverse(str);
