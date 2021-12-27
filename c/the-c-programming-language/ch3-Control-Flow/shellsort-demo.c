da06acdbbb980eaeea76edce1bc9e31ee8131a4d
lijunwei <ljw532344863@sina.com>
2021-12-27 23:45:17 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/shellsort-demo.c
+++ c/the-c-programming-language/ch3-Control-Flow/shellsort-demo.c
@@ -3,7 +3,20 @@
 #define ARR_SIZE 10
 
 void shellsort(int v[], int n){
+  int gap;
+  int i;
+  int j;
+  int temp;
 
+  for(gap = n / 2; gap > 0; gap /= 2){
+    for(i = gap; i < n; i++){
+      for(j = i - gap; j >= 0 && v[j] > v[j+gap]; j -= gap){
+        temp = v[j];
+        v[j] = v[j+gap];
+        v[j+gap] = temp;
+      }
+    }
+  }
 }
 
 void print_arr(char *info, const int v[]){
