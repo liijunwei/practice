270018d7ae2feeb0087dfa0e408ce5ab9983965a
lijunwei <ljw532344863@sina.com>
2021-12-28 00:00:24 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/EX3-3.c
+++ c/the-c-programming-language/ch3-Control-Flow/EX3-3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /*
-page xx
+page 52
 
-
+编写函数 expand(s1,s2)，将字符串s1中类似于a-z一类的速记符号在字符串s2中扩展为等价的完整列表abc...xyz。
+该函数可以处理大小写字母和数字，并可以处理a-b-c、a-z0-9与a-z等类似的情况。作为前导和尾随的字符原样复制
 */
 
 int main(int argc, char const *argv[])
