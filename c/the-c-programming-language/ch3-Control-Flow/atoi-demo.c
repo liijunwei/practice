6954f5e7e0f4e6996e3b9f619ef38775798639d0
lijunwei <ljw532344863@sina.com>
2021-12-27 22:56:46 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch3-Control-Flow/atoi-demo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+
+/*
+page 50
+
+*/
+
+// 将s转化为整型数
+int atoi(char s[]){
+
+  return 100;
+}
+
+int main(int argc, char const *argv[])
+{
+  assert(100 == atoi("1 2"));
+  printf("PASS.\n");
+
+  return 0;
+}
