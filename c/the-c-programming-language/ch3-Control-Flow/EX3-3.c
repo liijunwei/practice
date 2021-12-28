01e6fb62c8312118cdd4eb02d300132791bef848
lijunwei <ljw532344863@sina.com>
2021-12-28 08:37:57 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/EX3-3.c
+++ c/the-c-programming-language/ch3-Control-Flow/EX3-3.c
@@ -16,7 +16,7 @@ void expand(const char s1[], char s2[]){
     i++;
     if(s1[i] == '-' && s1[i+1] >= c){
       i++;
-      while(c < s1[i]){
+      while(c < s1[i]){ // expand
         s2[j++] = c++;
       }
     } else {
