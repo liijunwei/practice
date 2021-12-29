02ae30815d6ebc98912c2dcffbe24581bf9e15ae
lijunwei <ljw532344863@sina.com>
2021-12-29 08:50:11 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/EX3-4.c
+++ c/the-c-programming-language/ch3-Control-Flow/EX3-4.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /*
-page xx
+page 53
 
-
+在数的对二进制补码表示中, 我们编写的itoa函数不能处理最大的负数, 即n等于 `-(2^字长 - 1)` 的情况
+1. 请解释其原因
+2. 修改该函数, 使它在任何机器上运行时都能打印出正确的值
 */
 
 int main(int argc, char const *argv[])
