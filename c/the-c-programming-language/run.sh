d63efee225cbfeb45237dd9413c6f148912049e0
lijunwei <ljw532344863@sina.com>
2021-12-30 08:49:41 +0800

    Refactor run.sh

--- c/the-c-programming-language/run.sh
+++ c/the-c-programming-language/run.sh
@@ -1,5 +1,10 @@
 # echo $HOME/practice/c/the-c-programming-language/run.sh >> $HOME/.zshrc
 function run(){
-  local filename=$1;
-  gcc -g $filename && ./a.out
+  local filename=$1
+  gcc -g $filename
+  echo "ExitCode(Compile): $?"
+  ./a.out
+  echo "ExitCode(Execute): $?"
+  echo "========================"
+  echo
 }
