6761e5fef3f3fefd158cf9d2b5c2bc2f4561ea74
lijunwei <ljw532344863@sina.com>
2021-12-30 08:34:08 +0800

    Commit manually.

---
+++ c/the-c-programming-language/ch3-Control-Flow/trim-demo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+/*
+page 53
+
+*/
+
+int trim(char s[]){
+
+  return 100;
+}
+
+int main(int argc, char const *argv[])
+{
+  char str[100];
+
+  assert(100 == trim(str));
+  printf("PASS.\n");
+
+  return 0;
+}
