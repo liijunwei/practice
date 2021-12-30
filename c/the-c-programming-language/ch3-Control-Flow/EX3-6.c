3d11e967c1fbfd0a2ae1d408fbb367b2f085eba7
lijunwei <ljw532344863@sina.com>
2021-12-30 08:31:11 +0800

    Commit manually.

--- c/the-c-programming-language/ch3-Control-Flow/EX3-6.c
+++ c/the-c-programming-language/ch3-Control-Flow/EX3-6.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 /*
 page 53
@@ -8,8 +10,59 @@ page 53
 
 */
 
+#define abs(x) ((x) < 0 ? -(x) : (x))
+
+// ch3-Control-Flow/reverse-demo.c
+void reverse(char s[]){
+  int c;
+  int i;
+  int j;
+
+  for(i = 0, j = strlen(s) - 1; i < j; i++, j--){
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+// 把数字转为字符串
+void itoa(int n, char s[], int w){
+  int i;
+  int sign;
+
+  sign = n;
+  i = 0;
+
+  do {
+    s[i++] = abs(n % 10) + '0';
+  } while((n /= 10) != 0);
+
+  if(sign < 0){
+    s[i++] = '-';
+  }
+
+  while(i < w){ // 必要时补充空格
+    s[i++] = ' ';
+  }
+
+  s[i] = '\0';
+  reverse(s);
+}
+
 int main(int argc, char const *argv[])
 {
+  // printf("min: %d\n", INT_MIN); // -2147483648
+  char buffer[100];
 
+  itoa(-2147483648, buffer,  11);
+  assert(strcmp("-2147483648", buffer) == 0);
+
+  itoa(998, buffer,  11);
+  assert(strcmp("        998", buffer) == 0);
+
+  itoa(2021, buffer,  11);
+  assert(strcmp("       2021", buffer) == 0);
+
+  printf("PASS.\n");
   return 0;
 }
