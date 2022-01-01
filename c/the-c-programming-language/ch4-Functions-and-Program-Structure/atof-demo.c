31afe14b27476252daff1277e97d54dd5a2bf951
lijunwei <ljw532344863@sina.com>
2022-01-01 08:53:57 +0800

    Commit manually.

--- c/the-c-programming-language/ch4-Functions-and-Program-Structure/atof-demo.c
+++ c/the-c-programming-language/ch4-Functions-and-Program-Structure/atof-demo.c
@@ -11,15 +11,43 @@ page 60
 
 // 把字符串s转换为相应的双精度浮点数
 double atof(char s[]){
+  double val;
+  double power;
 
-  return 3.14;
+  int i;
+  int sign;
+
+  for(i = 0; isspace(s[i]); i++){
+    ; // 跳过空白符
+  }
+
+  sign = (s[i] == '-') ? -1 : 1;
+
+  if(s[i] == '+' || s[i] == '-'){
+    i++;
+  }
+
+  for(val = 0.0; isdigit(s[i]); i++){
+    val = 10.0 * val + (s[i] - '0');
+  }
+
+  if(s[i] == '.'){
+    i++;
+  }
+
+  for(power = 1.0; isdigit(s[i]); i++){
+    val = 10.0 * val + (s[i] - '0');
+    power *= 10.0;
+  }
+
+  return sign * val / power;
 }
 
 int main(int argc, char const *argv[])
 {
   char str[100] = "3.14159265354";
+  assert(3.14159265354 == atof(str));
 
-  assert(3.14 == atof(str));
   printf("PASS.");
   return 0;
 }
