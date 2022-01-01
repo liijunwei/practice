359c0608f903eb0c318b17bc54e45481ef26c7af
lijunwei <ljw532344863@sina.com>
2022-01-01 11:22:57 +0800

    Commit manually.

--- c/the-c-programming-language/ch4-Functions-and-Program-Structure/stack-demo.c
+++ c/the-c-programming-language/ch4-Functions-and-Program-Structure/stack-demo.c
@@ -7,7 +7,7 @@ page 65
 stack data structure
 */
 
-#define MAXVAL 5  // 栈val的最大深度
+#define MAXVAL 5    // 栈val的最大深度
 int sp = 0;         // 下一个空闲栈的位置
 double val[MAXVAL]; // 值栈
 
@@ -15,7 +15,9 @@ double val[MAXVAL]; // 值栈
 void push(double f){
   if(sp < MAXVAL){
     val[sp] = f;
+    int tmp_sp = sp;
     sp++;
+    printf("将 %f 压入栈位置: %d, 栈中下一空闲位置: %d\n", f, tmp_sp, sp);
   } else {
     printf("Error: stack(size: %d) full, can't push %g\n", MAXVAL, f);
   }
@@ -40,7 +42,7 @@ int main(int argc, char const *argv[])
   push(5);
   push(6);
 
-  for(int i = sp - 1; i >= 0; i--){
+  for(int i = MAXVAL - 1; i >= 0; i--){
     printf("%f\n", val[i]);
   }
 
