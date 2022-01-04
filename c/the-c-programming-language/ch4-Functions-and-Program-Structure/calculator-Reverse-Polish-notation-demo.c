e144d2043caed014276861a461c5f6822b2b7994
lijunwei <ljw532344863@sina.com>
2022-01-04 21:38:49 +0800

    Commit manually.

--- c/the-c-programming-language/ch4-Functions-and-Program-Structure/calculator-Reverse-Polish-notation-demo.c
+++ c/the-c-programming-language/ch4-Functions-and-Program-Structure/calculator-Reverse-Polish-notation-demo.c
@@ -37,10 +37,12 @@ double pop(void){
 char buf[BUFSIZE];
 int bufp = 0; // buf中下一空闲的位置
 
+// 取一个字符(可能是压回的字符)
 int getch(void){
   return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
+// 把字符压回输入中
 void ungetch(int c){
   if(bufp >= BUFSIZE) {
     printf("Ungetch, too many characters\n");
