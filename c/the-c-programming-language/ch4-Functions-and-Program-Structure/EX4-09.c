10f19656bef40bb2a6c7b442ade3e5fa5e6203e4
lijunwei <ljw532344863@sina.com>
2022-01-05 22:41:30 +0800

    Commit manually.

--- c/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-09.c
+++ c/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-09.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
 page 68
@@ -6,10 +7,43 @@ page 68
 以上介绍的getch和ungetch函数不能正确地处理压回的EOF
 考虑压回EOF时应该如何处理, 请实现你的设计方案
 
+TODO 没看懂...
+
 */
+
+#define BUFSIZE 100
+int buf[BUFSIZE]; // char buf[BUFSIZE] -> int buf[BUFSIZE]
+int bufp = 0;
+
+int getch(void){
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+  if(bufp >= BUFSIZE) {
+    printf("Ungetch, too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
 
 int main(int argc, char const *argv[])
 {
+  ungetch('h');
+  ungetch('e');
+  ungetch('l');
+  ungetch('l');
+  ungetch('o');
+  ungetch(' ');
+  ungetch('l');
+  ungetch('j');
+  ungetch('w');
+  ungetch(EOF);
+
+  int c;
+  while((c = getch()) != EOF){
+    putchar(c);
+  }
 
   return 0;
 }
