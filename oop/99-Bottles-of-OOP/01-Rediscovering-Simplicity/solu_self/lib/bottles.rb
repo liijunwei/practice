8464a3108fd47e713b42c7d8f959931c357c892e
lijunwei <ljw532344863@sina.com>
2022-01-05 23:39:28 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/solu_self/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/solu_self/lib/bottles.rb
@@ -1,16 +1,27 @@
 class Bottles
   def song
-    lyric_p1 = ""
+    lyric_p1 = song_first_part
+    lyric_p2 = song_second_part
+
+    return lyric_p1 + lyric_p2
+  end
+
+  def song_first_part
+    lyric = ""
 
     99.downto(3) do |number|
-      lyric_p1 += <<~EOF
+      lyric += <<~EOF
 #{number} bottles of beer on the wall, #{number} bottles of beer.
 Take one down and pass it around, #{number - 1} bottles of beer on the wall.
 
       EOF
     end
 
-    lyric_p2 = <<-SONG
+    return lyric
+  end
+
+  def song_second_part
+    lyric = <<-SONG
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
 
@@ -21,7 +32,7 @@ No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
     SONG
 
-    return lyric_p1 + lyric_p2
+    return lyric
   end
 end
 
