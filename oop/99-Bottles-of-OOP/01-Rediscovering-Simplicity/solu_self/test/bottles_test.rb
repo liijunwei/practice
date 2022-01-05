7e622b649a58553ef84f9a204b99b0e64260a4a0
lijunwei <ljw532344863@sina.com>
2022-01-05 23:29:49 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/solu_self/test/bottles_test.rb
+++ oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/solu_self/test/bottles_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/bottles'
 
+# rvm use 2.5.1
 # ruby test/bottles_test.rb
 class BottlesTest < Minitest::Test
   def test_the_whole_song
