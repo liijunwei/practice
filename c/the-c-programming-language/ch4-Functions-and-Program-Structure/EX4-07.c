3a88980bf1bd9d0b812b09942387c58ed35224c5
lijunwei <ljw532344863@sina.com>
2022-01-05 22:14:34 +0800

    Commit manually.

--- c/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-07.c
+++ c/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-07.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
 page 67
@@ -10,8 +11,41 @@ ungets 函数需要使用buf和bufp吗?
 
 */
 
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void){
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+  if(bufp >= BUFSIZE) {
+    printf("Ungetch, too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
+
+void ungets(char s[]){
+  int len = strlen(s);
+
+  while(len > 0){
+    --len;
+    ungetch(s[len]);
+  }
+}
+
 int main(int argc, char const *argv[])
 {
+  char str[100] = "helloworld.\n";
+  int c;
+
+  ungets(str);
+
+  while((c = getch()) != EOF){
+    putchar(c);
+  }
 
   return 0;
 }
