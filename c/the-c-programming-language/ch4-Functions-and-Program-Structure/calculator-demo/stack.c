ad7fb192fe3199166beec5bfa0f8537cd64fbf54
lijunwei <ljw532344863@sina.com>
2022-01-06 23:15:09 +0800

    Fix.

--- c/the-c-programming-language/ch4-Functions-and-Program-Structure/calculator-demo/stack.c
+++ c/the-c-programming-language/ch4-Functions-and-Program-Structure/calculator-demo/stack.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "./calc.h"
 
-#define MAXVAL 100 // 栈的最大深度
+#define MAXVAL 100 // 栈val的最大深度
 
-static double buf[MAXVAL]; // 值栈
-static int bufp = 0;       // 下一个空闲栈的位置
+int sp = 0; // 下一个空闲栈的位置
+double val[MAXVAL]; // 值栈
 
 // 把f压入栈中
 void push(double f){
-  if(bufp < MAXVAL){
-    buf[bufp] = f;
-    bufp++;
+  if(sp < MAXVAL){
+    val[sp] = f;
+    sp++;
   } else {
     printf("Error: stack full, can't push %g\n", f);
   }
@@ -18,8 +18,8 @@ void push(double f){
 
 // 弹出并返回栈顶的值
 double pop(void){
-  if(bufp > 0) {
-    return buf[--bufp];
+  if(sp > 0) {
+    return val[--sp];
   } else {
     printf("Error: stack empty\n");
     return 0.0;
