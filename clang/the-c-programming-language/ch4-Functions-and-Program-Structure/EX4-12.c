75794b3627b4bc69218804dab7592d33f27766c9
lijunwei <ljw532344863@sina.com>
2022-01-09 14:35:29 +0800

    Commit manually.

--- clang/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-12.c
+++ clang/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-12.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 /*
 page 75
@@ -6,11 +8,36 @@ page 75
 用printd函数的设计思想编写一个递归版本的itoa函数
 即通过递归调用把整数转换为字符串
 
-
 */
+
+#define abs(x) ((x) < 0 ? -(x) : (x))
+
+void itoa(int n, char s[]){
+  static int i;
+
+  if(n / 10){
+    itoa(n / 10, s);
+  } else {
+    i = 0;
+    if(n < 0){
+      s[i++] = '-';
+    }
+  }
+
+  s[i++] = abs(n) % 10 + '0';
+  s[i] = '\0';
+}
 
 int main(int argc, char const *argv[])
 {
+  char buffer[100];
+
+  itoa(-2147483, buffer);
+  assert(strcmp("-2147483", buffer) == 0);
+
+  itoa(998, buffer);
+  assert(strcmp("998", buffer) == 0);
 
   return 0;
 }
+
