f5cc0bfcc350b8287e8d6c1f2a743c30975a4513
lijunwei <ljw532344863@sina.com>
2022-01-09 14:40:41 +0800

    Commit manually.

--- clang/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-13.c
+++ clang/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-13.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 /*
 page 75
@@ -7,8 +9,26 @@ page 75
 
 */
 
+void reverse(char s[]){
+  int c;
+  int i;
+  int j;
+
+  for(i = 0, j = strlen(s) - 1; i < j; i++, j--){
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+#define ARR_SIZE 10
+
 int main(int argc, char const *argv[])
 {
+  char str[ARR_SIZE] = "abcdefg";
+
+  reverse(str);
+  assert(strcmp("gfedcba", str) == 0);
 
   return 0;
 }
