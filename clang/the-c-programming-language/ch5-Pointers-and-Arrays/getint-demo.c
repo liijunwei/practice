1057c836d3ea3bec7a19b5ceeaf767fce9a2f881
lijunwei <ljw532344863@sina.com>
2022-01-10 23:21:24 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/getint-demo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+page 82
+
+函数getint接收自由格式的输入, 并执行转换, 将输入的字符流分解为整数, 且每次调用得到一个整数
+getint需要返回转换后得到的整数, 并且在到达输入结尾时要返回文件结束标记
+这些值必须以不同的方式返回
+EOF可以用任何值表示, 当然也可用一个输入的整数表示
+*/
+
+
+int getint(int *pn){
+
+  return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
