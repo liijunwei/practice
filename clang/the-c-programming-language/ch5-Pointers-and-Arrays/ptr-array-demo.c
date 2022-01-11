32e9cb61671f2b8bb538e47c4b2f2cc15cfe1f27
lijunwei <ljw532344863@sina.com>
2022-01-11 23:43:23 +0800

    Add pointer-array demo

---
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/ptr-array-demo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <assert.h>
+
+/*
+page 84
+
+理解指针变量的加减运算
+
+*/
+
+#define ARR_SIZE 10
+
+int main(int argc, char const *argv[])
+{
+  int *pa;
+  int a[ARR_SIZE] = {1, 2, 3, 5};
+
+  pa = &a[0]; // <=> pa = a;
+
+  // a[i]  <=> *(pa + i)
+  // &a[i] <=> pa + i
+  // pa[i] <=> *(pa + i)
+  assert(a[0] == *(pa + 0));
+  assert(a[1] == *(pa + 1));
+  assert(a[2] == *(pa + 2));
+  assert(a[3] == *(pa + 3));
+
+  // assert(2 == *(pa + 3));
+
+  return 0;
+}
+
