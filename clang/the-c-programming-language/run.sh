de4afdc481006b26f682320dad6c7a517f1fc826
lijunwei <ljw532344863@sina.com>
2022-01-12 23:55:12 +0800

    Refactor run.sh

--- clang/the-c-programming-language/run.sh
+++ clang/the-c-programming-language/run.sh
@@ -2,14 +2,8 @@
 function run(){
   local filename=$1
   gcc -g $filename
-  echo "========================"
   echo "ExitCode(Compile): $?"
-  echo "=========START=========="
   echo
   ./a.out
-  echo
-  echo "=========PASS==========="
   echo "ExitCode(Execute): $?"
-  echo "========================"
-  echo
 }
