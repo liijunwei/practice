7e872fcae3872fe2d5ec088be9ca6b56b387d7ca
lijunwei <ljw532344863@sina.com>
2022-01-12 08:54:27 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/pass-paitial-array-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/pass-paitial-array-demo.c
@@ -14,6 +14,10 @@ void print_partial_array(char *s){
   printf("string is: %s\n", s);
 }
 
+void print_partial_array_backward(char *s){
+  printf("char is: %c\n", s[-1]);
+}
+
 int main(int argc, char const *argv[])
 {
   char s[ARR_SIZE] = {'a', 'b', 'c', 'd', '\0'};
@@ -30,6 +34,12 @@ int main(int argc, char const *argv[])
   print_partial_array(&s[2]);
   print_partial_array(&s[3]);
   print_partial_array(&s[4]);
+  printf("=================\n");
+
+  print_partial_array_backward(&s[4]); // d
+  print_partial_array_backward(&s[3]); // c
+  print_partial_array_backward(&s[2]); // b
+  print_partial_array_backward(&s[1]); // a
 
   return 0;
 }
