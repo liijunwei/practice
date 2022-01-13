6f4d278de41268150f4464415a9fab73966e5958
lijunwei <ljw532344863@sina.com>
2022-01-13 08:34:00 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/allocbuf-afree-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/allocbuf-afree-demo.c
@@ -19,7 +19,7 @@ char *alloc(int n){
     return allocp - n; // 明白了: allocp 这个指针在向右移动了n个单位后, 到了新的位置, 返回一个新的位置(指针), 表示分配好的这n个单位内存空间
   } else {
     printf("memory not enough(%d > %d)\n", n, ALLOCSIZE);
-    return 0;
+    return 0; // c语言保证, 0永远不是有效的数据地址, 因此可以用做没有足够空间的标识
   }
 }
 
