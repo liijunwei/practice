eacb1db060db337068d3e2f150ba7f882e4cd94a
lijunwei <ljw532344863@sina.com>
2022-01-13 22:16:51 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/char-pointer-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/char-pointer-demo.c
@@ -28,7 +28,12 @@ int main(int argc, char const *argv[])
    */
   char amessage[] = "now is the time";
 
-  char *pmessage = "now is the time";  // 定义一个指针
+  /**
+   * 定义一个指针
+   * pmessage 是一个指针, 其初值指向一个字符串常量, 之后它可以被修改以指向其他地址
+   * 但是如果试图修改字符串的内容, 结果是没有定义的
+   */
+  char *pmessage = "now is the time";
 
 
   return 0;
