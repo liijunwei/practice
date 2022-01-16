bee6522a41eaf5c17f69fa239b9f87b7303b7e3f
lijunwei <ljw532344863@sina.com>
2022-01-16 21:27:57 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/sort-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/sort-demo.c
@@ -95,3 +95,32 @@ void writelines(char *lineptr[], int nlines){
   }
 }
 
+void swap(char *v[], int i, int j){
+  char *temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
+
+void qsort(char *v[], int left, int right){
+  int i;
+  int last;
+
+  if(left >= right){ // 如果数组元素的个数小于2, 则返回
+    return;
+  }
+
+  swap(v, left, (left + right) / 2);
+  last = left;
+
+  for(i = left + 1; i <= right; i++){
+    if(strcmp(v[i], v[left]) < 0){
+      swap(v, ++last, i);
+    }
+  }
+
+  swap(v, left, last);
+  qsort(v, left, last - 1);
+  qsort(v, last + 1, right);
+}
+
+
