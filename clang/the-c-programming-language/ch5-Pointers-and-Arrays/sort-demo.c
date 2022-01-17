10356152315ccdb237cfeb46e97ed0c9d939cc2b
lijunwei <ljw532344863@sina.com>
2022-01-17 23:24:35 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/sort-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/sort-demo.c
@@ -37,9 +37,8 @@ static char *allocp = allocbuf;  // 下一空闲位置
 
 // 返回指向n个字符的指针(ch5-Pointers-and-Arrays/allocbuf-afree-demo.c)
 char *alloc(int n){
-  if(allocbuf + ALLOCSIZE - allocp >= n){ // 有足够的空闲空间
+  if(allocbuf + ALLOCSIZE - allocp >= n){
     allocp += n;
-    // printf("allocating memory: %d\n", n);
     return allocp - n; // 明白了: allocp 这个指针在向右移动了n个单位后, 到了新的位置, 返回一个新的位置(指针), 表示分配好的这n个单位内存空间
   } else {
     printf("memory not enough(%d > %d)\n", n, ALLOCSIZE);
