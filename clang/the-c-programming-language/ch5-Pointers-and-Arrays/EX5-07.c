4153c4f88a225dab8303e8640499ca61b5619163
lijunwei <ljw532344863@sina.com>
2022-01-18 23:51:13 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-07.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-07.c
@@ -9,6 +9,8 @@ ch5-Pointers-and-Arrays/sort-demo.c
 重写函数readlines, 将输入的文本行存储到由main函数提供的一个数组中, 而不是存储到调用alloc分配的存储空间中;
 改函数的运行速度比改写之前快多少?
 
+TODO not clear
+
 */
 
 #define MAXLINES 5000 // 进行排序的最大文本行数
