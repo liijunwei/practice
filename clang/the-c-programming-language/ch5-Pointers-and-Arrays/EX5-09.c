c754d1c492c46cca188f0ea7016801481e7fbd81
lijunwei <ljw532344863@sina.com>
2022-01-19 23:13:36 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-09.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-09.c
@@ -1,13 +1,89 @@
 #include <stdio.h>
+#include <assert.h>
 
 /*
-page xx
+page 98
 
+用指针方式代替数组下标方式改写 day_of_year 和 month_day
 
 */
 
+static char daytab[2][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day){
+  char *p;
+  int leap = year % 4 && year % 100 != 0 || year % 400;
+
+  if(month < 1 || month > 12){
+    return -1;
+  }
+
+  if(day < 1 || day > daytab[leap][month]){
+    return -1;
+  }
+
+  p = daytab[leap];
+
+  while(--month){
+    day += *++p;
+  }
+
+  return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday){
+  char *p;
+  int leap = year % 4 && year % 100 != 0 || year % 400;
+
+  if(year < 1){
+    *pmonth = -1;
+    *pday = -1;
+    return;
+  }
+
+  p = daytab[leap];
+  while(yearday > *++p){
+    yearday -= *p;
+  }
+
+  *pmonth = p - *(daytab + leap);
+  *pday = yearday;
+}
+
 int main(int argc, char const *argv[])
 {
+
+  assert(day_of_year(2022, 1, 1) == 1);
+  assert(day_of_year(2022, 1, 19) == 19);
+  assert(day_of_year(2021, 12, 30) == 365);
+
+  assert(day_of_year(2021, 13, 30) == -1);
+  assert(day_of_year(2021, 12, 32) == -1);
+  assert(day_of_year(2021, 12, 0) == -1);
+  assert(day_of_year(2021, 12, -999) == -1);
+  assert(day_of_year(2021, 12, 999) == -1);
+
+  int month;
+  int day;
+
+  month_day(2022, 1, &month, &day);
+  assert(month == 1);
+  assert(day == 1);
+
+  month_day(2022, 32, &month, &day);
+  assert(month == 2);
+  assert(day == 1);
+
+  // month_day(-999, 32, &month, &day);
+  // assert(month == -1);
+  // assert(day ==-1);
+
+  // month_day(2022, 999, &month, &day);
+  // assert(month == -1);
+  // assert(day ==-1);
 
   return 0;
 }
