abd8e4a797c3b8197073ecb3652f26869b03dfb7
lijunwei <ljw532344863@sina.com>
2022-01-19 21:31:25 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/date-transform-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/date-transform-demo.c
@@ -12,6 +12,20 @@ static char daytab[2][13] = {
   {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
+// 将某月某日的日期表示形式转换为某年中第几天的表现形式
+int day_of_year(int year, int month, int day){
+  int i;
+  int leap;
+
+  leap = year % 4 && year % 100 != 0 || year % 400;
+
+  for(i = 1; i < month; i++){
+    day += daytab[leap][i];
+  }
+
+  return day;
+}
+
 int main(int argc, char const *argv[])
 {
 
