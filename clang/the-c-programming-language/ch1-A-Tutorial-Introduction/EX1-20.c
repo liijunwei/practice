be28920e6b45a3c0be9aa9694bc7052c084c8773
lijunwei <ljw532344863@sina.com>
2022-01-25 23:03:53 +0800

    Commit manually.

--- clang/the-c-programming-language/ch1-A-Tutorial-Introduction/EX1-20.c
+++ clang/the-c-programming-language/ch1-A-Tutorial-Introduction/EX1-20.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "../common-utils/getline.c"
+
 // page 25
 
 // 懂了 :D
@@ -8,35 +10,48 @@
 // 假设制表符终止位的位置是固定的, 比如每个n列就会出现一个制表符终止位, n应该为变量还是符号常量呢?(本例中, 使用常量)
 
 // ch5-Pointers-and-Arrays/EX5-11.c
-
-#define TABING 8 // tab increment size
-#define VISIABLE_CHAR '*'
-
 // OK 如何检查程序的正确性?
 // 将空格 ' ' 换为其他可视的符号即可, 比如 '*'
 
+#define TABING 8 // tab increment size
+#define VISIABLE_CHAR '*'
+#define MAXLINE 1000 // 允许的输入行的最大长度
+
+void detab(char src[], char tar[]);
+
 // replace tabs with the proper number of blanks
 int main(){
-  int c;
-  int nb  = 0; // number of blanks necessary
-  int pos = 1; // position of character in line
+  char s[MAXLINE];
+  char t[MAXLINE * TABING];
 
-  while((c = getchar()) != EOF){
-    if(c == '\t'){
-      nb = TABING - (pos - 1) % TABING;
-      while(nb > 0){
-        putchar(VISIABLE_CHAR);
-        pos++;
-        nb--;
-      }
-    } else if(c == '\n'){
-      putchar(c);
-      pos = 1;
-    } else {
-      putchar(c);
-      pos++;
-    }
+  while(custom_getline(s, MAXLINE) > 0) {
+    detab(s, t);
+    printf("%s\n", t);
   }
 
   return 0;
+}
+
+void detab(char src[], char tar[]){
+  int c;
+  int nb  = 0; // number of blanks necessary
+
+  int i;
+  int j = 0;
+
+  for(i = 0; (c = src[i]) != '\0'; i++){
+    if(c == '\t'){
+      nb = TABING - j % TABING;
+      while(nb > 0){
+        tar[j] = VISIABLE_CHAR;
+        nb--;
+        j++;
+      }
+    } else {
+      tar[j] = c;
+      j++;
+    }
+  }
+
+  tar[j] = '\0';
 }
---
+++ clang/the-c-programming-language/common-utils/getline.c
@@ -0,0 +1,19 @@
+// 将行保存到s中, 并返回该行的行数
+int custom_getline(char s[], int max){
+  int c;
+  int i = 0;
+
+  while(--max > 0 && (c = getchar()) != EOF && c != '\n'){
+    s[i] = c;
+    i++;
+  }
+
+  if(c == '\n'){
+    s[i] = c;
+    ++i;
+  }
+
+  s[i] = '\0';
+
+  return i;
+}
