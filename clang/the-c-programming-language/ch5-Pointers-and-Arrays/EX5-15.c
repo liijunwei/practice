713b650af94cc2e25322a8721f064c1c9314df0a
lijunwei <ljw532344863@sina.com>
2022-01-28 23:14:23 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-15.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-15.c
@@ -20,7 +20,7 @@ page 105
 
 int charcmp(char *, char *);
 int numcmp(char *, char *);
-int readlines(char *, char *);
+int readlines(char *lineptr[], int maxlines);
 void custom_qsort(char *v[], int left, int right, int (*comp)(void *, void *));
 void printlines(char *lineptr[], int nlines, int order);
 
@@ -31,6 +31,75 @@ int main(int argc, char const *argv[])
 {
 
   return 0;
+}
+
+
+#define MAXLEN 1000
+#define ALLOCSIZE 10000
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+// 返回指向n个字符的指针(ch5-Pointers-and-Arrays/allocbuf-afree-demo.c)
+char *alloc(int n) {
+  if(allocbuf + ALLOCSIZE - allocp >= n) {
+    allocp += n;
+    return allocp - n;
+  } else {
+    printf("memory not enough(%d > %d)\n", n, ALLOCSIZE);
+    return NULL;
+  }
+}
+
+int readlines(char *lineptr[], int maxlines) {
+  int len;
+  int nlines;
+  char *p;
+  char line[MAXLEN];
+
+  nlines = 0;
+
+  while((len = custom_getline(line, MAXLEN)) > 0) {
+    if(nlines >= maxlines || (p = alloc(len)) == NULL) {
+      return -1;
+    } else {
+      line[len - 1] = '\0'; /* 把 '\n' 替换为 '\0' */
+      strcpy(p, line); /* char* strcpy( char* dest, const char* src ); */
+      lineptr[nlines++] = p;
+    }
+  }
+
+  return nlines;
+}
+
+
+void printlines(char *lineptr[], int nlines, int decr) {
+  int i;
+
+  if(decr) {
+    for(i = nlines - 1; i >= 0; i--) {
+      printf("%s\n", lineptr[i]);
+    }
+  } else {
+    for(i = 0; i < nlines; i++) {
+      printf("%s\n", lineptr[i]);
+    }
+  }
+}
+
+int numcmp(char *s1, char *s2) {
+  double v1;
+  double v2;
+
+  v1 = atof(s1);
+  v2 = atof(s2);
+
+  if(v1 < v2) {
+    return -1;
+  } else if (v1 > v2) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 /* return < 0 if s < t  */
