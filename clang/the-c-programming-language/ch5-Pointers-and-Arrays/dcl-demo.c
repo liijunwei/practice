17a811c3e7dd10deb2a82a4e493d5bf54ae2f661
lijunwei <ljw532344863@sina.com>
2022-01-29 16:27:36 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-demo.c
@@ -9,6 +9,8 @@ page 107
 #include <string.h>
 #include <ctype.h>
 
+#include "../common-utils/getch-ungetch.c"
+
 #define MAXTOKEN 100
 
 enum {
@@ -30,7 +32,18 @@ char out[1000];
 /* 将声明转为文字描述 */
 int main(int argc, char const *argv[])
 {
-  /* code */
+  while(gettoken() != EOF) {
+    strcpy(datatype, token);
+    out[0] = '\0';
+    dcl();
+
+    if(tokentype != '\n') {
+      printf("Syntax error\n");
+    }
+
+    printf("%s %s %s\n", name, out, datatype);
+  }
+
   return 0;
 }
 
@@ -52,6 +65,39 @@ void dirdcl() {
 }
 
 int gettoken() {
+  int c;
+  char *p = token;
+
+  while((c = getch()) == ' ' || c == '\t') {
+    ;
+  }
+
+  if(c == '(') {
+    if((c = getch()) == ')') {
+      strcpy(token, "()");
+      return tokentype = PARENS;
+    } else {
+      ungetch(c);
+      return tokentype = '(';
+    }
+  } else if(c == '[') {
+    for(*p++ = c; (*p++ = getch()) != ']';) {
+      ;
+    }
+
+    *p = '\0';
+    return tokentype = BRACKETS;
+  } else if(isalpha(c)) {
+    for(*p++ = c; isalnum(c = getch());) {
+      *p++ = c;
+    }
+    *p = '\0';
+    ungetch(c);
+    return tokentype = NAME;
+  } else {
+    return tokentype = c;
+  }
+
   return 1;
 }
 
