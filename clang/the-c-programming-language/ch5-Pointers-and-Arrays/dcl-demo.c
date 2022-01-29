4d834ab5a547f1b7e8809c93c12339aec9185f40
lijunwei <ljw532344863@sina.com>
2022-01-29 16:09:01 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-demo.c
@@ -9,9 +9,35 @@ page 107
 #include <string.h>
 #include <ctype.h>
 
+void dcl();
+void dirdcl();
+int gettoken();
+
+char out[1000];
+
 int main(int argc, char const *argv[])
 {
   /* code */
   return 0;
 }
 
+void dcl() {
+  int ns;
+  for(ns = 0; gettoken() == '*';) {
+    ns++;
+  }
+
+  dirdcl();
+
+  while(ns-- > 0) {
+    strcat(out, " pointer to");
+  }
+}
+
+void dirdcl() {
+
+}
+
+int gettoken() {
+  return 1;
+}
