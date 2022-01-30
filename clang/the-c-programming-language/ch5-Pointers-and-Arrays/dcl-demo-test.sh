b62015626b2f257f9e6276d880417c54eb14d1b1
lijunwei <ljw532344863@sina.com>
2022-01-30 19:45:56 +0800

    Modify test

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-demo-test.sh
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-demo-test.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
+check() {
+  command=$1
+  actual=$(eval $1)
+  expected=$2
+
+  if [ "$actual" == "$expected" ]; then
+    echo "$command # ok"
+  else
+    echo "$command # not ok"
+  fi
+}
+
 gcc ch5-Pointers-and-Arrays/dcl-demo.c
 
-echo "char **argv"       | ./a.out
-echo "int (*daytab)[13]" | ./a.out
-echo "int *daytab[13]"   | ./a.out
-echo "void *comp()"      | ./a.out
-echo "void (*comp)()"    | ./a.out
-echo "char (*(*x())[])()"   | ./a.out
-echo "char (*(*x[3])())[5]" | ./a.out
+check 'echo "int (*daytab)[13]"    | ./a.out' 'daytab:  pointer to array[13] of int'
+check 'echo "int *daytab[13]"      | ./a.out' 'daytab:  array[13] of pointer to int'
+check 'echo "void *comp()"         | ./a.out' 'comp:  function returning pointer to void'
+check 'echo "void (*comp)()"       | ./a.out' 'comp:  pointer to function returning void'
+check 'echo "char (*(*x())[])()"   | ./a.out' 'x:  function returning pointer to array[] of pointer to function returning char'
+check 'echo "char (*(*x[3])())[5]" | ./a.out' 'x:  array[3] of pointer to function returning pointer to array[5] of char'
