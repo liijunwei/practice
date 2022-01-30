448f1aef3f0303cf314e2ef760ccf4fafd14d3b3
lijunwei <ljw532344863@sina.com>
2022-01-30 19:55:56 +0800

    Commit manually.

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-undecl-demo.c
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/dcl-undecl-demo.c
@@ -24,6 +24,10 @@ int tokentype;
 char token[MAXTOKEN];
 char out[1000];
 
+// x is a function returning a pointer to an array of pointers to functions returning char
+// <=>
+// f () * [] * () char
+
 // gcc ch5-Pointers-and-Arrays/dcl-undecl-demo.c && echo "f () * [] * () char" | ./a.out
 
 /* undecl 函数, 将文字描述转换为声明 */
