50ec2bfd75f1e7331b2d19a29764e4eb590ecd82
lijunwei <ljw532344863@sina.com>
2022-01-31 00:43:27 +0800

    Modify test

--- clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-19-test.sh
+++ clang/the-c-programming-language/ch5-Pointers-and-Arrays/EX5-19-test.sh
@@ -3,9 +3,9 @@
 set -e
 
 check() {
-  expected=$1
-  shift
-  actual="$@"
+  expected="$1"
+  # shift
+  actual="$2"
 
   if [ "$expected" == "$actual" ]; then
     echo "ok"
@@ -21,16 +21,15 @@ gcc -o old ch5-Pointers-and-Arrays/dcl-undecl-demo.c
 gcc -o new ch5-Pointers-and-Arrays/EX5-19.c
 
 # daytab是一个指针, 它指向一个有13个元素的数组
-check 'int (*daytab)[13]' $(echo 'daytab * [13] int' | ./old)
-check 'int (*daytab)[13]' $(echo 'daytab * [13] int' | ./new)
+check 'int (*daytab)[13]' "$(echo 'daytab * [13] int' | ./old)"
+check 'int (*daytab)[13]' "$(echo 'daytab * [13] int' | ./new)"
 
 # daytab是一个有13个元素的数组指针, 数组里每个元素分别指向一个int
-check 'int (*daytab[13])' $(echo 'daytab [13] * int' | ./old)
-check 'int *daytab[13]' $(echo 'daytab [13] * int' | ./new)
+check 'int (*daytab[13])' "$(echo 'daytab [13] * int' | ./old)"
+check 'int *daytab[13]' "$(echo 'daytab [13] * int' | ./new)"
 
-check 'char (*x)' $(echo 'x * char' | ./old)
-check 'char *x' $(echo 'x * char' | ./new)
-check 'char *x' $(echo 'x * char1' | ./new)
+check 'char (*x)' "$(echo 'x * char' | ./old)"
+check 'char *x' "$(echo 'x * char' | ./new)"
 
 rm -rf old
 rm -rf new
