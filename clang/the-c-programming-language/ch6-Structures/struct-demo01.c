be76eaa4f79eeeefbd3b194b7e46f3f4e215217b
lijunwei <ljw532344863@sina.com>
2022-01-31 11:14:11 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/struct-demo01.c
+++ clang/the-c-programming-language/ch6-Structures/struct-demo01.c
@@ -16,8 +16,25 @@ struct point2 {
   int y;
 } a, b, c;
 
+typedef struct point2 Point ;
+
+void print_point(Point point) {
+  printf("x: %d y: %d\n", point.x, point.y);
+}
+
 int main(int argc, char const *argv[])
 {
+  a.x = 1;
+  a.y = 2;
+  print_point(a);
+
+  b.x = 6;
+  b.y = 8;
+  print_point(b);
+
+  c.x = 88;
+  c.y = 99;
+  print_point(c);
 
   return 0;
 }
