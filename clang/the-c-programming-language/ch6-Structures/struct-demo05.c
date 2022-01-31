76050c51467560e899f92de7593241d9570df2c9
lijunwei <ljw532344863@sina.com>
2022-01-31 12:13:24 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/struct-demo05.c
+++ clang/the-c-programming-language/ch6-Structures/struct-demo05.c
@@ -25,7 +25,12 @@ int main(int argc, char const *argv[])
   struct point *pp;
 
   pp = &origin;
-  printf("origin is (%d, %d)\n", (*pp).x, (*pp).y); /* (*pp).x 里的括号是必须的, 因为运算符'.'的优先级高于'*' */
+  /* (*pp).x 里的括号是必须的, 因为运算符'.'的优先级高于'*' */
+  printf("origin is (%d, %d)(V1)\n", (*pp).x, (*pp).y);
+
+  /* 结构指针的使用频度非常高, 为了使用方便, C语言提供了另一种简写方式 */
+  /* 假定p是一个执行结构的指针, 则 p->结构成员 <=> (*p).结构成员       */
+  printf("origin is (%d, %d)(V2)\n", pp->x, pp->y);
 
   return 0;
 }
