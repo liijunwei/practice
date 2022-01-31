a67ef1953e24958ea1af6109d84be2548bbd483a
lijunwei <ljw532344863@sina.com>
2022-01-31 21:14:47 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/keyword-count-demo0.c
+++ clang/the-c-programming-language/ch6-Structures/keyword-count-demo0.c
@@ -5,8 +5,8 @@ page 116
 */
 
 #include <stdio.h>
-
-#define NKEYS 100
+#include <ctype.h>
+#include <string.h>
 
 struct key {
   char *word;
@@ -25,8 +25,57 @@ struct key {
   {"while",    0},
 };
 
+#define NKEYS   11 /* keytab中关键字的个数 */
+#define MAXWORD 100
+
+int getword(char *, int);
+int binsearch(char *word, struct key tab[], int n);
+
 int main(int argc, char const *argv[])
 {
+  int n;
+  char word[MAXWORD];
+
+  while(getword(word, MAXWORD) != EOF) {
+    if(isalpha(word[0])) {
+      if((n = binsearch(word, keytab, NKEYS)) >= 0) {
+        keytab[n].count++;
+      }
+    }
+  }
+
+  for(n = 0; n < NKEYS; n++) {
+    if(keytab[n].count > 0) {
+      printf("%4d %s\n", keytab[n].count, keytab[n].word);
+    }
+  }
 
   return 0;
+}
+
+/* 每调用一次该函数, 将读入一个单词, 并将其复制到名字为该函数的第一个参数的数组中 */
+int getword(char *, int) {
+
+}
+
+int binsearch(char *word, struct key tab[], int n) {
+  int low = 0;
+  int mid;
+  int high = n -1;
+
+  int cond;
+
+  while(low <= high){
+    mid = (low + high) >> 1;
+
+    if((cond = strcmp(word, tab[mid].word)) < 0) {
+      high = mid - 1;
+    } else if(cond > 0) {
+      low = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
 }
