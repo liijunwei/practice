ab6606f779271d68f0d279ce3965ce1d56d52bad
lijunwei <ljw532344863@sina.com>
2022-02-01 12:39:07 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch6-Structures/struct-demo08.c
@@ -0,0 +1,21 @@
+/*
+page 122
+
+自引用结构的变体
+*/
+
+#include <stdio.h>
+
+struct t {
+  struct s *p;
+};
+
+struct s {
+  struct t *q;
+};
+
+int main(int argc, char const *argv[])
+{
+
+  return 0;
+}
