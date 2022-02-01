f92be24c2f25eee3234d894c8f802c4c7e2161b7
lijunwei <ljw532344863@sina.com>
2022-02-01 11:42:56 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/keyword-count-test.sh
+++ clang/the-c-programming-language/ch6-Structures/keyword-count-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 gcc ch6-Structures/keyword-count-demo01.c
+cat ch6-Structures/keyword-count-sample.md | ./a.out
 
-cat ch6-Structures/keyword-count-demo01.c   | ./a.out
 echo
-cat ch6-Structures/keyword-count-sample.md | ./a.out | sort -r
+echo "pointer to struct"
+gcc ch6-Structures/keyword-count-demo02.c
+cat ch6-Structures/keyword-count-sample.md | ./a.out
+
