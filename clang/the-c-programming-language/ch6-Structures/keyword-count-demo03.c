d6ead2fc53bcf03d013246eb60a7bd38eb3db5e2
lijunwei <ljw532344863@sina.com>
2022-02-01 12:31:40 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/keyword-count-demo03.c
+++ clang/the-c-programming-language/ch6-Structures/keyword-count-demo03.c
@@ -21,7 +21,12 @@ now is the time for all good men to come the aid of their party
 
 #include "../common-utils/getch-ungetch.c"
 
-
+struct tnode {         /* tree node       */
+  char *word;          /* pointer to word */
+  int count;           /* word count      */
+  struct tnode *left;  /* left tree node  */
+  struct tnode *right; /* right tree node */
+};
 
 int main(int argc, char const *argv[])
 {
