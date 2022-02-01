0490ed581bc8392b4a3cfb1b0b100db38f49d15e
lijunwei <ljw532344863@sina.com>
2022-02-01 13:42:35 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/EX6-02.c
+++ clang/the-c-programming-language/ch6-Structures/EX6-02.c
@@ -12,6 +12,8 @@ page 125
 #include <string.h>
 #include <stdlib.h>
 
+#include "../common-utils/getch-ungetch.c"
+
 struct tnode {
   char *word;
   int match;
@@ -23,7 +25,7 @@ struct tnode {
 #define YES 1
 #define NO 0
 
-struct tnode *address(struct tnode *, char *, int , int *);
+struct tnode *addtreex(struct tnode *, char *, int , int *);
 void treeprint(struct tnode *);
 int getword(char *, int);
 
@@ -31,6 +33,113 @@ int getword(char *, int);
 /* identical in the first num characters (default 6) */
 int main(int argc, char const *argv[])
 {
+  struct tnode *root;
+  char word[MAXWORD];
+  int found = NO;
+  int num;
+
+  num = (--argc && (*++argv)[0] == '-') ? atoi(argv[0] + 1) : 6;
+  root = NULL;
+
+  while(getword(word, MAXWORD) != EOF) {
+    if(isalpha(word[0]) && strlen(word) >= num) {
+      root = addtreex(root, word, num, &found);
+    }
+
+    found = NO;
+  }
+
+  treeprint(root);
 
   return 0;
 }
+
+
+struct tnode *talloc() {
+  return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *custom_strdup(char *s) {
+  char *p;
+
+  p = (char *) malloc(strlen(s) + 1);
+  if(p != NULL) {
+    strcpy(p, s);
+  }
+
+  return p;
+}
+
+// compare words and update p->match
+int compare(char *s, struct tnode *p, int num, int *found) {
+  int i;
+  char *t = p->word;
+
+  for(i = 0; *s == *t; i++, s++, t++) {
+    if(*s == '\0') {
+      return 0;
+    }
+  }
+
+  if(i >= num) {
+    *found = YES;
+    p->match = YES;
+  }
+
+  return *s - *t;
+}
+
+struct tnode *addtreex(struct tnode *p, char *w, int num, int *found) {
+  int cond;
+
+  if(p == NULL) {
+    p = talloc();
+    p->word = custom_strdup(w);
+    p->match = *found;
+    p->left = NULL;
+    p->right = NULL;
+  } else if((cond = compare(w, p, num, found)) < 0) {
+    p->left = addtreex(p->left, w, num, found);
+  } else if(cond > 0) {
+    p->right = addtreex(p->right, w, num, found);
+  }
+
+  return p;
+}
+
+int getword(char *word, int limit) {
+  int c;
+  char *w = word;
+
+  while(isspace(c = getch())) {
+    ;
+  }
+
+  if(c != EOF) {
+    *w++ = c;
+  }
+
+  if(!isalpha(c)) {
+    *w = '\0';
+    return c;
+  }
+
+  for( ; --limit > 0; w++) {
+    if(!isalnum(*w = getch())) {
+      ungetch(*w);
+      break;
+    }
+  }
+
+  *w = '\0';
+  return word[0];
+}
+
+void treeprint(struct tnode *p) {
+  if(p != NULL) {
+    treeprint(p->left);
+    printf("%s\n", p->word);
+    treeprint(p->right);
+  }
+}
+
