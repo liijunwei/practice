ea2d77e00942b76483f65fcd3a6a84938760799c
lijunwei <ljw532344863@sina.com>
2022-02-01 12:10:08 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch6-Structures/keyword-count-demo03.c
@@ -0,0 +1,24 @@
+/*
+page 121
+
+处理更一般的问题: 统计输入中所有单词的出现频次
+
+使用二叉树存储数据: 任何节点最多有两个子树, 也可能只有一个子树或0个子树
+对节点的所有操作要保证, 任何节点的左子树只包含按字典顺序小于该节点的那些单词
+右子树只包含按字典顺序大于该节点中单词的那些单词
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "../common-utils/getch-ungetch.c"
+
+
+
+int main(int argc, char const *argv[])
+{
+
+
+  return 0;
+}
