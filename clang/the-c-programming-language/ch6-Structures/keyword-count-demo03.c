616e4d18d12306bc09ff2ba5a8541a8d80611224
lijunwei <ljw532344863@sina.com>
2022-02-01 13:06:44 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/keyword-count-demo03.c
+++ clang/the-c-programming-language/ch6-Structures/keyword-count-demo03.c
@@ -31,14 +31,88 @@ struct tnode {         /* tree node       */
 };
 
 #define MAXWORD 100
-struct tnode *address(struct tnode *, char *);
-void treeprint(struct tnode *);
+struct tnode *address(struct tnode *p, char *w);
+void treeprint(struct tnode *p);
 int getword(char *, int);
 
 // 统计单词出现的频率
 int main(int argc, char const *argv[])
 {
+  struct tnode *root;
+  char word[MAXWORD];
 
+  root = NULL;
+
+  while(getword(word, MAXWORD) != EOF) {
+    if(isalpha(word[0])) {
+      root = address(root, word);
+    }
+  }
+
+  treeprint(root);
 
   return 0;
 }
+
+struct tnode *talloc() {
+
+  return NULL;
+}
+
+// TODO
+// char *strdup(char *w);
+
+struct tnode *address(struct tnode *p, char *w) {
+  int cond;
+
+  if(p == NULL) {
+    p = talloc();
+    p->word = strdup(w);
+    p->count = 1;
+    p->left = NULL;
+    p->right = NULL;
+  } else if((cond = strcmp(w, p->word)) == 0) {
+    p->count++;
+  } else if(cond < 0) {
+    p->left = address(p->left, w);
+  } else {
+    p->right = address(p->right, w);
+  }
+
+  return p;
+}
+
+int getword(char *word, int limit) {
+  int c;
+  char *w = word;
+
+  while(isspace(c = getch())) {
+    ;
+  }
+
+  if(c != EOF) {
+    *w++ = c;
+  }
+
+  if(!isalpha(c)) {
+    *w = '\0';
+    return c;
+  }
+
+  for( ; --limit > 0; w++) {
+    if(!isalnum(*w = getch())) {
+      ungetch(*w);
+      break;
+    }
+  }
+
+  *w = '\0';
+  return word[0];
+}
+
+// 打印树p
+void treeprint(struct tnode *p) {
+
+}
+
+
