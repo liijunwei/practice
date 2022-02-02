c50ce9250d3ebf7d3ce93d59c49be2a752fa354c
lijunwei <ljw532344863@sina.com>
2022-02-02 17:51:36 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/EX6-04.c
+++ clang/the-c-programming-language/ch6-Structures/EX6-04.c
@@ -50,3 +50,28 @@ void treestore(struct tnode *p) {
   }
 }
 
+/* sort list of pointers to tree nodes */
+void sortlist() {
+  int gap;
+  int i;
+  int j;
+
+  struct tnode *temp;
+
+  for (gap = ntn/2; gap > 0; gap /= 2) {
+    for (i = gap; i < ntn; i++) {
+      for (j = i - gap; j >= 0; j -= gap) {
+        if ((list[j]->count) >= (list[j+gap]->count)) {
+          break;
+        }
+
+        temp = list[j];
+        list[j] = list[j+gap];
+        list[j+gap] = temp;
+      }
+    }
+  }
+}
+
+
+
