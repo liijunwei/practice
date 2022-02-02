a99561e41a0b6eaac9f20b76431474ec0e7e4150
lijunwei <ljw532344863@sina.com>
2022-02-02 17:26:37 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/EX6-03-test.sh
+++ clang/the-c-programming-language/ch6-Structures/EX6-03-test.sh
@@ -4,7 +4,13 @@ git add .
 
 gcc -o cross-referencer ch6-Structures/EX6-03.c
 
-# cat ch6-Structures/EX6-03.c | ./cross-referencer
+echo "file: ch6-Structures/EX6-03.c"
+echo "==========================================="
+cat ch6-Structures/EX6-03.c | ./cross-referencer
+echo
+
+echo "file: ch6-Structures/EX6-03.sample.md"
+echo "==========================================="
 cat ch6-Structures/EX6-03.sample.md | ./cross-referencer
 echo
 
