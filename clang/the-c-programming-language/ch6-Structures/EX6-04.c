24bc5d41d3c56d0e00d11753bdafb31192f7e508
lijunwei <ljw532344863@sina.com>
2022-02-02 17:58:01 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/EX6-04.c
+++ clang/the-c-programming-language/ch6-Structures/EX6-04.c
@@ -34,6 +34,24 @@ int ntn = 0;                  /* number of tree nodes  */
 /* print distinct words sorted in decreasing order of frequency */
 int main(int argc, char const *argv[])
 {
+  struct tnode *root;
+  char word[MAXWORD];
+  int i;
+
+  root = NULL;
+
+  while (getword(word, MAXWORD) != EOF) {
+    if (isalpha(word[0])) {
+      root = addtree(root, word);
+    }
+  }
+
+  treestore(root);
+  sortlist();
+
+  for (i = 0; i < ntn; i++) {
+    printf("%2d:%20s\n", list[i]->count, list[i]->word);
+  }
 
   return 0;
 }
