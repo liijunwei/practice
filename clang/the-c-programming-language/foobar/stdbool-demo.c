197ea16abaa703ca573807b2b76a8d083e6c2a86
lijunwei <ljw532344863@sina.com>
2022-02-02 16:48:12 +0800

    Add demo for stdbool.h

---
+++ clang/the-c-programming-language/foobar/stdbool-demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// https://www.runoob.com/w3cnote/c-bool-true-false.html
+#include <stdbool.h>
+
+int main(void)
+{
+    bool flag = true;
+
+    if(flag) {
+      printf("flag is true");
+    } else {
+      printf("flag is false");
+    }
+
+    return 0;
+}
