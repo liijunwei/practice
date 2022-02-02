5faae84468ed99f7510f5b03f13d407d3fd200ec
lijunwei <ljw532344863@sina.com>
2022-02-02 18:37:31 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/text-substitution-demo01.c
+++ clang/the-c-programming-language/ch6-Structures/text-substitution-demo01.c
@@ -54,6 +54,7 @@ struct nlist *lookup(char *s) {
   struct nlist *np;
 
   /* standard way of iterating through a linked list */
+  /* for (ptr = head; ptr != NULL; ptr = ptr->next) */
   for (np = hashtable[hash(s)]; np != NULL; np = np->next) {
     if (strcmp(s, np->name) == 0) {
       return np;
