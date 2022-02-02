c2a56445e81dad1792144206c1e605abd518f504
lijunwei <ljw532344863@sina.com>
2022-02-02 17:56:35 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/EX6-04.c
+++ clang/the-c-programming-language/ch6-Structures/EX6-04.c
@@ -38,6 +38,29 @@ int main(int argc, char const *argv[])
   return 0;
 }
 
+struct tnode *talloc() {
+  return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+struct tnode *addtree(struct tnode *p, char *w) {
+  int cond;
+
+  if(p == NULL) {
+    p = talloc();
+    p->word = strdup(w);
+    p->count = 1;
+    p->left = NULL;
+    p->right = NULL;
+  } else if((cond = strcmp(w, p->word)) == 0) {
+    p->count++;
+  } else if(cond < 0) {
+    p->left = addtree(p->left, w);
+  } else {
+    p->right = addtree(p->right, w);
+  }
+
+  return p;
+}
 
 int comment() {
   int c;
