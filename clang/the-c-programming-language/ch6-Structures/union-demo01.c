1b4bbc85430c62b973cde1d94fab14fe034d921a
lijunwei <ljw532344863@sina.com>
2022-02-03 17:03:12 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/union-demo01.c
+++ clang/the-c-programming-language/ch6-Structures/union-demo01.c
@@ -7,11 +7,24 @@ TODO
 
 #include <stdio.h>
 
+#define NSYM 10
+
 union u_tag {
   int ival;
   float fval;
   char *svar;
 } u;
+
+struct {
+  char *name;
+  int flags;
+  int utype;
+  union {
+    int ival;
+    float fval;
+    char *svar;
+  } u;
+} systab[NSYM];
 
 int main(int argc, char const *argv[]) {
 
