8741b62e916a8825a8d38435b1fcff01e9d4f6be
lijunwei <ljw532344863@sina.com>
2022-02-03 16:27:37 +0800

    Commit manually.

--- clang/the-c-programming-language/ch6-Structures/typedef-demo01.c
+++ clang/the-c-programming-language/ch6-Structures/typedef-demo01.c
@@ -11,7 +11,15 @@ typedef int Length;
 int main(int argc, char const *argv[])
 {
   Length a = 1;
-
   printf("%d\n", a);
+
+  Length a1 = 100;
+  Length a2 = 200;
+  Length a3 = 300;
+  Length *lenths[] = {&a1, &a2, &a3};
+  printf("%d\n", *lenths[0]);
+  printf("%d\n", *lenths[1]);
+  printf("%d\n", *lenths[2]);
+
   return 0;
 }
