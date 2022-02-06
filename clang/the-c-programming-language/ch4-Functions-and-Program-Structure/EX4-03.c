34c989bfa1c02a87e667ca939090988605f69496
lijunwei <ljw532344863@sina.com>
2022-02-06 16:56:29 +0800

    Fix error for EX4-03.c

--- clang/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-03.c
+++ clang/the-c-programming-language/ch4-Functions-and-Program-Structure/EX4-03.c
@@ -40,45 +40,6 @@ double pop(void){
   }
 }
 
-#define NUMBER '0'
-
-int getop(char s[]){
-  int i;
-  int c;
-
-  while((s[0] = c = getch()) == ' ' || c == '\t'){
-    ;
-  }
-
-  s[1] = '\0';
-  if(!isdigit(c) && c != '.') {
-    return c; // 不是数
-  }
-
-  i = 0;
-  if(isdigit(c)) {
-    // 整数部分
-    while(isdigit(s[++i] = c = getch())){
-      ;
-    }
-  }
-
-  if(c == '.') {
-    // 小数部分
-    while(isdigit(s[++i] = c = getch())){
-      ;
-    }
-  }
-
-  s[i] = '\0';
-
-  if(c != EOF) {
-    ungetch(c);
-  }
-
-  return NUMBER;
-}
-
 #define MAXOP  100
 
 int main(int argc, char const *argv[])
