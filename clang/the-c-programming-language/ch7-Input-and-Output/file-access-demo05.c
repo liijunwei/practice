696e4d9e10775ea52e8b3b815b8a0dc8e6ea4c2d
lijunwei <ljw532344863@sina.com>
2022-02-06 22:18:54 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch7-Input-and-Output/file-access-demo05.c
@@ -0,0 +1,16 @@
+/*
+page 144
+
+char *fgets(char *line, int maxline, FILE *fp)
+fgets函数从fp指向的文件中读取下一个输入行(包括换行符), 并将他们存在字符数组line里, 最多可读取maxline-1个字符, 读取到的行将以'\0'结尾保存到数组里
+    通常情况下, fgets返回line, 但如果遇到了文件结尾或者发生了错误, 则返回NULL
+
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
