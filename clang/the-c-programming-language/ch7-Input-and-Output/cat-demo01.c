0fc1c2a5820015b5f7f4a8eaf016b8a5ec154655
lijunwei <ljw532344863@sina.com>
2022-02-06 18:04:58 +0800

    Commit manually.

--- clang/the-c-programming-language/ch7-Input-and-Output/cat-demo01.c
+++ clang/the-c-programming-language/ch7-Input-and-Output/cat-demo01.c
@@ -12,7 +12,33 @@ int fprintf(FILE *fp, char *fmt, ...)
 
 #include <stdio.h>
 
+void filecopy(FILE *ifp, FILE *ofp);
+
+/* 连接多个文件(V1) */
 int main(int argc, char const *argv[]) {
+  FILE *fp;
+
+  if (argc == 1) {
+    filecopy(stdin, stdout);
+  } else {
+    while (--argc > 0) {
+      if ((fp = fopen(*++argv, "r")) == NULL) {
+        printf("cat: can't open %s\n", *argv);
+        return 1;
+      } else {
+        filecopy(fp, stdout);
+        fclose(fp);
+      }
+    }
+  }
 
   return 0;
+}
+
+void filecopy(FILE *ifp, FILE *ofp) {
+  int c;
+
+  while ((c = getc(ifp)) != EOF) {
+    putc(c, ofp);
+  }
 }
