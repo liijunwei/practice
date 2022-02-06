9754095d3602063a0dbee7595eb9295121aebfd8
lijunwei <ljw532344863@sina.com>
2022-02-06 16:32:07 +0800

    Commit manually.

--- clang/the-c-programming-language/ch7-Input-and-Output/scanf-demo02.c
+++ clang/the-c-programming-language/ch7-Input-and-Output/scanf-demo02.c
@@ -6,9 +6,15 @@ page 139
 
 #include <stdio.h>
 
+// gcc -g ch7-Input-and-Output/scanf-demo02.c && echo 1.1 2.2 3.3 | ./a.out
 int main(int argc, char const *argv[])
 {
-  
+  double sum = 0;
+  double v;
+
+  while (scanf("%lf", &v) == 1) {
+    printf("\t%.2f\n", sum += v);
+  }
 
   return 0;
 }
