dafd319f3ebcbc81f773841ba93bf6ce9c6fbb1b
lijunwei <ljw532344863@sina.com>
2022-02-06 16:26:59 +0800

    Commit manually.

--- clang/the-c-programming-language/ch7-Input-and-Output/sscanf-demo01.c
+++ clang/the-c-programming-language/ch7-Input-and-Output/sscanf-demo01.c
@@ -17,10 +17,15 @@ int main(int argc, char const *argv[])
   printf("string1(before): %s\n", string1);
   printf("string2(before): %s\n", string2);
 
-  char sentense[100] = "today is Sunday and a Sunny day";
+  char sentense[100] = "today is Sunday and a Sunny day, this is the last day(7th) of sping festival holiday";
   sscanf(sentense, "today is %s and a %s day", string1, string2);
   printf("string1(after):  %s\n", string1);
   printf("string2(after):  %s\n", string2);
+
+  int number;
+  printf("number(before): %d\n", number);
+  sscanf(sentense, "today is Sunday and a Sunny day, this is the last day(%dth) of sping festival holiday", &number);
+  printf("number(after):  %d\n", number);
 
   return 0;
 }
