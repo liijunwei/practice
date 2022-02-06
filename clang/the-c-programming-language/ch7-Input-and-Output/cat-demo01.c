0f06f852f340cd11a3913698f9a7120025c70842
lijunwei <ljw532344863@sina.com>
2022-02-06 18:00:51 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch7-Input-and-Output/cat-demo01.c
@@ -0,0 +1,18 @@
+/*
+page 142
+
+int fscanf(FILE *fp, char *fmt, ...)
+int fprintf(FILE *fp, char *fmt, ...)
+
+编写将多个文件连接起来的cat程序
+该程序的设计思路和其他许多程序类似:
+  如果有命令行参数, 参数将被解释为文件名, 并按顺序逐个处理
+  如果没有参数, 则处理标准输入
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
