a4a4cbf686d344919dbcd51294be243284e42bfa
lijunwei <ljw532344863@sina.com>
2022-02-06 17:35:37 +0800

    Commit manually.

--- clang/the-c-programming-language/ch7-Input-and-Output/file-access-demo01.c
+++ clang/the-c-programming-language/ch7-Input-and-Output/file-access-demo01.c
@@ -10,5 +10,8 @@ stdio.h中定义了结构FILE
 int main(int argc, char const *argv[]) {
   FILE *fp;
 
+  fp = fopen("$HOME/practice/clang/the-c-programming-language/ch7-Input-and-Output/file-access-demo01.c", "r");
+  fclose(fp);
+
   return 0;
 }
