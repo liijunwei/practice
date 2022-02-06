65098126f8010f690f3f3fd0db6631e5f38093f7
lijunwei <ljw532344863@sina.com>
2022-02-06 23:14:06 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch7-Input-and-Output/EX7-06-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git add .
+
+gcc ch7-Input-and-Output/EX7-06.c
+
+./a.out
+
+./a.out demo.c
+
+./a.out demo.c README.md
+
+./a.out demo.c README.md README.md
+
+./a.out ch7-Input-and-Output/EX7-06-sample01.md ch7-Input-and-Output/EX7-06-sample02.md
+
+
+
