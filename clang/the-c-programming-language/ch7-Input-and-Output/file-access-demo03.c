e2ae44461357c5ef9a5954284e996b4e8fabe3fc
lijunwei <ljw532344863@sina.com>
2022-02-06 17:55:29 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch7-Input-and-Output/file-access-demo03.c
@@ -0,0 +1,25 @@
+/*
+page 141
+
+ref:
+  ch7-Input-and-Output/file-access-demo02.c
+
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[]) {
+  FILE *fp;
+  fp = fopen("./ch7-Input-and-Output/file-access-demo01.c", "r");
+
+  int c;
+
+  while ((c = getc(fp)) != EOF) {
+    putc(toupper(c), stdout);
+  }
+
+  fclose(fp);
+
+  return 0;
+}
