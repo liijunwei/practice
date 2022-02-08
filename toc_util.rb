daccfeb20dd81c5a034359531ac47a4df971938d
lijunwei <ljw532344863@sina.com>
2022-02-08 15:03:43 +0800

    Commit manually.

--- toc_util.rb
+++ toc_util.rb
@@ -16,6 +16,10 @@ def make_toc_directories(list)
   list.each {|dir_name| FileUtils.mkdir_p dir_name}
 end
 
+def touch_note_file(list)
+  list.each {|dir_name| FileUtils.touch "#{dir_name}/note01.md"}
+end
+
 toc = <<-EOF
 UNIX PROCESSES
 Introduction
@@ -47,4 +51,4 @@ EOF
 dir_base = "/Users/lijunwei/practice/ruby/working-with-unix-processes"
 toc_list = get_toc_directories(dir_base, toc)
 make_toc_directories(toc_list)
-
+touch_note_file(toc_list)
