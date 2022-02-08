32e0f557db21b5d82f8949da2c41e307425a088b
lijunwei <ljw532344863@sina.com>
2022-02-08 09:59:25 +0800

    Commit manually.

--- clang/the-c-programming-language/ch7-Input-and-Output/EX7-07-test.sh
+++ clang/the-c-programming-language/ch7-Input-and-Output/EX7-07-test.sh
@@ -5,13 +5,29 @@ git add .
 gcc ch7-Input-and-Output/EX7-07.c
 
 echo "find main"
-./a.out main ch7-Input-and-Output/EX7-07.c
+./a.out -n main ch7-Input-and-Output/EX7-07.c
 echo
 
 echo "find break"
-./a.out break ch7-Input-and-Output/EX7-07.c
+./a.out -n break ch7-Input-and-Output/EX7-07.c
 echo
 
 echo "find fclose"
 ./a.out fclose ch7-Input-and-Output/EX7-07.c
+echo
+
+echo "find fclose"
+./a.out -n fclose ch7-Input-and-Output/EX7-07.c
+echo
+
+echo "find page"
+./a.out -n page ch7-Input-and-Output/EX7-07.c
+echo
+
+echo "find include"
+./a.out -n include ch7-Input-and-Output/EX7-07.c
+echo
+
+echo "find MAXLINE"
+./a.out -n MAXLINE ch7-Input-and-Output/EX7-07.c
 echo
--- clang/the-c-programming-language/ch7-Input-and-Output/EX7-07.c
+++ clang/the-c-programming-language/ch7-Input-and-Output/EX7-07.c
@@ -5,6 +5,8 @@ ch5-Pointers-and-Arrays/grep-demo2.c
 修改第5章的模式查找程序, 是他从一个命名文件的集合中读取输入(有文件名参数时), 如果没有文件名参数, 则从标准输入中读取输入;
 当发现一个匹配行时, 是否应该将相应的文件名打印出来?
 
+TODO fixbug: off by one
+
 */
 
 #include <stdio.h>
