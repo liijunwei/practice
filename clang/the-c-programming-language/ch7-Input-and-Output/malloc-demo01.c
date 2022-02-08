0a18a633739a768dd20561afaa8c31315ac1c229
lijunwei <ljw532344863@sina.com>
2022-02-08 12:44:40 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch7-Input-and-Output/malloc-demo01.c
@@ -0,0 +1,22 @@
+/*
+page 147
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* man 3 malloc */
+int main(int argc, char const *argv[]) {
+  int *p;
+  p = (int *) malloc(sizeof(int));
+  free(p);
+
+  char *pa;
+  int arr_size = 10;
+  pa = (char *) calloc(arr_size, sizeof(char));
+  free(pa);
+
+  return 0;
+}
