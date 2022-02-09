24e02808ed3daa086535df91a8c43c500c7954b3
lijunwei <ljw532344863@sina.com>
2022-02-09 08:23:58 +0800

    Add question

--- clang/the-c-programming-language/ch8-The-UNIX-System-Interface/read-demo01.c
+++ clang/the-c-programming-language/ch8-The-UNIX-System-Interface/read-demo01.c
@@ -6,16 +6,18 @@ page 150
 #include <stdio.h>
 #include <unistd.h>
 
-#define BUFSIZE 100
+ /* TODO 问题: BUFSIZ 为1 和 为 100 有什么区别呢? */
 
 int main(int argc, char const *argv[]) {
-  char buf[BUFSIZE];
+  char buf[BUFSIZ]; /* BUFSIZ 在 <stdio.h> 中定义 */
   int n;
+
+  printf("BUFSIZ %d\n", BUFSIZ);
 
   int fd_stdin  = 0;
   int fd_stdout = 1;
 
-  while ((n = read(fd_stdin, buf, BUFSIZE)) > 0) {
+  while ((n = read(fd_stdin, buf, BUFSIZ)) > 0) {
     write(fd_stdout, buf, n);
   }
 
