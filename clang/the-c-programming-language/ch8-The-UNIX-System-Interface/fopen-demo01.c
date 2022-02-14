d6cf8f9c2729cb0a08cbd5e62b4998de7f31cdcb
lijunwei <ljw532344863@sina.com>
2022-02-14 21:50:57 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch8-The-UNIX-System-Interface/fopen-demo01.c
@@ -0,0 +1,16 @@
+/*
+page 155
+
+man 3 fopen
+*/
+
+#define NULL     0
+#define EOF      (-1)
+#define BUFSIZ   1024
+#define OPEN_MAX 20
+
+int main(int argc, char const *argv[]) {
+
+
+  return 0;
+}
