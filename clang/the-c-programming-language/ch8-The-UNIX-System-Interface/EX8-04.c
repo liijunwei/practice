d718d3b7e0206472a5e18f55c4dcb76670a7e24e
lijunwei <ljw532344863@sina.com>
2022-02-19 08:29:09 +0800

    Commit manually.

--- clang/the-c-programming-language/ch8-The-UNIX-System-Interface/EX8-04.c
+++ clang/the-c-programming-language/ch8-The-UNIX-System-Interface/EX8-04.c
@@ -1,6 +1,10 @@
 /*
-page xx
+page 157
 
+标准库函数 `int fseek(FILE *fp, lone offset, int origin)` 类似于函数 `lseek`, 所不同的是, 该函数中的fp
+是一个文件指针而不是文件描述符, 且返回值是一个int类型的状态而非位置值.
+
+编写函数fseek, 并确保该函数与库中其他函数使用的缓冲能够协同工作
 
 */
 
