d91303a201ca894fe425cfc6bc926b7555cb90f2
lijunwei <ljw532344863@sina.com>
2022-03-03 22:27:16 +0800

    Commit manually.

---
+++ clang/the-c-programming-language/ch6-Structures/struct-demo09.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct point1 {
+  int x;
+  int y;
+};
+
+void print_point1(struct point1 *ps) {
+  printf("x: %d y: %d sum: %d\n", ps->x, ps->y, ps->x + ps->y);
+}
+
+int main(int argc, char const *argv[])
+{
+  struct point1 d = {100, 200};
+  print_point1(&d);
+
+  return 0;
+}
