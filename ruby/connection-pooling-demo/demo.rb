7310aea8c26583ecb1e25519c9a7f7b86d3e3f5c
lijunwei <ljw532344863@sina.com>
2022-03-04 00:05:21 +0800

    Add mysql connection pooling demo.

--- ruby/connection-pooling-demo/demo.rb
+++ ruby/connection-pooling-demo/demo.rb
@@ -1,17 +1,25 @@
 require 'mysql2'
+require 'connection_pool'
 require 'pry'
 
-# https://github.com/brianmario/mysql2/#connection-options
-client = Mysql2::Client.new(
-  :host => "localhost",
-  :username => ENV['username'],
-  :password => ENV['password'],
-  :database => ENV['database'],
-  :encoding => 'utf8',
-  :init_command => "use #{ENV['database']}"
-)
+database = ENV['database']
 
-result = client.query("SELECT * FROM reasons limit 10")
+# https://github.com/mperham/connection_pool/
+pool = ConnectionPool.new(size: 5, timeout: 5) {
+  # https://github.com/brianmario/mysql2/#connection-options
+  Mysql2::Client.new(
+    host:         "localhost",
+    username:     ENV['username'],
+    password:     ENV['password'],
+    database:     database,
+    encoding:     'utf8',
+    init_command: "use #{database}"
+  )
+}
+
+result = pool.with { |conn|
+  conn.query("SELECT * FROM reasons limit 10")
+}
 
 binding.pry
 puts result.to_a
