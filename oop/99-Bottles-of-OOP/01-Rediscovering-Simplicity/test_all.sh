0e6488f9b111421c82d8c1572e0e8f96f129bfc2
lijunwei <ljw532344863@sina.com>
2022-03-24 23:09:38 +0800

    Commit manually.

---
+++ oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/test_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# practice && bash oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/test_all.sh
+
+basedir=$(dirname -- "${BASH_SOURCE[0]}")
+current_dir=$(pwd)
+
+dirs=(
+1.1.1.Incomprehensibly-Concise
+1.1.2.Speculatively-General
+1.1.3.Concretely-Abstract
+1.1.4.Shameless-Green
+)
+
+for dir in ${dirs[@]}; do
+  cd "$basedir/$dir"
+  echo "Run test for $dir"
+  echo "============================"
+  rake
+  echo
+  cd $current_dir
+done
+
