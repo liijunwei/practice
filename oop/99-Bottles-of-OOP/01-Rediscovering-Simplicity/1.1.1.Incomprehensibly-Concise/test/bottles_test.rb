20faf787b784107e8309be65f3ceddc44f3931b5
lijunwei <ljw532344863@sina.com>
2022-03-24 22:59:48 +0800

    Code cleanup.

--- oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/1.1.1.Incomprehensibly-Concise/test/bottles_test.rb
+++ oop/99-Bottles-of-OOP/01-Rediscovering-Simplicity/1.1.1.Incomprehensibly-Concise/test/bottles_test.rb
@@ -3,8 +3,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/bottles'
 
-# rvm use 2.5.1
-# ruby test/bottles_test.rb
 class BottlesTest < Minitest::Test
   def test_the_whole_song
     filepath = File.expand_path(".", '../../whole-lyric.md')
