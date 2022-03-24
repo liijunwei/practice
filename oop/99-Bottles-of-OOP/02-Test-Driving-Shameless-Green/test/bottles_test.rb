bb95446dd80fac982a5eeb9aceda7476efb64901
lijunwei <ljw532344863@sina.com>
2022-03-24 08:45:59 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/02-Test-Driving-Shameless-Green/test/bottles_test.rb
+++ oop/99-Bottles-of-OOP/02-Test-Driving-Shameless-Green/test/bottles_test.rb
@@ -1,10 +1,6 @@
-gem 'minitest', '~>5'
 require 'minitest/autorun'
-require 'minitest/pride'
 require_relative '../lib/bottles'
 
-# rvm use 2.5.1
-# ruby test/bottles_test.rb
 class BottlesTest < Minitest::Test
   def test_the_first_verse
     expected =
