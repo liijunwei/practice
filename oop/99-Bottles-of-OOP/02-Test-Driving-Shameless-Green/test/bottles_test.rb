6bcce0ee08fee78a4519a10c4d31f48627bb7e6a
lijunwei <ljw532344863@sina.com>
2022-03-24 08:37:56 +0800

    Add test case for verse(1) and make it pass.

--- oop/99-Bottles-of-OOP/02-Test-Driving-Shameless-Green/test/bottles_test.rb
+++ oop/99-Bottles-of-OOP/02-Test-Driving-Shameless-Green/test/bottles_test.rb
@@ -7,7 +7,8 @@ require_relative '../lib/bottles'
 # ruby test/bottles_test.rb
 class BottlesTest < Minitest::Test
   def test_the_first_verse
-    expected = "99 bottles of beer on the wall, " +
+    expected =
+    "99 bottles of beer on the wall, " +
     "99 bottles of beer.\n" +
     "Take one down and pass it around, " +
     "98 bottles of beer on the wall.\n"
@@ -16,7 +17,8 @@ class BottlesTest < Minitest::Test
   end
 
   def test_another_verse
-    expected = "3 bottles of beer on the wall, " +
+    expected =
+    "3 bottles of beer on the wall, " +
     "3 bottles of beer.\n" +
     "Take one down and pass it around, " +
     "2 bottles of beer on the wall.\n"
@@ -25,9 +27,20 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_2
-    expected = "2 bottles of beer on the wall, " +
+    expected =
+    "2 bottles of beer on the wall, " +
     "2 bottles of beer.\n" +
-    "Take one down and pass it around, " + "1 bottle of beer on the wall.\n"
+    "Take one down and pass it around, " +
+    "1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse_1
+    expected =
+    "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Take one down and pass it around, " +
+    "no more bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(1)
   end
 end
