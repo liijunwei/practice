c549b0e2b0cfd7e8a342937ea1a38e91466ca13e
lijunwei <ljw532344863@sina.com>
2022-03-27 18:18:24 +0800

    Commit manually.

--- toc_util_pdf.rb
+++ toc_util_pdf.rb
@@ -4,4 +4,13 @@ require 'pdf-reader'
 # not quite handy :(
 
 reader = PDF::Reader.new("#{ENV['HOME']}/Desktop/99-bottles.pdf")
-reader.pages[2..5].each {|page| puts page.text}
+reader.pages[1..5].map {|page| page.text.to_s.split("\n").reject(&:blank?)}.flatten
+
+
+toc = reader.pages[1..5]
+            .map {|page| page.text.to_s.split("\n")
+            .reject(&:blank?)}
+            .flatten
+            .reject {|line| line.match(/\A\s+\d\z/)}
+
+puts toc
