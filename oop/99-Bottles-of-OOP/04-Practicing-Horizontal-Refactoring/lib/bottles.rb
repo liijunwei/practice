618808c2602293752c766df8347a3c855723cb31
lijunwei <ljw532344863@sina.com>
2022-04-07 22:45:44 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/04-Practicing-Horizontal-Refactoring/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/04-Practicing-Horizontal-Refactoring/lib/bottles.rb
@@ -8,18 +8,10 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
-    else
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
-    end
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+    "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
   end
 
   def container(number)
