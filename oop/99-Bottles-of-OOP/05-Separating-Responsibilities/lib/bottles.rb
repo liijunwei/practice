f9c145025d115810234f68dc4d1495b36546798a
lijunwei <ljw532344863@sina.com>
2022-04-11 23:39:09 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/05-Separating-Responsibilities/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/05-Separating-Responsibilities/lib/bottles.rb
@@ -1,5 +1,44 @@
 class BottleNumber
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
 
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  # TODO: too abstract to understand
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 end
 
 class Bottles
