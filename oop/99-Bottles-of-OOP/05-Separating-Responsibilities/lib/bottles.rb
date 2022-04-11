3598694a0e64df4e5cf9b080a9c8adefd5ed72cb
lijunwei <ljw532344863@sina.com>
2022-04-11 23:47:42 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/05-Separating-Responsibilities/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/05-Separating-Responsibilities/lib/bottles.rb
@@ -64,12 +64,12 @@ class Bottles
   end
 
   def container(number)
-    BottleNumber.new(number).container(number)
     if number == 1
       "bottle"
     else
       "bottles"
     end
+    BottleNumber.new(number).container(number)
   end
 
   def pronoun(number)
