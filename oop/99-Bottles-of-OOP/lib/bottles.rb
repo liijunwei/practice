a11331ed561eee872206f23225bd5de17b804af8
lijunwei <ljw532344863@sina.com>
2022-04-16 21:17:33 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/lib/bottles.rb
@@ -66,10 +66,8 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = (number == 0) ? BottleNumber0.new(number) : BottleNumber.new(number)
-
-    succ = bottle_number.successor
-    next_bottle_number = (succ == 0) ? BottleNumber0.new(succ) : BottleNumber.new(succ)
+    bottle_number      = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
