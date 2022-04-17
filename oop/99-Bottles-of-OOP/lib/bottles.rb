f526ff6e74f1871703801875adf871d55b103226
lijunwei <ljw532344863@sina.com>
2022-04-17 23:33:37 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/lib/bottles.rb
@@ -5,6 +5,17 @@ class BottleNumber
     @number = number
   end
 
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
+  end
+
   def container
     "bottles"
   end
