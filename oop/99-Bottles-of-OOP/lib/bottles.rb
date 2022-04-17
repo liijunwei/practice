3cddfa1ece74e8b18b278224982994ebed4b9d99
lijunwei <ljw532344863@sina.com>
2022-04-18 00:08:46 +0800

    Commit manually.

--- oop/99-Bottles-of-OOP/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/lib/bottles.rb
@@ -11,6 +11,8 @@ class BottleNumber
       BottleNumber0
     when 1
       BottleNumber1
+    when 6
+      BottleNumber6
     else
       BottleNumber
     end.new(number)
@@ -66,6 +68,13 @@ class BottleNumber1 < BottleNumber
 end
 
 class BottleNumber6 < BottleNumber
+  def quantity
+    "1"
+  end
+
+  def container
+    "six-pack"
+  end
 end
 
 class Bottles
