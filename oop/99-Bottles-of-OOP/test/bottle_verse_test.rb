af4191982cf633cf2990ba0aa60d2c90c840a8e9
lijunwei <ljw532344863@sina.com>
2022-05-01 22:16:20 +0800

    Commit automatically by watch.

--- oop/99-Bottles-of-OOP/test/bottle_verse_test.rb
+++ oop/99-Bottles-of-OOP/test/bottle_verse_test.rb
@@ -3,6 +3,12 @@ require_relative '../lib/bottles'
 
 class BottleVerseTest < Minitest::Test
   def test_the_first_verse
-    expected = 
+    expected =
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n"
+
+    assert_equal expected, BottleVerse.lyrics(99)
   end
 end
