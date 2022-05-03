0906d231e61629a98ae9fe1291ba138c9d4c709e
lijunwei <ljw532344863@sina.com>
2022-05-03 22:42:39 +0800

    Inject number to CountdownSong.

--- oop/99-Bottles-of-OOP/lib/bottles.rb
+++ oop/99-Bottles-of-OOP/lib/bottles.rb
@@ -119,14 +119,16 @@ class BottleVerse
 end
 
 class CountdownSong
-  attr_reader :verse_template
+  attr_reader :verse_template, :max, :min
 
-  def initialize(verse_template: BottleVerse)
+  def initialize(verse_template: BottleVerse, max: 99, min: 0)
     @verse_template = verse_template
+    @max = max
+    @min = min
   end
 
   def song
-    verses(99, 0)
+    verses(max, min)
   end
 
   def verses(starting, ending)
