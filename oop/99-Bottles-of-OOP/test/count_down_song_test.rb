568a139cb3e6cd0526d714b1b198b448a4885cce
lijunwei <ljw532344863@sina.com>
2022-05-03 22:36:40 +0800

    Commit automatically by watch.

--- oop/99-Bottles-of-OOP/test/count_down_song_test.rb
+++ oop/99-Bottles-of-OOP/test/count_down_song_test.rb
@@ -25,10 +25,6 @@ class CountdownSongTest < Minitest::Test
     assert_equal expected, CountdownSong.new(verse_template: VerseFake).verses(99, 97)
   end
 
-  # OK 没看懂 page 265 讲的 为什么不需要测试这 2,1,0 这几个特殊的
-  # 因为对CountdownSong来说, 他不知道, 或者说他不关心有没有特殊的情况吗?
-  # 对, CountdownSong 里面, 没有特殊情况, 不一样的地方交给 BottleNumber.for 工厂处理了
-
   def test_the_whole_song
     filepath = File.expand_path(".", "./whole-lyric-six-pack.md")
     expected = File.read(filepath)
