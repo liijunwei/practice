8ebf8d5680ffc28209c7ddb3d14f71d6253108fb
lijunwei <ljw532344863@sina.com>
2022-05-14 17:14:23 +0800

    Commit automatically by watch.

---
+++ ruby/metaprogramming/blocks/monitor_framework/redflag_v2.rb
@@ -0,0 +1,38 @@
+# run this script every 1 seconds to check all events
+# watch -d -n1 "ruby ruby/metaprogramming/blocks/monitor_framework/redflag_v2.rb"
+
+# page 98
+
+require 'redis'
+
+def redis
+  @instance ||= Redis.new
+end
+
+
+lambda {
+  setups = []
+  events = {}
+
+  Kernel.send :define_method, :event do |name, &block|
+    @events[name] = block
+  end
+
+}.call
+
+script_dir = File.dirname(__FILE__)
+Dir.glob("#{script_dir}/events/*events.rb").each do |file|
+  @setups = []
+  @events = {}
+  load file
+
+  @events.each_pair do |name, event_block|
+    env = Object.new
+    @setups.each do |setup|
+      env.instance_eval &setup
+    end
+
+    puts "#{Time.now} [WARN] #{name}" if env.instance_eval &event_block
+  end
+end
+
