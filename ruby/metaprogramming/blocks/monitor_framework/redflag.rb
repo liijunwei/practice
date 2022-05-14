9c74353c21fd17dca9b4dd1371e51c6060918c26
lijunwei <ljw532344863@sina.com>
2022-05-14 16:47:36 +0800

    Commit automatically by watch.

---
+++ ruby/metaprogramming/blocks/monitor_framework/redflag.rb
@@ -0,0 +1,12 @@
+# run this script every 1 seconds to check all events
+# watch -d -n1 "ruby ruby/metaprogramming/blocks/monitor_framework/redflag.rb"
+
+def event(name)
+  puts "#{Time.now} ALERT: #{name}" if yield
+end
+
+# require 'pry'; binding.pry
+
+script_dir = File.dirname(__FILE__)
+Dir.glob("#{script_dir}/events/*events.rb").each { |file| load file }
+
