d21597bd7b25cdda5787b4a520ba47d3b9fbe984
lijunwei <ljw532344863@sina.com>
2022-05-14 11:26:08 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/blocks/closure.rb
+++ ruby/metaprogramming/blocks/closure.rb
@@ -8,6 +8,6 @@ x = "Hello"
 p my_method { |y| "#{x}, #{y} world" }
 
 # my_method 中定义的 x 对块来说是不可见的
-#     块 看不见 my_method 里定义的x
-#     块 能看见 和它平级定义的 x
+#     块 看不见 my_method 里定义的x(Goodbye)
+#     块 能看见 和它平级的 x(Hello)
 
---
+++ ruby/metaprogramming/blocks/scopes.rb
@@ -0,0 +1,8 @@
+v1 = 1
+p "line:#{__LINE__} #{local_variables}"
+
+class MyClass
+  v2 = 2
+  p local_variables
+
+end
