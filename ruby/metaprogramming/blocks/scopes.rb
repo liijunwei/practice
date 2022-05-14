bab06eabfad828dd582b9d148715c7498a7c26d9
lijunwei <ljw532344863@sina.com>
2022-05-14 11:28:06 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/blocks/scopes.rb
+++ ruby/metaprogramming/blocks/scopes.rb
@@ -1,8 +1,23 @@
 v1 = 1
-p "line:#{__LINE__} #{local_variables}"
+puts "line:#{__LINE__} #{local_variables}"
 
 class MyClass
   v2 = 2
-  p local_variables
+  puts "line:#{__LINE__} #{local_variables}"
 
+  def my_method
+    v3 = 3
+    puts "line:#{__LINE__} #{local_variables}"
+  end
+
+  puts "line:#{__LINE__} #{local_variables}"
 end
+
+puts "================"
+obj = MyClass.new
+obj.my_method
+obj.my_method
+puts "line:#{__LINE__} #{local_variables}"
+
+
+
