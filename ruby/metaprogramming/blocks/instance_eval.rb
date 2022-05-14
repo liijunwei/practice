5c126b657545819971f53b6d30647b95c9937ac9
lijunwei <ljw532344863@sina.com>
2022-05-14 16:22:29 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/blocks/instance_eval.rb
+++ ruby/metaprogramming/blocks/instance_eval.rb
@@ -10,5 +10,10 @@ obj.instance_eval do
   p @v
 end
 
+puts "=" * 30
+
 v = 2
 obj.instance_eval { @v = v }
+obj.instance_eval { p @v }
+
+# 可以把传递给 `instance_eval()` 方法的块称为一个上下文探针(Context Probe), 因为它就像一个深入到对象中的代码片段, 对其进行操作
