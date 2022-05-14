c43c73cf2397814dd4035c919d886a6f7cee8afc
lijunwei <ljw532344863@sina.com>
2022-05-14 16:53:54 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/blocks/monitor_framework/redflag.rb
+++ ruby/metaprogramming/blocks/monitor_framework/redflag.rb
@@ -4,15 +4,29 @@
 # page 96
 
 def event(name, &block)
-  puts "#{Time.now} ALERT: #{name}" if yield
-  
+  # puts "#{Time.now} ALERT: #{name}" if yield
+  @events[name] = block
 end
 
-def setup
+def setup(&block)
+  @setups << block
 end
 
 # require 'pry'; binding.pry
 
 script_dir = File.dirname(__FILE__)
-Dir.glob("#{script_dir}/events/*events.rb").each { |file| load file }
+Dir.glob("#{script_dir}/events/*events.rb").each do |file|
+  @setups = []
+  @events = []
+  load file
+
+  @events.each_pair do |name, event|
+    env = Object.new
+    @setups.each do |setup|
+      env.instance_eval &setup
+    end
+
+    puts 
+  end
+end
 
