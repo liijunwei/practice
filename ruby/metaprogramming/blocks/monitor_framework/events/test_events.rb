bf23af84c612f0fb6f0f6865cce514a139873df7
lijunwei <ljw532344863@sina.com>
2022-05-14 16:49:43 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/blocks/monitor_framework/events/test_events.rb
+++ ruby/metaprogramming/blocks/monitor_framework/events/test_events.rb
@@ -1,7 +1,18 @@
-event "an event that always happens" do
-  true
+event "the sky is falling" do
+  @sky_height < 300
 end
 
-event "an event that never happens" do
-  false
+event "it's getting closer" do
+  @sky_height < @mountains_height
 end
+
+setup do
+  puts "Setting up sky"
+  @sky_height = 100
+end
+
+setup do
+  puts "Setting up mountains"
+  @mountains_height = 200
+end
+
