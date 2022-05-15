b792cbef674f18c546000c2fc5b7eeeb7d1fc768
lijunwei <ljw532344863@sina.com>
2022-05-15 16:38:27 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/methods/bug_hunt.rb
+++ ruby/metaprogramming/methods/bug_hunt.rb
@@ -1,6 +1,8 @@
 class Roulette
   def method_missing(name, *args)
     person = name.to_s.capitalize
+    number = 0
+
     3.times do
       number = rand(10) + 1
       puts "#{number}..."
@@ -14,4 +16,8 @@ number_of = Roulette.new
 puts number_of.bob
 puts number_of.frank
 
+# why "stack level too deep (SystemStackError)" ?
+# because number variable(method) is out of scope, and is missing all the way down
 
+# how to fix?
+# initialize `number` before the loop
