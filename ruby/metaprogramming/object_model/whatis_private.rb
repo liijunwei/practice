af29ca04a0be3b8229f7292aafefc4cb48bcd922
lijunwei <ljw532344863@sina.com>
2022-05-15 15:57:02 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/object_model/whatis_private.rb
+++ ruby/metaprogramming/object_model/whatis_private.rb
@@ -10,7 +10,8 @@ class C
   end
 end
 
+# https://www.ruby-lang.org/en/news/2019/12/25/ruby-2-7-0-released/
 # when ruby version <  2.7 =>  private method `method_private' called(NoMethodError)
-# when ruby version >= 2.7 =>  private method `method_private' called(NoMethodError)
+# when ruby version >= 2.7 =>  OK
 p C.new.method_public
 
