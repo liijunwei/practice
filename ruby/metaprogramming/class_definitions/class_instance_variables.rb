855934a1e0a4b4cb6fefeec915bdf31e22312811
lijunwei <ljw532344863@sina.com>
2022-05-15 12:55:53 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/class_instance_variables.rb
+++ ruby/metaprogramming/class_definitions/class_instance_variables.rb
@@ -2,6 +2,24 @@
 
 class MyClass
   @my_var = 1
+
+  def self.read
+    @my_var
+  end
+
+  def write
+    @my_var = 2
+  end
+
+  def read
+    @my_var
+  end
 end
 
-p MyClass
+obj = MyClass.new
+
+obj.write
+p obj.read
+
+p MyClass.read
+
