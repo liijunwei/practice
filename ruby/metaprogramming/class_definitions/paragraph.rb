6771c67e765442b3beaebaa6d84e3c2bdfaf0056
lijunwei <ljw532344863@sina.com>
2022-05-18 09:11:39 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/paragraph.rb
+++ ruby/metaprogramming/class_definitions/paragraph.rb
@@ -1,5 +1,31 @@
 # page 111
 
 class Paragraph
+  def initialize(text)
+    @text = text
+  end
 
+  def title?
+    @text.upcase == @text
+  end
+
+  def reverse
+    @text.reverse
+  end
+
+  def upcase
+    @text.upcase
+  end
 end
+
+require 'test/unit'
+
+class TestParagraph < Test::Unit::TestCase
+  def test_upcase
+    Loan.instance_eval { @time_class = FakeTime }
+    loan = Loan.new("War and Peace")
+
+    assert_equal "WAR AND PEACE loaned on 2022-05-18 09:02:28 +0800", loan.to_s
+  end
+end
+
