7284fbd5e7deb029b602ea3e1035690a8dff67fa
lijunwei <ljw532344863@sina.com>
2022-05-19 20:45:31 +0800

    Commit manually.

--- ruby/design-pattern/null-object-pattern.rb
+++ ruby/design-pattern/null-object-pattern.rb
@@ -1,5 +1,7 @@
 # https://en.wikipedia.org/wiki/Null_object_pattern#:~:text=In%20object%2Doriented%20computer%20programming,of%20Program%20Design%20book%20series.
 class Dog
+  # return object if found
+  # return null object if not found, do not retur nil or raise exception
   def self.find(id)
     id == 1 ? new : NilAnimal.new
   end
