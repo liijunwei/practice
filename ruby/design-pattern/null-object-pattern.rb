7ac55306c853acf07828e1b44687bee11cd701a2
lijunwei <ljw532344863@sina.com>
2022-05-19 20:44:35 +0800

    Commit manually.

--- ruby/design-pattern/null-object-pattern.rb
+++ ruby/design-pattern/null-object-pattern.rb
@@ -1,18 +1,21 @@
 # https://en.wikipedia.org/wiki/Null_object_pattern#:~:text=In%20object%2Doriented%20computer%20programming,of%20Program%20Design%20book%20series.
 class Dog
+  def self.find(id)
+    id == 1 ? new : NilAnimal.new
+  end
+
   def sound
     "bark"
   end
 end
- 
+
 class NilAnimal
-  def sound(*); end
+  def sound
+  end
 end
 
-def get_animal(animal=NilAnimal.new)
-  animal
-end
-
-p get_animal(Dog.new).sound
-p get_animal.sound
+dog = Dog.find(1)
+p dog.sound
+dog = Dog.find(2)
+p dog.sound
 
