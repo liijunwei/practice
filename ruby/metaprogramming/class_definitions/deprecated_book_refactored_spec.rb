fe4df3f7802da7af561a416b6546553568497891
lijunwei <ljw532344863@sina.com>
2022-05-19 23:34:21 +0800

    Add expect to warnings.

--- ruby/metaprogramming/class_definitions/deprecated_book_refactored_spec.rb
+++ ruby/metaprogramming/class_definitions/deprecated_book_refactored_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe BookRefactored do
   describe "#GetTitle" do
     it "responds to GetTitle" do
       expect(subject.GetTitle).to eq("Different Seasons")
+      expect { subject.GetTitle }.to output("Warning: GetTitle() is deprecated. Please use title().\n").to_stderr
     end
 
     it "responds to title" do
@@ -15,6 +16,7 @@ RSpec.describe BookRefactored do
   describe "#title2" do
     it "responds to title2" do
       expect(subject.title2).to eq("Rita Heyworth and Shawshank Redemption")
+      expect { subject.title2 }.to output("Warning: title2() is deprecated. Please use subtitle().\n").to_stderr
     end
 
     it "responds to subtitle" do
@@ -25,6 +27,7 @@ RSpec.describe BookRefactored do
   describe "#LEND_TO_USER" do
     it "responds to LEND_TO_USER" do
       expect(subject.LEND_TO_USER("Bob")).to eq("Lending Different Seasons to Bob")
+      expect { subject.LEND_TO_USER("Bob") }.to output("Warning: LEND_TO_USER() is deprecated. Please use lend_to().\n").to_stderr
     end
 
     it "responds to lend_to" do
