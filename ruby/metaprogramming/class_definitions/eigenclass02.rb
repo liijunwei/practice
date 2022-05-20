a66ff06cc38fa5d849d471f2564816a317c54c7a
lijunwei <ljw532344863@sina.com>
2022-05-20 22:46:48 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/eigenclass02.rb
+++ ruby/metaprogramming/class_definitions/eigenclass02.rb
@@ -7,7 +7,8 @@ eigenclass =
     self
   end
 
-# 每个eigenclass只有一个实例, 这就是他们被称为单件类的原因
+# eigenclass是一个很特殊的类
+# 每个eigenclass只有一个实例(这就是他们被称为单件类的原因)
 # eigenclass不能被继承
 p eigenclass # #<Class:#<Object:0x000000010a71e610>>
 p eigenclass.class # Class
@@ -17,5 +18,16 @@ end
 
 p eigenclass.instance_methods.grep(/my_/) # [:my_singleton_method]
 
-demo = eigenclass.new
-p demo
+begin
+  eigenclass.new
+rescue => e
+  p [e.class, e.message] # [TypeError, "can't create instance of singleton class"]
+end
+
+begin
+  class Demo < eigenclass
+  end
+rescue => e
+  p [e.class, e.message] # [TypeError, "can't make subclass of singleton class"]
+end
+
