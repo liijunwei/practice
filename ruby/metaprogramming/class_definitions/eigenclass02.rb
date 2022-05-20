11df93d08f8a759ca528ace8207f9c3d7e9c2db7
lijunwei <ljw532344863@sina.com>
2022-05-20 22:44:43 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/eigenclass02.rb
+++ ruby/metaprogramming/class_definitions/eigenclass02.rb
@@ -7,6 +7,8 @@ eigenclass =
     self
   end
 
+# 每个eigenclass只有一个实例, 这就是他们被称为单件类的原因
+# eigenclass不能被继承
 p eigenclass # #<Class:#<Object:0x000000010a71e610>>
 p eigenclass.class # Class
 
@@ -15,3 +17,5 @@ end
 
 p eigenclass.instance_methods.grep(/my_/) # [:my_singleton_method]
 
+demo = eigenclass.new
+p demo
