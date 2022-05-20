9d49d11b08903bdd0bc941611efbec18931854e2
lijunwei <ljw532344863@sina.com>
2022-05-20 22:48:53 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/eigenclass02.rb
+++ ruby/metaprogramming/class_definitions/eigenclass02.rb
@@ -10,6 +10,7 @@ eigenclass =
 # eigenclass是一个很特殊的类
 # 每个eigenclass只有一个实例(这就是他们被称为单件类的原因)
 # eigenclass不能被继承
+# eigenclass是一个对象(class or instance)的单件方法的存活之所
 p eigenclass # #<Class:#<Object:0x000000010a71e610>>
 p eigenclass.class # Class
 
