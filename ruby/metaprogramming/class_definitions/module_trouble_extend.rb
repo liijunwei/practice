409970b1b7cc3dc55f945630e698bf398ddba6bc
lijunwei <ljw532344863@sina.com>
2022-05-21 00:31:20 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/module_trouble_extend.rb
+++ ruby/metaprogramming/class_definitions/module_trouble_extend.rb
@@ -14,12 +14,16 @@ require 'rspec'
 
 RSpec.describe MyClass do
   describe "my_method" do
-    let(:obj) { Object.new }
+    let(:obj) do
+      o = Object.new
+      o.extend MyModule
+
+      o
+    end
 
     it 'works' do
       expect(described_class.my_method).to eq("hello")
+      expect(obj.my_method).to eq("hello")
     end
   end
-
-
 end
