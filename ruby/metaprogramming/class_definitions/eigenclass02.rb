ff0054aff9c0ce0a704dea77ebf0af8ea0a23d2c
lijunwei <ljw532344863@sina.com>
2022-05-20 22:42:39 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/eigenclass02.rb
+++ ruby/metaprogramming/class_definitions/eigenclass02.rb
@@ -7,11 +7,11 @@ eigenclass =
     self
   end
 
-p eigenclass
-p eigenclass.class
+p eigenclass # #<Class:#<Object:0x000000010a71e610>>
+p eigenclass.class # Class
 
 def obj.my_singleton_method
 end
 
-p eigenclass.instance_methods.grep(/my_/)
+p eigenclass.instance_methods.grep(/my_/) # [:my_singleton_method]
 
