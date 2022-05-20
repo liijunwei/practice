8144382817df16ce4ed97a60c5564983bdc225f9
lijunwei <ljw532344863@sina.com>
2022-05-20 08:49:56 +0800

    Commit automatically by watch.

--- ruby/metaprogramming/class_definitions/eigenclass01.rb
+++ ruby/metaprogramming/class_definitions/eigenclass01.rb
@@ -1,10 +1,24 @@
+# page 117
+
 class MyClass
   def my_method
   end
 end
 
 obj = MyClass.new
-p obj.my_method # nil
-p obj.class # MyClass
+
+p obj.my_method        # nil
+p obj.class            # MyClass
 p obj.class.superclass # Object
 
+# 问题: 这个obj的方法, 放在类体系结构的哪个位置?
+def obj.my_singleton_method
+end
+
+p obj.my_singleton_method # nil
+
+# 问题: 这个MyClass的方法, 放在类体系结构的哪个位置?
+def MyClass.my_class_method
+end
+
+p MyClass.my_class_method
