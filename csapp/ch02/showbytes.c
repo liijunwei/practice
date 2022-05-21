b754147f54fced9108e09c58537164bfa89de2c0
lijunwei <ljw532344863@sina.com>
2022-05-22 07:21:10 +0800

    Commit automatically by watch.

--- csapp/ch02/showbytes.c
+++ csapp/ch02/showbytes.c
@@ -9,15 +9,26 @@ void show_bytes(byte_pointer start, size_t len) {
     printf(" %.2x", start[i]);
   }
 
-  printf("\n")
+  printf("\n");
 }
 
 void show_int(int x) {
-  show_bytes((byte_pointer))
+  show_bytes((byte_pointer) &x, sizeof(int));
 }
 
-int main(int argc, char const *argv[]) {
+void show_float(float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
+}
 
+void show_float(float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+// gcc csapp/ch02/showbytes.c && ./a.out
+int main(int argc, char const *argv[]) {
+  show_int(1);
+  show_int(2);
+  show_int(3);
 
   return 0;
 }
