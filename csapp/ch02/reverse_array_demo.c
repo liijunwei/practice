41378fa77593a040775cf5bc96a11c22520debfc
lijunwei <ljw532344863@sina.com>
2022-05-23 22:54:47 +0800

    Commit manually.

--- csapp/ch02/reverse_array_demo.c
+++ csapp/ch02/reverse_array_demo.c
@@ -13,7 +13,9 @@ void reverse_array(int a[], int count) {
   int last;
 
   for (first = 0, last = count - 1; first <= last; first++, last--) {
-    inplace_swap(&a[first], &a[last]);
+    if (first != last) {
+      inplace_swap(&a[first], &a[last]);
+    }
   }
 }
 
@@ -25,11 +27,11 @@ void print_array(int a[], int count) {
   printf("\n");
 }
 
-const int count = 3;
+const int count = 4;
 int main(int argc, char const *argv[])
 {
-  // int a[4] = {1, 2, 3, 4};
-  int a[count] = {1, 2, 3};
+  int a[count] = {1, 2, 3, 4};
+  // int a[count] = {1, 2, 3};
   print_array(a, count);
   reverse_array(a, count);
   print_array(a, count);
