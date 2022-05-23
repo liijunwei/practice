f9531e0b72412de93bc9f12d260d4a673ade8621
lijunwei <ljw532344863@sina.com>
2022-05-23 22:36:31 +0800

    Commit automatically by watch.

---
+++ csapp/ch02/reverse_array_demo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <assert.h>
+
+// page 38
+void inplace_swap(int *x, int *y) {
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
+
+void reverse_array(int a[], int count) {
+  int first
+}
+
+int main(int argc, char const *argv[])
+{
+  int a = 10;
+  int b = 20;
+
+  assert(a == 10); assert(b == 20);
+  inplace_swap(&a, &b);
+  assert(a == 20); assert(b == 10);
+
+  printf("PASS.\n");
+
+  return 0;
+}
