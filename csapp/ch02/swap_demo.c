7e8eb5be35f55cf955c3dbd811a743bc637378b8
lijunwei <ljw532344863@sina.com>
2022-05-23 22:17:25 +0800

    Commit automatically by watch.

--- csapp/ch02/swap_demo.c
+++ csapp/ch02/swap_demo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 // page 38
 void inplace_swap(int *x, int *y) {
@@ -7,15 +8,16 @@ void inplace_swap(int *x, int *y) {
   *y = *x ^ *y;
 }
 
-
 int main(int argc, char const *argv[])
 {
   int a = 10;
   int b = 20;
-  printf("a: %d, b: %d\n", a, b);
 
+  assert(a == 10); assert(b == 20);
   inplace_swap(&a, &b);
-  printf("a: %d, b: %d\n", a, b);
+  assert(a == 20); assert(b == 10);
+
+  printf("PASS.\n");
 
   return 0;
 }
