5c3a1b325a2419abe4c784444df07b55000f0715
lijunwei <ljw532344863@sina.com>
2022-05-23 22:13:10 +0800

    Commit automatically by watch.

--- csapp/ch02/swap_demo.c
+++ csapp/ch02/swap_demo.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-
+// page 38
+void inplace_swap(int *x, int *y) {
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
 
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  int a = 10;
+  int b = 20;
+  printf("a: %d, b: %d\n", a, b);
+
+  inplace_swap(&a, &b);
+  printf("a: %d, b: %d\n", a, b);
+
   return 0;
 }
