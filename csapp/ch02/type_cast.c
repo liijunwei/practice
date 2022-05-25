5402fd7cffa95c2ef4ceb36854ce8bc64ad6078c
lijunwei <ljw532344863@sina.com>
2022-05-25 08:01:51 +0800

    Commit automatically by watch.

--- csapp/ch02/type_cast.c
+++ csapp/ch02/type_cast.c
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
   short int v = -12345;
   unsigned short uv = (unsigned short) v;
   printf("v = %d, uv = %u\n", v, uv);
+  // v = -12345, uv = 53191
 
   return 0;
 }
