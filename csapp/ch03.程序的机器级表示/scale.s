eeade37a7e18388b079b38f658ca136172213cae
lijunwei <ljw532344863@sina.com>
2022-06-03 10:54:46 +0800

    Commit automatically by watch.

--- csapp/ch03.程序的机器级表示/scale.s
+++ csapp/ch03.程序的机器级表示/scale.s
@@ -10,9 +10,17 @@ _scale:                                 ## @scale
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
-	leaq	(%rdi,%rsi,4), %rax
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	%rdx, -24(%rbp)
+	movq	-8(%rbp), %rax
+	movq	-16(%rbp), %rcx
+	shlq	$2, %rcx
+	addq	%rcx, %rax
 	addq	$12, %rax
-	andq	%rdx, %rax
+	andq	-24(%rbp), %rax
+	movq	%rax, -32(%rbp)
+	movq	-32(%rbp), %rax
 	popq	%rbp
 	retq
 	.cfi_endproc
