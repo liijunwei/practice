9958af71f506a7be2e8500607b6602b819bd3bd7
lijunwei <ljw532344863@sina.com>
2022-07-14 22:19:40 +0800

    Commit automatically by watch

---
+++ csapp/ch03.程序的机器级表示/rfun_demo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+long rfun(unsigned long x) {
+  if (x == 0) {
+    return 0;
+  }
+
+  unsigned long nx = x >> 2;
+  long rv = rfun(nx);
+
+  return x + rv;
+}
+
+int main(int argc, char const *argv[])
+{
+  printf("%lu \n", rfun(0));
+  printf("%lu \n", rfun(1));
+  printf("%lu \n", rfun(2));
+  printf("%lu \n", rfun(3));
+  printf("%lu \n", rfun(4));
+  printf("%lu \n", rfun(5));
+
+  return 0;
+}
