1960e9571bf1d3336e80d052a59e05f056292e1d
lijunwei <ljw532344863@sina.com>
2022-07-16 18:43:25 +0800

    Commit manually

---
+++ golang/a-tour-of-go/methods-pointers.go
@@ -0,0 +1,37 @@
+// https://go.dev/tour/methods/4
+
+// Methods with pointer receivers can modify the value to which the receiver points (as Scale does here). Since methods often need to modify their receiver, pointer receivers are more common than value receivers.
+
+// 方法接收者用指针表示, 可以修改结构里的数据
+
+// Try removing the * from the declaration of the Scale function on line 16 and observe how the program's behavior changes.
+
+// 如果把Scale函数声明里的 *去掉, 实际上是操作结构的副本, 不会影响结构的值
+
+// The Scale method must have a pointer receiver to change the Vertex value declared in the main function.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
+}
