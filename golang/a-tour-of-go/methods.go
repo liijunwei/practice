6d7bb6d9370b9196071a359720d53061ee202356
lijunwei <ljw532344863@sina.com>
2022-07-16 18:35:04 +0800

    Commit manually

---
+++ golang/a-tour-of-go/methods.go
@@ -0,0 +1,27 @@
+// https://go.dev/tour/methods/1
+
+package main
+
+// Go does not have classes. However, you can define methods on types.
+// go没有类, 但可以给用struc定义的类型定义方法
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+
+	v2 := Vertex{1, 1}
+	fmt.Println(v2.Abs())
+}
