17ee57dbef08e62a13aa999907edbdcd8e9f5e25
lijunwei <ljw532344863@sina.com>
2022-07-17 11:21:10 +0800

    Commit automatically by watch

---
+++ golang/code-block-and-scope/variable-shadowing.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var a = 11
+
+func foo(n int) {
+	a := 1
+	a += n
+}
+
+func main() {
+	fmt.Println("a = ", a)
+	foo(5)
+	fmt.Println("after calling foo, a = ", a)
+}
---
+++ golang/code-block-and-scope/variable-shadowing.md
@@ -0,0 +1,5 @@
++ variable shadowing(变量遮蔽)
+    + ./variable-shadowing.go
+
++ 变量遮蔽只是引子, 真正的重点是 代码库(block) 和 作用域(scope)
+
