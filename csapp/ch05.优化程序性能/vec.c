f0240f854dc73ea47f8f93304213dbab351f74a1
lijunwei <ljw532344863@sina.com>
2022-08-09 21:26:52 +0800

    Commit automatically by watch

---
+++ csapp/ch05.优化程序性能/vec.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef int data_t;
+// typedef long data_t;
+// typedef float data_t;
+// typedef double data_t;
+
+typedef struct {
+  long len;
+  data_t *data;
+} vec_rec, *vec_prt;
+
+/* Create vector of specified length */
+vec_prt new_vec(long len) {
+
+}
+
+int get_vec_
+
+int main(int argc, char const *argv[]) {
+
+
+  return 0;
+}
