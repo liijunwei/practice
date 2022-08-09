43e08b0c5a68a55b58ed5f6fcb32dd5aa24add22
lijunwei <ljw532344863@sina.com>
2022-08-09 21:31:56 +0800

    Commit automatically by watch

--- csapp/ch05.优化程序性能/vec.c
+++ csapp/ch05.优化程序性能/vec.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// p348
 
 typedef int data_t;
 // typedef long data_t;
@@ -8,14 +11,44 @@ typedef int data_t;
 typedef struct {
   long len;
   data_t *data;
-} vec_rec, *vec_prt;
+} vec_rec, *vec_ptr;
 
 /* Create vector of specified length */
-vec_prt new_vec(long len) {
+vec_ptr new_vec(long len) {
+  vec_ptr result = (vec_ptr) malloc(sizeof(vec_rec));
+  data_t *data = NULL;
 
+  if(!result) {
+    return NULL;
+  }
+
+  result->len = len;
+
+  if(len > 0) {
+    data = (data_t *)calloc(len, sizeof(data_t));
+
+    if(!data) {
+      free((void *) result);
+      return NULL;
+    }
+  }
+
+  result->data = data;
+  return result;
 }
 
-int get_vec_
+int get_vec_element(vec_ptr v, long index, data_t *dest) {
+  if(index < 0 || index >= v->len) {
+    return 0;
+  }
+
+  *dest = v->data[index];
+  return 1;
+}
+
+long vec_length(vec_ptr v) {
+  return v->len;
+}
 
 int main(int argc, char const *argv[]) {
 
