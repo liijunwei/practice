3cb7a5ef34c7a7f2d79cc0fd0611d2bb96d8cfac
lijunwei <ljw532344863@sina.com>
2022-08-16 00:40:59 +0800

    Commit automatically by watch

--- scheme/Yet-Another-Scheme-Tutorial/02.using_scheme_as_a_calculator/exercise.scm
+++ scheme/Yet-Another-Scheme-Tutorial/02.using_scheme_as_a_calculator/exercise.scm
@@ -2,15 +2,16 @@
 
 ; Calculate followings using Scheme interpreter.
 
-(1+39) * (53-45)
-(1020 / 39) + (45 * 2)
+; (1+39) * (53-45)
+(* (+ 1 39) (- 53 45))
+
+; (1020 / 39)  (45 * 2)
+(+ (/ 1020 39) (* 45 2))
 
 ; Sum of 39, 48, 72, 23, and 91
-
 (+ 39 48 72 23 91)
 
 ; Average of 39, 48, 72, 23, and 91 as a floating point.
-
 (exact->inexact (/ (+ 39 48 72 23 91) 5))
 ; 54.6
 
@@ -24,6 +25,7 @@
 ; exp(2/3)
 (exp (/ 2 3))
 (exp (exact->inexact (/ 2 3)))
+(exp 2/3)
 
 ; 3 to the power of 4
 ; The value of b to the power of a can be calculated by (expt a b)
