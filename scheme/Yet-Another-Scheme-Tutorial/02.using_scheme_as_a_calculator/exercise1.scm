132c79a1377549485ea0462e4a82f8fb926503eb
lijunwei <ljw532344863@sina.com>
2022-08-16 00:20:30 +0800

    Commit automatically by watch

---
+++ scheme/Yet-Another-Scheme-Tutorial/02.using_scheme_as_a_calculator/exercise1.scm
@@ -0,0 +1,15 @@
+; Exercise 1
+
+; Calculate followings using Scheme interpreter.
+
+(1+39) * (53-45)
+(1020 / 39) + (45 * 2)
+
+; Sum of 39, 48, 72, 23, and 91
+
+(+ 39 48 72 23 91)
+
+; Average of 39, 48, 72, 23, and 91 as a floating point.
+
+
+
