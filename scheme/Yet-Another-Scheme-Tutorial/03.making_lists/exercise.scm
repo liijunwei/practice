2f5187a79e261512e6e5fa0a40f269d7fbe2f3e8
lijunwei <ljw532344863@sina.com>
2022-08-18 00:25:43 +0800

    Commit automatically by watch

--- scheme/Yet-Another-Scheme-Tutorial/03.making_lists/exercise.scm
+++ scheme/Yet-Another-Scheme-Tutorial/03.making_lists/exercise.scm
@@ -5,14 +5,24 @@
 (cons "hi" "everybody")
 
 ; (0)
-(cons 0 nil)
+(cons 0 ())
 
 ; (1 10 . 100)
 (cons 1 (cons 10 100))
 
 ; (1 10 100)
-
-(cons 1 (cons 10 (cons 100 ())))
+(cons 1
+  (cons 10
+    (cons 100
+      ())))
 
 ; (#\I "saw" 3 "girls")
+(cons #\I
+  (cons "saw"
+    (cons 3
+      (cons "girls"
+        ()))))
+
 ; ("Sum of" (1 2 3 4) "is" 10)
+
+(cons "Sum of" (cons (cons (cons 1 (cons 2 (cons 3 (cons 4 ())))) "is") ) )
