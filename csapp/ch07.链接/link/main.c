a25ef045359608f13f1faff2d75738f03aad3830
lijunwei <ljw532344863@sina.com>
2022-08-23 23:13:20 +0800

    If static keyword used, it's considered as private

--- csapp/ch07.链接/link/main.c
+++ csapp/ch07.链接/link/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int sum(int *a, int n);
+int sum1(int *a, int n);
 
 int array[2] = {1, 2};
 
@@ -8,6 +9,7 @@ int main(int argc, char const *argv[]) {
   int val = sum(array, 2);
 
   printf("val = %d\n", val);
+  printf("val1 = %d\n", sum1(array, 2));
 
   return 0;
 }
--- csapp/ch07.链接/link/sum.c
+++ csapp/ch07.链接/link/sum.c
@@ -1,3 +1,14 @@
+static int sum1(int *a, int n) {
+  int i, s = 0;
+
+  for(i = 0; i < n; i++) {
+    s += a[i];
+  }
+
+  return s;
+}
+
+
 int sum(int *a, int n) {
   int i, s = 0;
 
