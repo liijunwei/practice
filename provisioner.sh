bc751f9572b5139e89327b354d3d997c187b09d5
lijunwei <ljw532344863@sina.com>
2022-08-24 22:58:30 +0800

    Commit automatically by watch

--- provisioner.sh
+++ provisioner.sh
@@ -2,10 +2,12 @@
 
 # https://gist.github.com/puremourning/a41b4c6ac732091f63736e3ccb6d8d67
 
-sudo yum groupinstall -y "Development tools"
-sudo yum install -y git
+sudo yum install epel-release
+sudo yum makecache
 
-sudo yum install -y zsh
+sudo yum groupinstall -y "Development tools"
+sudo yum install -y git tig zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo chsh -s $(which zsh) vagrant
 
