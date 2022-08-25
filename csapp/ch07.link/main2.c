68ee3447558091da3f82d71c62886b8d01392ef2
lijunwei <ljw532344863@sina.com>
2022-08-25 21:59:21 +0800

    Commit automatically by watch

--- csapp/ch07.link/main2.c
+++ csapp/ch07.link/main2.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
-// #include "vector.h"
-// #include <vector>
-// #include <vector.h>
 
 int x[2] = {1, 2};
 int y[2] = {3, 4};
 int z[2];
 
-/**
-sudo yum --setopt=group_package_types=mandatory,default,optional groupinstall "Development Tools" -y
-sudo yum install vector.x86_64
-yum provides libstdc++
-sudo yum install libstdc++ -y
-sudo yum update libstdc++
-
-先跳过了
-*/
 int main(int argc, char const *argv[]) {
   addvec(x, y, z, 2);
   printf("z = [%d %d]\n", z[0], z[1]);
