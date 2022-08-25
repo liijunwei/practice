24b46e36d513c6fb9512448d8c82ab7312516c26
lijunwei <ljw532344863@sina.com>
2022-08-25 08:44:04 +0800

    Commit automatically by watch

--- provisioner.sh
+++ provisioner.sh
@@ -6,6 +6,10 @@ sudo yum install -y epel-release
 sudo yum makecache
 
 sudo yum groupinstall -y "Development tools"
-sudo yum install -y git tig zsh
+
+sudo yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
+sudo yum install -y git
+sudo yum install -y tig zsh
+
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && sudo chsh -s $(which zsh) vagrant
