24405f644d4e44be28815d9d942503749755bc93
lijunwei <ljw532344863@sina.com>
2022-08-25 09:40:58 +0800

    Add todo

---
+++ csapp/ch07.link/main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// #include "vector.h"
+// #include "vector"
+// #include <vector.h>
+
+int x[2] = {1, 2};
+int y[2] = {3, 4};
+int z[2];
+
+int main(int argc, char const *argv[]) {
+  addvec(x, y, z, 2);
+  printf("z = [%d %d]\n", z[0], z[1]);
+
+  return 0;
+}
