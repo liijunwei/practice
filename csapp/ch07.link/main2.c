e22991ca34403c83f09e70b1cbb8c763f67e0e65
lijunwei <ljw532344863@sina.com>
2022-08-25 22:00:31 +0800

    Commit manually

--- csapp/ch07.link/main2.c
+++ csapp/ch07.link/main2.c
@@ -2,11 +2,15 @@
 
 int x[2] = {1, 2};
 int y[2] = {3, 4};
-int z[2];
+int m[2];
+int n[2];
 
 int main(int argc, char const *argv[]) {
-  addvec(x, y, z, 2);
-  printf("z = [%d %d]\n", z[0], z[1]);
+  addvec(x, y, m, 2);
+  printf("m = [%d %d]\n", m[0], m[1]);
+
+  multvec(x, y, n, 2);
+  printf("n = [%d %d]\n", n[0], n[1]);
 
   return 0;
 }
