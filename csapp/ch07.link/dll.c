a3889d8aab19e9b91d89b806cb6cb2d05bb4e7d5
lijunwei <ljw532344863@sina.com>
2022-08-26 09:02:29 +0800

    Commit automatically by watch

--- csapp/ch07.link/dll.c
+++ csapp/ch07.link/dll.c
@@ -8,7 +8,29 @@ int m[2];
 int n[2];
 
 int main(int argc, char const *argv[]) {
+  void *handle;
+  void (*addvec)(int *, int *, int *, int);
+  char *error;
 
+  handle = dlopen("./libvector.so", RTLD_LAZY);
+  if(handle) {
+    fprintf(stderr, "%s\n", dlerror());
+    exit(1);
+  }
+
+  addvec = dlsym(handle, "addvec");
+  if((error = dlerror()) != NULL) {
+    fprintf(stderr, "%s\n", error);
+    exit(1);
+  }
+
+  addvec(x, y, m, 2);
+  printf("addvec  => [%d %d]\n", m[0], m[1]);
+
+  if(dlclose(handle) < 0) {
+    fprintf(stderr, "%s\n", dlerror());
+    exit(1);
+  }
 
   return 0;
 }
