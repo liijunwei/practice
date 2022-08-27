2ae12943363f9409615366aeb7c770c646f959fb
lijunwei <ljw532344863@sina.com>
2022-08-27 14:41:20 +0800

    Commit automatically by watch

--- csapp/ch07.link/interpose/mymalloc-runtime.c
+++ csapp/ch07.link/interpose/mymalloc-runtime.c
@@ -7,7 +7,17 @@
 
 // malloc wrapper function
 void *malloc(size_t size) {
-  void *
+  void *(mallocp)(size_t size);
+  char *error;
+
+  mallocp = dlsym(RTLD_NEXT, "malloc"); // get address of libc malloc
+
+  if((error = dlerror()) != NULL) {
+    fputs(error, stderr);
+    exit(1);
+  }
+
+  char *ptr = mallocp(size); // call libc malloc
 
   printf("malloc(%d)=%p\n", (int)size, ptr);
 
@@ -16,6 +26,21 @@ void *malloc(size_t size) {
 
 // free wrapper function
 void free(void *ptr) {
+  void (freep)(void *) = NULL;
+  char *error;
+
+  if(!ptr) {
+    return;
+  }
+
+  freep = dlsym(RTLD_NEXT, "free");
+
+  if((error = dlerror()) != NULL) {
+    fputs(error, stderr);
+    exit(1);
+  }
+
+  freep(ptr);
 
   printf("frees(%p)\n", ptr);
 }
