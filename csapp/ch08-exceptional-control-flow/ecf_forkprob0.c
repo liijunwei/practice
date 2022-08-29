6344ea90ca047d48fbcae1955b9f003469907060
lijunwei <ljw532344863@sina.com>
2022-08-29 22:54:12 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_forkprob0.c
+++ csapp/ch08-exceptional-control-flow/ecf_forkprob0.c
@@ -5,10 +5,9 @@ int main(int argc, char const *argv[]) {
   int x = 1;
 
   if(Fork() == 0) {
-    printf("child: x=%d\n", ++x);
-    exit(0);
+    printf("p1: x=%d\n", ++x);
   }
 
-  printf("parent: x=%d\n", --x);
+  printf("p2: x=%d\n", --x);
   exit(0);
 }
