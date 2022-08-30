47ce35c7c3f25c7deaa5edb27b090fb96ad263db
lijunwei <ljw532344863@sina.com>
2022-08-30 21:45:40 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_shellex.c
+++ csapp/ch08-exceptional-control-flow/ecf_shellex.c
@@ -22,4 +22,45 @@ int main(int argc, char const *argv[], char const *envp[]) {
   exit(0);
 }
 
+void eval(char *cmdline) {
+  char *argv[MAXARGS];
+  char buf[MAXLINE];
+  int bg;
+  pid_t pid;
+
+  strcpy(buf, cmdline);
+  bg = parseline(buf, argv);
+
+  if(argv[0] == NULL) {
+    return;
+  }
+
+  if(!builtin_command(argv)) {
+    if((pid = Fork()) == 0) {
+      if(execv(argv[0], argv, environ) < 0) {
+        printf("%s: Command not found.\n", argv[0]);
+        exit(0);
+      }
+    }
+
+    if(!bg) {
+      int status;
+      if(waitpid(pid, &status, 0) < 0) {
+        unix_error("waitfg: waitpid error");
+      }
+    } else {
+      printf("%d %s", pid, cmdline);
+    }
+  }
+
+  return;
+}
+
+
+int builtin_command(char **argv) {
+  if(!strcmp(argv[0], "quit"))
+}
+
+
+
 
