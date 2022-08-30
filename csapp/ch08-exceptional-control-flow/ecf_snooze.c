757b933763101843ef8789280c7aee65d0779d6d
lijunwei <ljw532344863@sina.com>
2022-08-30 21:25:23 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_snooze.c
+++ csapp/ch08-exceptional-control-flow/ecf_snooze.c
@@ -1,10 +1,15 @@
 #include "csapp.h"
 
 unsigned int snooze(unsigned int secs) {
-  unsigned int rc = sleep(sec);
+  printf("Sleeping...\n");
+  unsigned int rc = sleep(secs);
+  printf("Slept for %d of %d secs.\n", secs - rc, secs);
+
+  return rc;
 }
 
 int main(int argc, char const *argv[]) {
+  snooze(10);
 
   exit(0);
 }
