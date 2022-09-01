40f72f364d6d8518fe6b847b0e651a60dc68f3a8
lijunwei <ljw532344863@sina.com>
2022-09-02 07:29:58 +0800

    Commit automatically by watch

---
+++ csapp/ch08-exceptional-control-flow/ecf_kill.c
@@ -0,0 +1,16 @@
+#include "csapp.h"
+
+int main(int argc, char const *argv[]) {
+  pid_t pid;
+
+  // child sleeps until SIGKILL signal received, then dies
+  if((pid = Fork()) == 0) {
+    Pause();
+    printf("control should never reach here!\n");
+    exit(0);
+  }
+
+  printf("parent sends a SIGKILL signal to a child\n");
+  Kill(pid, SIGKILL);
+  exit(0);
+}
