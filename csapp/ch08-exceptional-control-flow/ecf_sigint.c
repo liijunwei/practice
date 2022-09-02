01761b2054f341c4cdbf8dbe836d4bfe599a7613
lijunwei <ljw532344863@sina.com>
2022-09-02 08:05:28 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_sigint.c
+++ csapp/ch08-exceptional-control-flow/ecf_sigint.c
@@ -1,6 +1,22 @@
 #include "csapp.h"
 
+// capture ctrl+c signal
+// print message
+// stop the program
+
+void sigint_handler(int sig) {
+  printf("Caught SIGINT!\n");
+  exit(0);
+}
+
 int main(int argc, char const *argv[]) {
+  // install the SIGINT handler
+  // man signal
+  if(signal(SIGINT, sigint_handler) == SIG_ERR) {
+    unix_error("signal error...");
+  }
+
+  Pause(); // wait for the receipt of a signal
 
   exit(0);
 }
