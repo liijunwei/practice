1ccd7f64acc44573fb6e143800c9bc18da7e3001
lijunwei <ljw532344863@sina.com>
2022-09-02 08:15:36 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_snooze_8_7.c
+++ csapp/ch08-exceptional-control-flow/ecf_snooze_8_7.c
@@ -5,15 +5,25 @@ void handler(int sig) {
 }
 
 unsigned int snooze(unsigned int secs) {
-  printf("Sleeping...\n");
+  printf("Snoozed for %d seconds...\n", secs);
   unsigned int rc = sleep(secs);
+  printf("\n");
   printf("Slept for %d of %d secs.\n", secs - rc, secs);
 
   return rc;
 }
 
 int main(int argc, char const *argv[]) {
-  if(argc != 2)
+  if(argc != 2) {
+    fprintf(stderr, "usage: %s <secs>\n", argv[0]);
+    exit(0);
+  }
 
-  return 0;
+  if(signal(SIGINT, handler) == SIG_ERR) {
+    unix_error("signal error\n");
+  }
+
+  (void)snooze(atoi(argv[1]));
+
+  exit(0);
 }
