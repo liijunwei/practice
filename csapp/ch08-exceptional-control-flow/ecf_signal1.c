c2d9fb9440dc0eb8505dd6d908b56b7d7710eef6
lijunwei <ljw532344863@sina.com>
2022-09-03 11:02:50 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_signal1.c
+++ csapp/ch08-exceptional-control-flow/ecf_signal1.c
@@ -1,6 +1,7 @@
 #include "csapp.h"
 
 // this code is buggy
+// 这个程序是有缺陷的, 因为它假设信号是排队的
 
 void sigint_handler1(int sig) {
   int olderrno = errno;
@@ -18,8 +19,28 @@ void sigint_handler1(int sig) {
 int main(int argc, char const *argv[]) {
   int i;
   int n;
-  char buf[MAXBUF]
+  char buf[MAXBUF];
 
+  if(signal(SIGCHLD, sigint_handler1) == SIG_ERR) {
+    unix_error("signal error");
+  }
+
+  for(i = 0; i < 3; i++) {
+    if(Fork() == 0) {
+      printf("hello from child %d\n", (int)getpid());
+      exit(0);
+    }
+  }
+
+  if((n = read(STDIN_FILENO, buf, sizeof(buf))) < 0) {
+    unix_error("read");
+  }
+
+  printf("Parent processing input\n");
+
+  while(1) {
+    ;
+  }
 
   exit(0);
 }
