e2599d39f8306da2c7892290eeb73e81d50d0bcb
lijunwei <ljw532344863@sina.com>
2022-09-04 16:00:05 +0800

    Commit automatically by watch

--- csapp/ch08-exceptional-control-flow/ecf_restart.c
+++ csapp/ch08-exceptional-control-flow/ecf_restart.c
@@ -1,5 +1,9 @@
 #include "csapp.h"
 
+// p549(图8-44)
+// c++ 和 java中提供的异常机制是较高层次的, 是c语言的setjmp和longjmp函数的更加结构化的版本
+// 你可以把 try语句中的catch子句看做类似setjmp函数, 相似的, throw语句类似于longjmp函数 (???)
+
 jmp_buf buf;
 
 void handler(int sig) {
