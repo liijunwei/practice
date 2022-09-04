98449c8b8fc3d8971b591689333985e2e9b93964
lijunwei <ljw532344863@sina.com>
2022-09-04 15:21:54 +0800

    Commit manually

---
+++ csapp/ch08-exceptional-control-flow/ecf_waitforsignal.c
@@ -0,0 +1,45 @@
+#include "csapp.h"
+
+volatile sig_atomic_t pid;
+
+void sig_chld_handler(int s) {
+  int olderrno = errno;
+  pid = waitpid(-1, NULL, 0);
+  errno = olderrno;
+}
+
+void sigint_handler(int s) {
+  exit(0); // if I remove this line, ctrl+c won't interrupt the program
+}
+
+int main(int argc, char const *argv[]) {
+  sigset_t mask;
+  sigset_t prev;
+
+  Signal(SIGCHLD, sig_chld_handler);
+  Signal(SIGINT, sigint_handler);
+  Sigemptyset(&mask);
+  Sigaddset(&mask, SIGCHLD);
+
+  while(1) {
+    Sigprocmask(SIG_BLOCK, &mask, &prev); // block SIGCHLD
+
+    if(Fork() == 0) {
+      exit(0);
+    }
+
+    pid = 0;
+    Sigprocmask(SIG_SETMASK, &prev, NULL); // unblock SIGCHLD
+
+    // wait for SIGCHLD to be received(wasteful)
+    while(!pid) {
+      ;
+    }
+
+    // do some work after receiving SIGCHLD
+    printf(".");
+
+  }
+
+  exit(0);
+}
