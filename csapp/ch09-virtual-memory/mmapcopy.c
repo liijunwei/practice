601da98e9d0c8476ba44ddce6ca17a8df44d43e1
lijunwei <ljw532344863@sina.com>
2022-09-07 22:54:46 +0800

    Commit automatically by watch

--- csapp/ch09-virtual-memory/mmapcopy.c
+++ csapp/ch09-virtual-memory/mmapcopy.c
@@ -1,6 +1,14 @@
 // 9.5 编写一个从程序, 使用mmap将一个任意大小的磁盘文件复制到stdout. 输入文件的名字必须作为一个命令行参数来传递
 
-#include <stdio.h>
+#include "csapp.h"
+
+void mmapcopy(int fd, int size) {
+  char *bufp; // ptr to memory-mapped VM area
+
+  bufp = Mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+  Write(1, bufp, size);
+  return;
+}
 
 int main(int argc, char const *argv[]) {
 
