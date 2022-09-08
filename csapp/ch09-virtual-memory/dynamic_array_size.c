3815475fca5771e3d75deda22394a100e496a427
lijunwei <ljw532344863@sina.com>
2022-09-08 11:22:03 +0800

    Commit automatically by watch

--- csapp/ch09-virtual-memory/dynamic_array_size.c
+++ csapp/ch09-virtual-memory/dynamic_array_size.c
@@ -8,13 +8,11 @@ int main(int argc, char const *argv[]) {
   scanf("%d", &n);
   array = (int *)Malloc(n * sizeof(int));
 
-  if(n > MAXN) {
-    app_error("Input file too big");
-  }
-
   for(i = 0; i < n; i++) {
     scanf("%d", &array[i]);
   }
+
+  free(array);
 
   exit(0);
 }
