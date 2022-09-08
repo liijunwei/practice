af4e0c040ef12a5789c2e7d47b9deae7de1d59dc
lijunwei <ljw532344863@sina.com>
2022-09-08 17:10:40 +0800

    Commit automatically by watch

--- clang/learn-header-file/main.c
+++ clang/learn-header-file/main.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
-#include "max.h"
+#include <stdio.h> // 表示 让编译器去指定目录去找对应的文件
+#include "max.h"   // 表示 让编译器先去当前文件所在的目录找这个文件, 如果没有, 再去编译器指定的目录去找
+
+// 编译器知道自己该去哪里去找标准库的头文件
+// 环境变量 和 编译器命令行参数可以指定寻找头文件的目录
 
 int main(int argc, char const *argv[]) {
   int a = 5;
