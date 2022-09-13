8b1147bbb03d763bb3b4c6162bd3ac28c16d06d3
lijunwei <ljw532344863@sina.com>
2022-09-13 22:01:26 +0800

    Commit automatically by watch

---
+++ csapp/ch10-system-level-io/io_ex_10_2.c
@@ -0,0 +1,18 @@
+#include "csapp.h"
+
+int main(int argc, char const *argv[]) {
+  int fd1;
+  int fd2;
+
+  char c;
+
+  fd1 = Open("foobar.txt", O_RDONLY, 1);
+  fd2 = Open("foobar.txt", O_RDONLY, 1);
+
+  Read(fd1, &c, 1);
+  Read(fd2, &c, 1);
+
+  printf("c = %c\n", c);
+
+  return 0;
+}
