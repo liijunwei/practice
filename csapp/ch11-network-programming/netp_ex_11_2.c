75fb8bbb07953ffefce0b7522872117579d009a5
lijunwei <ljw532344863@sina.com>
2022-09-14 08:41:08 +0800

    Commit automatically by watch

--- csapp/ch11-network-programming/netp_ex_11_2.c
+++ csapp/ch11-network-programming/netp_ex_11_2.c
@@ -1,7 +1,24 @@
 #include "csapp.h"
 
+// hex2dd.c
 int main(int argc, char const *argv[]) {
+  struct in_addr inaddr; // address in network byte order
+  uint32_t addr;         // address in host byte order
+  char buf[MAXBUF];
 
+  if(argc != 2) {
+    fprintf(stderr, "usage: %s <hex number>\n", argv[0]);
+    exit(0);
+  }
+
+  sscanf(argv[1], "%x", &addr);
+  inaddr.s_addr = htonl(addr);
+
+  if(!inet_ntop(AF_INET, &inaddr, buf, MAXBUF)) {
+    unix_error("inet_ntop");
+  }
+
+  printf("%s\n", buf);
 
   exit(0);
 }
