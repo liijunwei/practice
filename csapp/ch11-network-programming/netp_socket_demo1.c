1c5c3c918b8cb77305138a2c6561e476b6e0e6c0
lijunwei <ljw532344863@sina.com>
2022-09-14 21:36:49 +0800

    Commit automatically by watch

---
+++ csapp/ch11-network-programming/netp_socket_demo1.c
@@ -0,0 +1,12 @@
+#include "csapp.h"
+
+int main(int argc, char const *argv[]) {
+  Socket(AF_INET, SOCK_STREAM, 0); // 3
+  Socket(AF_INET, SOCK_STREAM, 0); // 4
+  Socket(AF_INET, SOCK_STREAM, 0); // 5
+  int clientfd = Socket(AF_INET, SOCK_STREAM, 0); // 6
+
+  printf("clientfd: %d\n", clientfd);
+
+  exit(0);
+}
