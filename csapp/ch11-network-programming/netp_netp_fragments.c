a12416f0f15c5eb3b3d32a058cbb38b02142bac4
lijunwei <ljw532344863@sina.com>
2022-09-14 21:31:49 +0800

    Commit automatically by watch

--- csapp/ch11-network-programming/netp_netp_fragments.c
+++ csapp/ch11-network-programming/netp_netp_fragments.c
@@ -1,4 +1,7 @@
 
+// _t 后缀 表示这些数据类型是用 typedef 定义的, 而不是新的数据类型
+// _in 后缀 是 internet 的缩写
+
 // ip socket address structure
 struct sockaddr_in {
   uint16_t      sin_family;       // protocol family, always AF_INET
@@ -6,3 +9,12 @@ struct sockaddr_in {
   struct        in_addr sin_addr; // ip address  in network byte order
   unsigned char sin_zero[8];      // pad to sizeof(struct socketaddr)
 }
+
+// generic socket address structure(for connect, bind, and accept)
+struct socketaddr {
+  uint16_t sa_family; // protocol family
+  char sa_data[14];   // address data
+}
+
+// Steven
+typedef struct socketaddr SA;
