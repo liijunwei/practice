da8c200c3cec9728b578e79fb2be16ac95587fc1
lijunwei <ljw532344863@sina.com>
2022-09-22 21:36:30 +0800

    Commit automatically by watch

--- csapp/ch12-concurrent-programming/conc_badcnt.c
+++ csapp/ch12-concurrent-programming/conc_badcnt.c
@@ -9,7 +9,25 @@ void *thread(void *vargp);
 volatile long cnt = 0; // counter
 
 int main(int argc, char const *argv[]) {
+  long niters; // number of iterations
+  pthread_t tid1;
+  pthread_t tid2;
 
+  if(argc != 2) {
+    printf("usage: %s <niters>\n", argv[0]);
+    exit(0);
+  }
 
   exit(0);
+}
+
+// thread routine
+void *thread(void *vargp) {
+  long i, niters = *((long *)vargp); // unclear...
+
+  for(i = 0; i < niters; i++) {
+    cnt++;
+  }
+
+  return NULL;
 }
