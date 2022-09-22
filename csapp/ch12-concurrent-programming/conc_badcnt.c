a900fa77ba23c90c7e46dda5d2627d765881a133
lijunwei <ljw532344863@sina.com>
2022-09-22 21:31:30 +0800

    Commit automatically by watch

---
+++ csapp/ch12-concurrent-programming/conc_badcnt.c
@@ -0,0 +1,15 @@
+// This code is buggy
+// 12.5
+
+#include "csapp.h"
+
+void *thread(void *vargp);
+
+// golbal shared variable
+volatile long cnt = 0; // counter
+
+int main(int argc, char const *argv[]) {
+
+
+  exit(0);
+}
