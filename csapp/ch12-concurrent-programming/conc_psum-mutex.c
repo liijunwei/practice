6f731e090f0c5682a129cb3f4384130038938d0b
lijunwei <ljw532344863@sina.com>
2022-09-25 11:22:11 +0800

    Commit automatically by watch

--- csapp/ch12-concurrent-programming/conc_psum-mutex.c
+++ csapp/ch12-concurrent-programming/conc_psum-mutex.c
@@ -4,7 +4,31 @@
 
 void *sum_mutex(void *vargp); // thread routine
 
+long gsum = 0;
+long nelems_per_thread;
+sem_t mutex;
+
 int main(int argc, char const *argv[]) {
+  long i;
+  long nelems;
+  long log_nelems;
+  long nthreads;
+  long myid[MAXTHREADS];
+
+  pthread_t tid[MAXTHREADS];
+
+  if(argc != 3) {
+    printf("Usage: %s <nthreads> <log_nelems>\n", argv[0]);
+    exit(0);
+  }
+
+  nthreads = atoi(argv[1]);
+  log_nelems = atoi(argv[2]);
+  nelems = (1L << log_nelems);
+  nelems_per_thread = nelems / nthreads;
+
+  Sem_init(&mutex, 0, 1);
+
 
 
   exit(0);
