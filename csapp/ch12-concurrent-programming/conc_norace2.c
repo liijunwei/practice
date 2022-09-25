5f9ec3f45eecdeca58a67c240368eee57dd7f117
lijunwei <ljw532344863@sina.com>
2022-09-25 17:32:49 +0800

    Commit automatically by watch

--- csapp/ch12-concurrent-programming/conc_norace2.c
+++ csapp/ch12-concurrent-programming/conc_norace2.c
@@ -8,10 +8,8 @@ int main(int argc, char const *argv[]) {
   pthread_t tid[N];
   int i;
 
-  // `i++` 和 `int myid = *((int *)vargp);` 可能产生竞争
-  // 因为传给线程的是i的地址...
   for (i = 0; i < N; i++) {
-    Pthread_create(&tid[i], NULL, thread, &i);
+    Pthread_create(&tid[i], NULL, thread, i);
   }
 
   for (i = 0; i < N; i++) {
@@ -21,8 +19,9 @@ int main(int argc, char const *argv[]) {
   exit(0);
 }
 
+// unclear: 什么叫"它假设指针至少和int一样大?"
 void *thread(void *vargp) {
-  int myid = *((int *)vargp);
+  int myid = (int)vargp;
   printf("Hello from thread %d\n", myid);
 
   return NULL;
