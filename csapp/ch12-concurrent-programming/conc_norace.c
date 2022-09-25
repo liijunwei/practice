86dbfdadb9e789669ebeef73b500160ed1d264cc
lijunwei <ljw532344863@sina.com>
2022-09-25 17:27:49 +0800

    Commit automatically by watch

--- csapp/ch12-concurrent-programming/conc_norace.c
+++ csapp/ch12-concurrent-programming/conc_norace.c
@@ -7,10 +7,13 @@ void *thread(void *vargp);
 int main(int argc, char const *argv[]) {
   pthread_t tid[N];
   int i;
+  int *ptr;
 
   // `i++` 和 `int myid = *((int *)vargp);` 可能产生竞争
   for (i = 0; i < N; i++) {
-    Pthread_create(&tid[i], NULL, thread, &i);
+    ptr = Malloc(sizeof(int));
+    *ptr = i;
+    Pthread_create(&tid[i], NULL, thread, ptr);
   }
 
   for (i = 0; i < N; i++) {
@@ -22,6 +25,7 @@ int main(int argc, char const *argv[]) {
 
 void *thread(void *vargp) {
   int myid = *((int *)vargp);
+  Free(vargp);
   printf("Hello from thread %d\n", myid);
 
   return NULL;
