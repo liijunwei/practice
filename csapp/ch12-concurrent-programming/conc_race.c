a2c284669878a82136a729f392730bb7b125b39c
lijunwei <ljw532344863@sina.com>
2022-09-25 17:17:49 +0800

    Commit automatically by watch

--- csapp/ch12-concurrent-programming/conc_race.c
+++ csapp/ch12-concurrent-programming/conc_race.c
@@ -1,9 +1,27 @@
 #include "csapp.h"
 
-#define N 4
+#define N 10
 
 void *thread(void *vargp);
 
-int main(int argc, char const *argv[]) { exit(0); }
+int main(int argc, char const *argv[]) {
+  pthread_t tid[N];
+  int i;
 
-void *thread(void *vargp) {}
+  for (i = 0; i < N; i++) {
+    Pthread_create(&tid[i], NULL, thread, &i);
+  }
+
+  for (i = 0; i < N; i++) {
+    Pthread_join(tid[i], NULL);
+  }
+
+  exit(0);
+}
+
+void *thread(void *vargp) {
+  int myid = *((int *)vargp);
+  printf("Hello from thread %d\n", myid);
+
+  return NULL;
+}
