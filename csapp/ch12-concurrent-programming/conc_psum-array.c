a03dd6628ef9421c18fbfd3fae816037e666b808
lijunwei <ljw532344863@sina.com>
2022-09-25 11:37:11 +0800

    Commit automatically by watch

---
+++ csapp/ch12-concurrent-programming/conc_psum-array.c
@@ -0,0 +1,61 @@
+#include "csapp.h"
+
+#define MAXTHREADS 32
+
+void *sum_array(void *vargp); // thread routine
+
+long gsum = 0;
+long nelems_per_thread;
+sem_t mutex;
+
+int main(int argc, char const *argv[]) {
+  long i;
+  long nelems;
+  long log_nelems;
+  long nthreads;
+  long myid[MAXTHREADS];
+
+  pthread_t tid[MAXTHREADS];
+
+  if(argc != 3) {
+    printf("Usage: %s <nthreads> <log_nelems>\n", argv[0]);
+    exit(0);
+  }
+
+  nthreads = atoi(argv[1]);
+  log_nelems = atoi(argv[2]);
+  nelems = (1L << log_nelems);
+  nelems_per_thread = nelems / nthreads;
+
+  long psum[nthreads];
+
+  for(i = 0; i < nthreads; i++) {
+    myid[i] = i;
+    Pthread_create(&tid[i], NULL, sum_array, &myid[i]);
+  }
+
+  for(i = 0; i < nthreads; i++) {
+    Pthread_join(tid[i], NULL);
+  }
+
+  // check the result
+  if(gsum != (nelems * (nelems - 1)) / 2) {
+    printf("Error: result=%ld\n", gsum);
+  }
+
+  exit(0);
+}
+
+void *sum_array(void *vargp) {
+  long myid = *((long *)vargp);          // extract thread id
+  long start = myid * nelems_per_thread; // start element index
+  long end = start + nelems_per_thread;  // end element index
+  long i;
+
+  for(i = start; i < end; i++) {
+    psum[myid] += i;
+  }
+
+  return NULL;
+}
+
