ca62d277dade03d2befd7deeb3cae35da44f3b0e
lijunwei <ljw532344863@sina.com>
2022-09-25 12:23:03 +0800

    Commit automatically by watch

---
+++ csapp/ch12-concurrent-programming/conc_psum-local.c
@@ -0,0 +1,72 @@
+#include "csapp.h"
+
+#define MAXTHREADS 32
+
+void *sum_array(void *vargp); // thread routine
+
+long gsum = 0;
+long nelems_per_thread;
+sem_t mutex;
+long psum[MAXTHREADS];
+
+int main(int argc, char const *argv[]) {
+  long i;
+  long nelems;
+  long log_nelems;
+  long nthreads;
+  long myid[MAXTHREADS];
+
+  pthread_t tid[MAXTHREADS];
+
+  if(argc != 3) {
+    printf("Usage: %s <nthreads> <log_nelems>\n", argv[0]);
+    exit(0);
+  }
+
+  nthreads = atoi(argv[1]);
+  log_nelems = atoi(argv[2]);
+  nelems = (1L << log_nelems);
+  nelems_per_thread = nelems / nthreads;
+
+  for(i = 0; i < nthreads; i++) {
+    myid[i] = i;
+    Pthread_create(&tid[i], NULL, sum_array, &myid[i]);
+  }
+
+  for(i = 0; i < nthreads; i++) {
+    Pthread_join(tid[i], NULL);
+  }
+
+  for(i = 0; i < nthreads; i++) {
+    gsum += psum[i];
+  }
+
+  // long expected = (nelems * (nelems - 1)) / 2; // overflow... print ((nelems * (nelems - 1)) / 2) => -2147483648
+  long expected = (nelems / 2) * (nelems - 1);
+
+  // check the result
+  if(gsum != expected) {
+    printf("Error: expected=%ld actual=%ld\n", expected, gsum);
+  } else {
+    printf("Looking good");
+  }
+
+  exit(0);
+}
+
+void *sum_array(void *vargp) {
+  long myid = *((long *)vargp);          // extract thread id
+  long start = myid * nelems_per_thread; // start element index
+  long end = start + nelems_per_thread;  // end element index
+  long i;
+  long sum = 0;
+
+  for(i = start; i < end; i++) {
+    sum += i;
+  }
+
+  psum[myid] += sum;
+
+  return NULL;
+}
+
