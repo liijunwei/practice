cfb1bdfa33ec57840a005f6b74addf180331bd02
lijunwei <ljw532344863@sina.com>
2022-09-29 22:36:48 +0800

    Commit automatically by watch

---
+++ golang/a-tour-of-go/interfaces5.go
@@ -0,0 +1,22 @@
+package main
+
+// Type assertions
+// https://go.dev/tour/methods/15
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	// f = i.(float64) // panic
+	// fmt.Println(f)
+}
