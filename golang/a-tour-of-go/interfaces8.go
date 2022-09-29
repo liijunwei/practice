eeda587ac90acc4d1d709fb1f123717b0a41825c
lijunwei <ljw532344863@sina.com>
2022-09-30 07:48:48 +0800

    Commit automatically by watch

---
+++ golang/a-tour-of-go/interfaces8.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+func run() (int, error) {
+	return 100, nil
+	return 100, &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+
+}
+
+func main() {
+	value, err := run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(value)
+}
