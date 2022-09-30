046fdeedc81837b6201f5cca3b17a1a123e34205
lijunwei <ljw532344863@sina.com>
2022-09-30 08:08:48 +0800

    Classic ruby developer xD

--- golang/a-tour-of-go/interfaces9.go
+++ golang/a-tour-of-go/interfaces9.go
@@ -7,17 +7,20 @@ import (
 )
 
 func Sqrt(x float64) (float64, error) {
-	if x < 0
+	if x < 0 {
 		return 0, ErrNegativeSqrt(x)
-	else
+	} else {
 		return math.Sqrt(x), nil
-	end
-
+	}
 }
 
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func (s ErrNegativeSqrt) String() string {
 	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
