071d8a99a57987703ca59fe82ca452b6bc85299b
lijunwei <ljw532344863@sina.com>
2022-09-30 08:18:48 +0800

    Commit automatically by watch

--- golang/a-tour-of-go/interfaces10.go
+++ golang/a-tour-of-go/interfaces10.go
@@ -7,11 +7,10 @@ import (
 )
 
 func main() {
-	// pry
-	//
+	// jj "Hello, Reader!".chars.index_by {|c| c.bytes}
 	r := strings.NewReader("Hello, Reader!")
 
-	b := make([]byte, 8)
+	b := make([]byte, 10)
 	for {
 		n, err := r.Read(b)
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
