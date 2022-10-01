537396c221ba2fd966c923d46ea130f2bb3e46f1
lijunwei <ljw532344863@sina.com>
2022-10-01 11:42:00 +0800

    Commit automatically by watch

--- golang/a-tour-of-go/channels01.go
+++ golang/a-tour-of-go/channels01.go
@@ -14,10 +14,12 @@ func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
 
 	c := make(chan int)
+
 	go sum(s[:len(s)/2], c)
 	go sum(s[len(s)/2:], c)
+
 	x := <-c // receive from c
 	y := <-c
 
-	fmt.Println(x, y, x+y)
+	fmt.Printf("partial_x: %d \npartial_y: %d \nsum: %d \n", x, y, x+y)
 }
