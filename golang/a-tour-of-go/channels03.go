6013f07ef3666928278740a7689d7a772d9b9905
lijunwei <ljw532344863@sina.com>
2022-10-01 11:52:00 +0800

    Commit automatically by watch

---
+++ golang/a-tour-of-go/channels03.go
@@ -0,0 +1,30 @@
+package main
+
+// https://go.dev/tour/concurrency/4
+// Only the sender should close a channel, never the receiver.
+// Sending on a closed channel will cause a panic.
+
+import (
+	"fmt"
+)
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	// close(c)
+}
+
+func main() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+
+	// The loop for i := range c receives values from the channel repeatedly until it is closed.
+	for i := range c {
+		fmt.Println(i)
+		close(c)
+
+	}
+}
