b7650ceff5b32a7ab39ebaf505fcca8242d269bd
lijunwei <ljw532344863@sina.com>
2022-10-02 21:46:23 +0800

    Commit automatically by watch

--- refactoring/ch06-composing-methods/05-replace-temp-with-chain-after.rb
+++ refactoring/ch06-composing-methods/05-replace-temp-with-chain-after.rb
@@ -1,16 +1,19 @@
 class Select
+  def self.with_option(option)
+    s = self.new
+    s.options << option
+    s
+  end
+
   def options
     @options ||= []
   end
 
-  def add_option(arg)
-    options << arg
+  def and(option)
+    options << option
+    self
   end
 end
 
-select = Select.new
-select.add_option(1999)
-select.add_option(2000)
-select.add_option(2001)
-select.add_option(2002)
+select = Select.with_option(1999).and(2000).and(2001).and(2002)
 p select
