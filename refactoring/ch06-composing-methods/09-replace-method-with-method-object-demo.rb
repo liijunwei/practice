e7503158d03ca695caab1e6a785866c0f928d323
lijunwei <ljw532344863@sina.com>
2022-10-03 11:48:58 +0800

    Add initializer

--- refactoring/ch06-composing-methods/09-replace-method-with-method-object-demo.rb
+++ refactoring/ch06-composing-methods/09-replace-method-with-method-object-demo.rb
@@ -1,6 +1,6 @@
 class Account
   def gamma(input_val, quantity, year_to_date)
-    inportant_value1 = (input_val * quantity) + delta
+    important_value1 = (input_val * quantity) + delta
     important_value2 = (input_val * year_to_date) + 100
     if (year_to_date - important_value1) > 100
       important_value2 -= 20
@@ -18,4 +18,11 @@ attr_reader :account,
             :important_value1,
             :important_value2,
             :important_value3
+
+  def initialize(account, input_val_arg, quantity_arg, year_to_date_arg)
+    @account = account
+    @input_val = input_val_arg
+    @quantity = quantity_arg
+    @year_to_date = year_to_date_arg
+  end
 end
