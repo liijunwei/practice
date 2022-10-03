1e63d867af6da05658e3954315861120440a6f4c
lijunwei <ljw532344863@sina.com>
2022-10-03 10:44:30 +0800

    Refactor Trace: support goroutine id

--- golang/time-geekbang-tony-bai/defer01.go
+++ golang/time-geekbang-tony-bai/defer01.go
@@ -1,6 +1,13 @@
 package main
 
-import "runtime"
+// https://time.geekbang.org/column/article/471138
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strconv"
+)
 
 func Trace() func() {
 	pc, _, _, ok := runtime.Caller(1)
@@ -11,8 +18,9 @@ func Trace() func() {
 	fn := runtime.FuncForPC(pc)
 	name := fn.Name()
 
-	println("enter: ", name)
-	return func() { println("exit:  ", name) }
+	gid := curGoroutineID()
+	fmt.Printf("g[%05d] enter: [%s]\n", gid, name)
+	return func() { fmt.Printf("g[%05d] exit:  [%s]\n", gid, name) }
 }
 
 func foo() {
@@ -22,6 +30,25 @@ func foo() {
 
 func bar() {
 	defer Trace()()
+}
+
+var goroutineSpace = []byte("goroutine ")
+
+func curGoroutineID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	// Parse the 4707 out of "goroutine 4707 ["
+	b = bytes.TrimPrefix(b, goroutineSpace)
+	i := bytes.IndexByte(b, ' ')
+	if i < 0 {
+		panic(fmt.Sprintf("No space found in %q", b))
+	}
+	b = b[:i]
+	n, err := strconv.ParseUint(string(b), 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse goroutine ID out of %q: %v", b, err))
+	}
+	return n
 }
 
 func main() {
