5ee382f34158309c603835e788b3024619e5dab9
lijunwei <ljw532344863@sina.com>
2022-10-03 10:38:38 +0800

    Refactor Trace: get func name with the help of runtime package

--- golang/time-geekbang-tony-bai/defer01.go
+++ golang/time-geekbang-tony-bai/defer01.go
@@ -1,22 +1,30 @@
 package main
 
-func Trace(name string) func() {
-	println("enter: ", name)
-	return func() {
-		println("exit: ", name)
+import "runtime"
+
+func Trace() func() {
+	pc, _, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("caller not found")
 	}
+
+	fn := runtime.FuncForPC(pc)
+	name := fn.Name()
+
+	println("enter: ", name)
+	return func() { println("exit:  ", name) }
 }
 
 func foo() {
-	defer Trace("foo")()
+	defer Trace()()
 	bar()
 }
 
 func bar() {
-	defer Trace("bar")()
+	defer Trace()()
 }
 
 func main() {
-	defer Trace("main")()
+	defer Trace()()
 	foo()
 }
