0cb599f77aa7bab603e922cb2b7a9475161b6643
lijunwei <ljw532344863@sina.com>
2022-10-05 17:59:48 +0800

    Use the Customer factory

--- refactoring/ch08-organizing-data/03-change-value-to-reference.rb
+++ refactoring/ch08-organizing-data/03-change-value-to-reference.rb
@@ -1,10 +1,10 @@
 class Order
   def initialize(customer_name)
-    @customer = Customer.new(customer_name)
+    @customer = Customer.create(customer_name)
   end
 
   def customer=(customer_name)
-    @customer = Customer.new(customer_name)
+    @customer = Customer.create(customer_name)
   end
 
   def customer_name
