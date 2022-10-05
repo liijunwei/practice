4ba328c518a192ddba6ccb1d5b00617c78860239
lijunwei <ljw532344863@sina.com>
2022-10-05 21:22:08 +0800

    Commit automatically by watch

--- refactoring/ch08-organizing-data/07-change-unidirectional-association-to-bidirectional-demo.rb
+++ refactoring/ch08-organizing-data/07-change-unidirectional-association-to-bidirectional-demo.rb
@@ -1,9 +1,33 @@
 class Order
   attr_accessor :customer
+
+  #controlling methods
+  def add_customer(customer)
+    customer.friend_orders.add(self)
+    @customers.add(customer)
+  end
+
+  def remove_customer(customer)
+    customer.friend_orders.subtract(self)
+    @customers.subtract(customer)
+  end
 end
 
 class Customer
- def initialize
+  def initialize
     @orders = Set.new
- end
+  end
+
+  def friend_orders
+    #should only be used by Order when modifying the association
+    @orders
+  end
+
+  def add_order(order)
+    order.add_customer(self)
+  end
+
+  def remove_order(order)
+    order.remove_customer(self)
+  end
 end
