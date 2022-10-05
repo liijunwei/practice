c7bb61f715cc2b002c82084d162e88bb53f3d9b4
lijunwei <ljw532344863@sina.com>
2022-10-05 19:03:38 +0800

    Refactoring

--- refactoring/ch08-organizing-data/06-replace-hash-with-object-demo.rb
+++ refactoring/ch08-organizing-data/06-replace-hash-with-object-demo.rb
@@ -37,7 +37,7 @@ RSpec.describe NetworkResult do
     network = Network.new("foobar")
     node = Node.new(network)
 
-    new_network = { :nodes => [], :old_networks => [] }
+    new_network = NetworkResult.new
     new_network[:old_networks] << node.network
     new_network[:nodes] << node
     new_network[:name] = new_network[:old_networks].collect do |network|
