59f6739a1d691432af046beeea5b8ac85ffb84fb
lijunwei <ljw532344863@sina.com>
2022-10-05 21:37:08 +0800

    Commit automatically by watch

--- refactoring/ch08-organizing-data/07-change-unidirectional-association-to-bidirectional-demo.rb
+++ refactoring/ch08-organizing-data/07-change-unidirectional-association-to-bidirectional-demo.rb
@@ -1,20 +1,6 @@
 require 'set'
 
 class Order
-  def initialize
-    @customer = nil
-  end
-
-  # controlling methods
-  def add_customer(customer)
-    customer.friend_orders.add(self)
-    @customer = customer
-  end
-
-  def remove_customer(customer)
-    customer.friend_orders.subtract(self)
-    @customer = nil
-  end
 end
 
 class Customer
@@ -24,33 +10,24 @@ class Customer
     @orders = Set.new
   end
 
-  def friend_orders
-    #should only be used by Order when modifying the association
-    @orders
-  end
-
   def add_order(order)
-    order.add_customer(self)
-  end
-
-  def remove_order(order)
-    order.remove_customer(self)
+    @orders << order
   end
 end
 
+# rspec 07-change-unidirectional-association-to-bidirectional-demo.rb
+RSpec.describe Order do
+end
 
-c = Customer.new
-o1 = Order.new
-o2 = Order.new
+RSpec.describe Customer do
+  it 'responds to orders' do
+    c = Customer.new
+    o1 = Order.new
+    o2 = Order.new
 
-c.add_order(o1)
-c.add_order(o2)
+    c.add_order(o1)
+    c.add_order(o2)
 
-p c
-p o1
-p o2
-puts "=" * 20
-
-p c.orders
-c.remove_order(o1)
-
+    expect(c.orders).to eq(Set.new([o1, o2]))
+  end
+end
