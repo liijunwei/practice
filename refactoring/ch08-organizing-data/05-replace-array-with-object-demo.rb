5af08b656901e8790525811b1fa89c4d299ec752
lijunwei <ljw532344863@sina.com>
2022-10-05 18:52:02 +0800

    Commit automatically by watch

--- refactoring/ch08-organizing-data/05-replace-array-with-object-demo.rb
+++ refactoring/ch08-organizing-data/05-replace-array-with-object-demo.rb
@@ -6,6 +6,18 @@ puts "before: #{row.inspect}"
 class Performance
   attr_accessor :name
   attr_accessor :wins
+
+  def initialize
+    @data = []
+  end
+
+  def []=(index, value)
+    @data.insert(index, value)
+  end
+
+  def [](index)
+    @data[index]
+  end
 end
 
 row = Performance.new
