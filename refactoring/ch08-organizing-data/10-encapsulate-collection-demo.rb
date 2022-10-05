061f9329a47dea64ec56413980e131d3d0479e27
lijunwei <ljw532344863@sina.com>
2022-10-05 22:30:09 +0800

    Add test case

--- refactoring/ch08-organizing-data/10-encapsulate-collection-demo.rb
+++ refactoring/ch08-organizing-data/10-encapsulate-collection-demo.rb
@@ -6,6 +6,10 @@ class Course
     @name = name
     @advanced = advanced
   end
+
+  def advanced?
+    @advanced
+  end
 end
 
 class Person
@@ -13,4 +17,27 @@ class Person
 end
 
 RSpec.describe Person do
+  specify do
+    kent = Person.new
+
+    courses = []
+    courses << Course.new("Smalltalk Programming", false)
+    courses << Course.new("Appreciating Single Malts", true)
+
+    kent.courses = courses
+
+    expect(kent.courses.size).to eq(2)
+
+    refactoring = Course.new("Refactoring", true)
+    kent.courses << refactoring
+    kent.courses << Course.new("Brutal Sarcasm", false)
+
+    expect(kent.courses.size).to eq(4)
+    expect(kent.courses.select {|c| c.advanced?}.size).to eq(2)
+
+    kent.courses.delete(refactoring)
+    expect(kent.courses.size).to eq(3)
+
+    expect(kent.courses.select {|c| c.advanced?}.size).to eq(1)
+  end
 end
