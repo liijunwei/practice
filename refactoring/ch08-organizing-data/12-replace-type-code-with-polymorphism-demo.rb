3646c1c3322d896cc60ebac474c72ec94f385c7d
lijunwei <ljw532344863@sina.com>
2022-10-06 22:39:04 +0800

    Commit automatically by watch

--- refactoring/ch08-organizing-data/12-replace-type-code-with-polymorphism-demo.rb
+++ refactoring/ch08-organizing-data/12-replace-type-code-with-polymorphism-demo.rb
@@ -2,7 +2,16 @@ require 'pry'
 require 'rspec'
 
 class MountainBike
+  TIRE_WIDTH_FACTOR = 1
+  FRONT_SUSPENSION_FACTOR = 1
+  REAR_SUSPENSION_FACTOR = 1
+
   def initialize(params)
+    @commission = 1
+    @base_price = 1
+    @front_suspension_price = 1
+    @rear_suspension_price = 1
+    @rear_fork_travel = 1
     params.each { |key, value| instance_variable_set "@#{key}", value }
   end
 
@@ -12,9 +21,11 @@ class MountainBike
     if @type_code == :front_suspension || @type_code == :full_suspension
       result += @front_fork_travel * FRONT_SUSPENSION_FACTOR
     end
+
     if @type_code == :full_suspension
       result += @rear_fork_travel * REAR_SUSPENSION_FACTOR
     end
+
     result
   end
 
@@ -25,11 +36,22 @@ class MountainBike
     when :front_suspension
       (1 + @commission) * @base_price + @front_suspension_price
     when :full_suspension
-      (1 + @commission) * @base_price + @front_suspension_price +
-      @rear_suspension_price
+      (1 + @commission) * @base_price + @front_suspension_price + @rear_suspension_price
     end
   end
 end
 
 RSpec.describe MountainBike do
+  specify do
+    bike1 = MountainBike.new(:type_code => :rigid, :tire_width => 2.5)
+    bike2 = MountainBike.new(:type_code => :front_suspension, :tire_width => 2, :front_fork_travel => 3)
+    bike3 = MountainBike.new(:type_code => :full_suspension, :tire_width => 2, :front_fork_travel => 3)
+
+    bike1.off_road_ability
+    bike1.price
+    bike2.off_road_ability
+    bike2.price
+    bike3.off_road_ability
+    bike3.price
+  end
 end
