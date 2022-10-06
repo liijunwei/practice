89853c6ab1310ed6f720f253d55bfa88eff42e22
lijunwei <ljw532344863@sina.com>
2022-10-06 22:45:24 +0800

    Refactoring

--- refactoring/ch08-organizing-data/12-replace-type-code-with-polymorphism-demo.rb
+++ refactoring/ch08-organizing-data/12-replace-type-code-with-polymorphism-demo.rb
@@ -28,29 +28,30 @@ module MountainBike
 
     result
   end
-
-  def price
-    case @type_code
-    when :rigid
-      (1 + @commission) * @base_price
-    when :front_suspension
-      (1 + @commission) * @base_price + @front_suspension_price
-    when :full_suspension
-      (1 + @commission) * @base_price + @front_suspension_price + @rear_suspension_price
-    end
-  end
 end
 
 class RigidMountainBike
   include MountainBike
+
+  def price
+    (1 + @commission) * @base_price
+  end
 end
 
 class FrontSuspensionMountainBike
   include MountainBike
+
+  def price
+    (1 + @commission) * @base_price + @front_suspension_price
+  end
 end
 
 class FullSuspensionMountainBike
   include MountainBike
+
+  def price
+    (1 + @commission) * @base_price + @front_suspension_price + @rear_suspension_price
+  end
 end
 
 RSpec.describe MountainBike do
