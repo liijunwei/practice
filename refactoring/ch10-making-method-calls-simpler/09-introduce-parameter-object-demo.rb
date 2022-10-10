268cccddef316217e089262051ce45358bf17aac
lijunwei <ljw532344863@sina.com>
2022-10-10 21:39:49 +0800

    Commit automatically by watch

---
+++ refactoring/ch10-making-method-calls-simpler/09-introduce-parameter-object-demo.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require 'rspec'
+
+class Account
+  def initialize
+    @charges = []
+  end
+
+  def add_charge(base_price, tax_rate, imported)
+    total = base_price + base_price * tax_rate
+    total += base_price * 0.1 if imported
+    @charges << total
+  end
+
+  def total_charge
+    @charges.inject(0) { |total, charge| total + charge }
+  end
+end
+
+RSpec.describe Account do
+  let(:account) { subject }
+  specify do
+    account.add_charge(5, 0.1, true)
+    account.add_charge(12, 0.125, false)
+    total = account.total_charge
+    expect(total).to eq(19.5)
+  end
+end
--- refactoring/ch10-making-method-calls-simpler/09-introduce-parameter-object.md
+++ refactoring/ch10-making-method-calls-simpler/09-introduce-parameter-object.md
@@ -1,3 +1,7 @@
 You have a group of parameters that naturally go together.
 
 *Replace them with an object.*
+
++ Such a group of classes is a **data clump** and can be replaced with an object that carries all of this data.
+
++ You get a deeper benefit, however, because once you have clumped together the parameters, you soon see behavior that **you can also move into the new class**.
