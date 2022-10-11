0e144c9a056ad89cf4ec38acbae2fad43c3215b9
lijunwei <ljw532344863@sina.com>
2022-10-11 22:45:18 +0800

    Commit automatically by watch

--- refactoring/ch11-dealing-with-generalization/09-replace-inheritance-with-delegation-demo.rb
+++ refactoring/ch11-dealing-with-generalization/09-replace-inheritance-with-delegation-demo.rb
@@ -17,3 +17,32 @@ class Policy < Hash
     end
   end
 end
+
+class Rule
+  attr_reader :attribute, :default_value
+
+  def initialize(attribute, default_value)
+    @attribute, @default_value = attribute, default_value
+  end
+
+  def apply(account)
+    "applying rule #{attribute} for #{account}"
+  end
+end
+
+require 'pry'
+require 'rspec'
+
+RSpec.describe Policy do
+end
+
+RSpec.describe Rule do
+  let(:account) { 'foobar' }
+
+  specify do
+    rule = described_class.new('age_must_be_greater_than', 18)
+    expect(rule.attribute).to eq('age_must_be_greater_than')
+    expect(rule.default_value).to eq(18)
+    expect(rule.apply(account)).to eq("applying rule age_must_be_greater_than for foobar")
+  end
+end
