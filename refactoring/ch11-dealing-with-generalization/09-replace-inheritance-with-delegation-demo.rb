f4bc5129bc22e53f743c673ff5bdc3f7041e5181
lijunwei <ljw532344863@sina.com>
2022-10-11 22:50:18 +0800

    Freeze the code behavior: prepare for refactoring

--- refactoring/ch11-dealing-with-generalization/09-replace-inheritance-with-delegation-demo.rb
+++ refactoring/ch11-dealing-with-generalization/09-replace-inheritance-with-delegation-demo.rb
@@ -34,15 +34,25 @@ require 'pry'
 require 'rspec'
 
 RSpec.describe Policy do
+  let(:account) { 'foobar@policy' }
+
+  specify do
+    policy = Policy.new('kyc_terms')
+    policy << Rule.new('age_must_be_greater_than', 18)
+    policy << Rule.new('country_must_be', 'china')
+    policy << Rule.new('account_balance_must_be_greater_than', 10_000)
+
+    policy.apply(account)
+  end
 end
 
 RSpec.describe Rule do
-  let(:account) { 'foobar' }
+  let(:account) { 'foobar@rule' }
 
   specify do
     rule = described_class.new('age_must_be_greater_than', 18)
     expect(rule.attribute).to eq('age_must_be_greater_than')
     expect(rule.default_value).to eq(18)
-    expect(rule.apply(account)).to eq("applying rule age_must_be_greater_than for foobar")
+    expect(rule.apply(account)).to eq("applying rule age_must_be_greater_than for foobar@rule")
   end
 end
