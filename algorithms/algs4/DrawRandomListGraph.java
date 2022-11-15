ae12fb0b1a08039e9e00f28392640aa76a90c707
lijunwei <ljw532344863@sina.com>
2022-11-15 22:47:17 +0800

    Add another demo

---
+++ algorithms/algs4/DrawRandomListGraph.java
@@ -0,0 +1,23 @@
+// https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/BinarySearch.java.html
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class DrawRandomListGraph {
+    public static void main(String[] args) {
+        int N = 50;
+        double[] a = new double[N];
+
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.random();
+        }
+
+        for (int i = 1; i <= N; i++) {
+            double x = 1.0 * i / N;
+            double y = a[i] / 2.0;
+            double rw = 0.5 / N;
+            double rh = a[i] / 2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+}
