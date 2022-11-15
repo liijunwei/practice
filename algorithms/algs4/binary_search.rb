86c5d3637e635418b054f1b92aee643456013896
lijunwei <ljw532344863@sina.com>
2022-11-15 21:49:47 +0800

    Add more implementation

--- algorithms/algs4/binary_search.rb
+++ algorithms/algs4/binary_search.rb
@@ -18,19 +18,30 @@ def rank(key, sorted_list)
   return -1
 end
 
+def simple_rank(key, sorted_list)
+  sorted_list.each_with_index do |e, i|
+    return i if e == key
+  end
+
+  return -1
+end
+
 require 'test/unit'
 
 class TestRank < Test::Unit::TestCase
   def setup
-    @list = [1, 2, 3, 4]
+    @list = [1, 2, 3, 4].sort
   end
 
   def test_key_found
     assert_equal rank(1, @list), 0
     assert_equal rank(3, @list), 2
+    assert_equal simple_rank(1, @list), 0
+    assert_equal simple_rank(3, @list), 2
   end
 
   def test_key_not_found
     assert_equal rank(999, @list), -1
+    assert_equal simple_rank(999, @list), -1
   end
 end
