393b78d6086d673d74a4f8914572bccab2100039
lijunwei <ljw532344863@sina.com>
2022-11-15 22:29:03 +0800

    Add another demo

---
+++ algorithms/algs4/RandomSeq.java
@@ -0,0 +1,20 @@
+// https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/BinarySearch.java.html
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomSeq {
+  public static void main(String[] args) {
+    int N = Integer.parseInt(args[0]);
+
+    double lo = Double.parseDouble(args[1]);
+    double hi = Double.parseDouble(args[2]);
+
+    for(int i = 0; i < N; i++) {
+      double x = StdRandom.uniform(lo, hi);
+      StdOut.printf("%.2f\n", x);
+    }
+  }
+}
