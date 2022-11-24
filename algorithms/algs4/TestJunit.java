f2e1180d0739be4f560ce8f7d78323eaba7e019a
lijunwei <ljw532344863@sina.com>
2022-11-24 23:19:47 +0800

    Introduce junit test framework

---
+++ algorithms/algs4/TestJunit.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+    @Test
+    public void testSetup() {
+        String str = "I am done with Junit setup";
+        assertEquals("I am done with Junit setup", str);
+    }
+
+    @Test
+    public void testStringOperation() {
+        String a = "now is ";
+        String b = "the time ";
+        String c = "to";
+
+        assertEquals(a.length(), 7);
+        assertEquals(a.charAt(4), 'i');
+        assertEquals(a.concat(c), "now is to");
+        assertEquals(a.indexOf("is"), 4);
+        assertEquals(a.substring(2, 5), "w i");
+        assertEquals(a.split(" ")[0], "now");
+        assertEquals(a.split(" ")[1], "is");
+        assertEquals(b.equals(c), false);
+    }
+}
