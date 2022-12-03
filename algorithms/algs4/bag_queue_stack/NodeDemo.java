423ecafa06216462ae7898db269cc4ee4d96b183
lijunwei <ljw532344863@sina.com>
2022-12-03 16:39:51 +0800

    Add test case for inserting at head of linked-list

--- algorithms/algs4/bag_queue_stack/NodeDemo.java
+++ algorithms/algs4/bag_queue_stack/NodeDemo.java
@@ -30,6 +30,30 @@ public class NodeDemo<Item> {
         assertNull(third.next);
     }
 
+    @Test
+    public void testInsertFirstNode() {
+        Node<String> first = new Node<>();
+        Node<String> second = new Node<>();
+        Node<String> third = new Node<>();
+
+        first.item = "to";
+        second.item = "be";
+        third.item = "or";
+
+        first.next = second;
+        second.next = third;
+        third.next = null;
+
+        Node<String> oldfirst = first;
+        first = new Node<>();
+        first.item = "inserted first";
+        first.next = oldfirst;
+
+        assertEquals("inserted first", first.item);
+        assertEquals("to", first.next.item);
+        assertEquals(second, first.next.next);
+    }
+
     private static class Node<Item> {
         Item item;
         Node<Item> next;
