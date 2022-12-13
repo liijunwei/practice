a88afa0be7c7c06e872a55b38d23fba990e105ec
lijunwei <ljw532344863@sina.com>
2022-12-13 22:45:13 +0800

    Add quick union algorithm

---
+++ algorithms/algs4/union_find/UF.java
@@ -0,0 +1,67 @@
+package union_find;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class UF {
+    private int[] id;
+    private int count;
+
+    public UF(int N) {
+        this.count = N;
+        this.id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    private int find(int p) {
+        return id[p];
+    }
+
+    // quick union
+    public void union(int p, int q) {
+        int pID = find(p);
+        int qID = find(q);
+
+        if (pID == qID) {
+            return;
+        }
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pID) {
+                id[i] = qID;
+            }
+        }
+
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+
+            if (uf.connected(p, q)) {
+                continue;
+            }
+
+            uf.union(p, q);
+
+            StdOut.println(p + " " + q);
+        }
+
+        StdOut.println(uf.count + " components");
+    }
+}
