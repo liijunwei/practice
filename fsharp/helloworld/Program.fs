f9ecde0001294d4dd5139f510cb06f7ea9d6e495
lijunwei <ljw532344863@sina.com>
2023-01-01 10:02:26 +0800

    Update example

--- fsharp/helloworld/Program.fs
+++ fsharp/helloworld/Program.fs
@@ -1,4 +1,6 @@
-﻿
+﻿// https://dotnet.microsoft.com/en-us/learn/languages/fsharp-hello-world-tutorial/modify
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let PrintGreeting name = printfn $"Hello {name} from F#!"
+PrintGreeting "junwei"
+
+[1..100] |> List.sum |> printfn "sum=%d"
