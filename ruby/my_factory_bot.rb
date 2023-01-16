27382de021515583b587df71db8e9be15891f939
liijunwei <ljw532344863@sina.com>
2023-01-16 10:24:19 +0800

    WIP

--- ruby/my_factory_bot.rb
+++ ruby/my_factory_bot.rb
@@ -1,12 +1,21 @@
+class User
+  attr_accessor :name
+  attr_accessor :email
+end
+
 class MyFactoryBot
   def self.define
+  end
+
+  def self.create(model_sym)
+    User.new
   end
 end
 
 MyFactoryBot.define do
   factory :user do
-    name { FFaker::Name.name }
-    email { FFaker::Internet.unique.email }
+    name { 'Bogs' }
+    email { 'demo@example.com' }
   end
 end
 
@@ -15,4 +24,11 @@ require 'pry'
 
 # rspec my_factory_bot.rb
 RSpec.describe MyFactoryBot do
+  describe '.create' do
+    it 'creates a new instance of User' do
+      user = described_class.create(:user)
+      expect(user.name).to eq('Bogs')
+      expect(user.email).to eq('demo@example.com')
+    end
+  end
 end
