24f1a0d7c3ccde42f9a80aa3397be3b4217ff663
lijunwei <ljw532344863@sina.com>
2023-02-10 08:18:06 +0800

    Add one more test case

--- algorithms/algs4/exercises/EX1_3_4.java
+++ algorithms/algs4/exercises/EX1_3_4.java
@@ -1,17 +1,22 @@
-package bag_queue_stack;
+package exercises;
 
+import bag_queue_stack.LinkedListStack;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 // practice 1.3.4
-public class Parentheses {
+public class EX1_3_4 {
     public static boolean foo(LinkedListStack<String> s1, String[] tokens) {
         for (String item : tokens) {
             if (item.equals("[") || item.equals("{") || item.equals("(")) {
                 s1.push(item);
                 continue;
+            }
+
+            if (s1.isEmpty()) {
+                return false;
             }
 
             String popStr = s1.pop();
@@ -63,6 +68,14 @@ public class Parentheses {
     @Test
     public void testBalancedParentheses3() {
         String input = "[()]{}{[()()]()}[";
+        String[] t = input.split("(?!^)");
+        LinkedListStack<String> s = new LinkedListStack();
+        assertFalse(foo(s, t));
+    }
+
+    @Test
+    public void testBalancedParentheses_WithEmptyStack() {
+        String input = "][";
         String[] t = input.split("(?!^)");
         LinkedListStack<String> s = new LinkedListStack();
         assertFalse(foo(s, t));
