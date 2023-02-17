fce0a4fd0285db1c92c4b3e136a7a2b29c46045c
liijunwei <ljw532344863@sina.com>
2023-02-17 23:20:16 +0800

    Add selection sort ruby implementation

---
+++ algorithms/algs4/SelectionSorter.rb
@@ -0,0 +1,34 @@
+class SelectionSorter
+  def sort(a)
+    length = a.length
+
+    a.each_with_index do |e, i|
+      min_index = i
+
+      j = i + 1
+
+      while (j < length)
+        min_index = j if a[j] < a[min_index]
+        j += 1
+      end
+
+      a[min_index], a[i] = a[i], a[min_index]
+    end
+
+    a
+  end
+end
+
+require 'test/unit'
+
+# ruby SelectionSorter.rb
+class TestSelectionSorter < Test::Unit::TestCase
+  def setup
+    @list1 = ["S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"]
+    @list2 = ["A", "E", "E", "L", "M", "O", "P", "R", "S", "T", "X"]
+  end
+
+  def test_sort_work_as_expected
+    assert_equal @list2, SelectionSorter.new.sort(@list1)
+  end
+end
