fb3528dc9ce7886cd25502c4a7591f53e13daed0
lijunwei <ljw532344863@sina.com>
2023-02-19 10:33:16 +0800

    Add notes

--- algorithms/algs4/MergeSorter.java
+++ algorithms/algs4/MergeSorter.java
@@ -19,24 +19,24 @@ public class MergeSorter {
     }
 
     public static void merge(Comparable[] a, int lo, int mid, int hi) {
-        int i = lo;
-        int j = mid + 1;
+        int i = lo; // 左半边的cursor
+        int j = mid + 1; // 右半边的cursor
 
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k]; // 将a[lo..hi] 复制到aux[lo..hi]
         }
 
         for (int k = lo; k <= hi; k++) {
-            if (i > mid) {
+            if (i > mid) { // 左半边用尽，取右半边的元素
                 a[k] = aux[j];
                 j++;
-            } else if (j > hi) {
+            } else if (j > hi) { // 右半边用尽，取左半边的元素
                 a[k] = aux[i];
                 i++;
-            } else if (less(aux[i], aux[j])) {
+            } else if (less(aux[j], aux[i])) { // 右半边当前元素小于左半边的当前元素，取右半边的元素
                 a[k] = aux[j];
                 j++;
-            } else {
+            } else { // 右半边当前元素大于等于左半边当前元素，取左半边的元素
                 a[k] = aux[i];
                 i++;
             }
