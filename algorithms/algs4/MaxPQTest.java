4cb47326fa59149b087306faf8b6a8ed3048954f
liijunwei <ljw532344863@sina.com>
2023-02-26 10:33:50 +0800

    Add test cases

--- algorithms/algs4/MaxPQTest.java
+++ algorithms/algs4/MaxPQTest.java
@@ -1,14 +1,17 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class MaxPQTest {
     @Test
-    public void testMaxPQArraySize() {
+    public void testMaxPQArrayQueryMethods() {
         MaxPQArray<Integer> pq = new MaxPQArray<>(10);
+        assertTrue(pq.isEmpty());
+
         pq.insert(1);
         pq.insert(10);
         pq.insert(2);
+        assertFalse(pq.isEmpty());
         assertEquals(3, pq.size());
     }
 }
