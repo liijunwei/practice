c93b90cc76b6e4990622f5046734d9f1c2410cee
liijunwei <ljw532344863@sina.com>
2023-02-26 11:07:56 +0800

    Add test cases

--- algorithms/algs4/MaxPQArray.java
+++ algorithms/algs4/MaxPQArray.java
@@ -1,28 +1,50 @@
 public class MaxPQArray<Key extends Comparable<Key>> {
     private Key[] pq;
-    private int N = 0;
+    private int size = 0;
 
     public MaxPQArray(int maxN) {
         pq = (Key[]) new Comparable[maxN + 1];
     }
 
     public boolean isEmpty() {
-        return N == 0;
+        return size == 0;
     }
 
     public int size() {
-        return N;
+        return size;
     }
 
     public void insert(Key v) {
-        pq[++N] = v;
+        pq[size++] = v;
     }
 
     public Key delMax() {
-        return null;
+        int maxIndex = 0;
+
+        for (int i = maxIndex + 1; i < size; i++) {
+            if (less(pq[maxIndex], pq[i])) {
+                maxIndex = i;
+            }
+        }
+
+        Key max = pq[maxIndex];
+        exch(pq, maxIndex, size - 1);
+        size--;
+
+        return max;
     }
 
     public Key max() {
         return null;
+    }
+
+    private boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private void exch(Object[] a, int i, int j) {
+        Object tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
 }
--- algorithms/algs4/MaxPQTest.java
+++ algorithms/algs4/MaxPQTest.java
@@ -14,4 +14,15 @@ public class MaxPQTest {
         assertFalse(pq.isEmpty());
         assertEquals(3, pq.size());
     }
+
+    @Test
+    public void testMaxPQArrayDeleteMax() {
+        MaxPQArray<Integer> pq = new MaxPQArray<>(10);
+        pq.insert(1);
+        pq.insert(10);
+        pq.insert(2);
+
+        assertEquals(Integer.valueOf(10), pq.delMax());
+        assertEquals(Integer.valueOf(2), pq.delMax());
+    }
 }
