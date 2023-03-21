8edf7e865eaf1cdbe1a32bbe60a398ca32df0d2b
liijunwei <ljw532344863@sina.com>
2023-03-21 22:04:46 +0800

    Add RedBlackBST

--- algorithms/algs4/RedBlackBST.java
+++ algorithms/algs4/RedBlackBST.java
@@ -121,4 +121,24 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         StdOut.println(x.key);
         print(x.right);
     }
+
+    public Value get(Key key) {
+        return get(this.root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
+    }
 }
--- algorithms/algs4/TestRedBlackBST.java
+++ algorithms/algs4/TestRedBlackBST.java
@@ -31,4 +31,22 @@ public class TestRedBlackBST {
         assertEquals(10, tree.size());
         tree.print();
     }
+    @Test
+    public void testGet() {
+        RedBlackBST<String, String> tree = new RedBlackBST();
+        tree.put("A", "foo1");
+        tree.put("C", "foo2");
+        tree.put("E", "foo3");
+        tree.put("H", "foo4");
+        tree.put("L", "foo5");
+        tree.put("M", "foo6");
+        tree.put("P", "foo7");
+        tree.put("R", "foo8");
+        tree.put("S", "foo9");
+        tree.put("X", "foo10");
+
+        assertEquals(10, tree.size());
+        assertEquals("foo10", tree.get("X"));
+        assertEquals("foo5", tree.get("L"));
+    }
 }
