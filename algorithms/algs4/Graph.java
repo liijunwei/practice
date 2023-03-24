5b4c71ccee06d5748027009a8222b58ca38c7b37
liijunwei <ljw532344863@sina.com>
2023-03-24 21:44:44 +0800

    Add Graph data structure

---
+++ algorithms/algs4/Graph.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Graph {
+    private final int V; // 顶点个数
+    private int E; // 边的条数
+    private Bag<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        this.adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            this.adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    private void addEdge(int v, int w) {
+        this.adj[v].add(w);
+        this.adj[w].add(v);
+        this.E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return this.adj[v];
+    }
+
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        StdOut.println(G);
+    }
+}
---
+++ algorithms/algs4/GraphTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GraphTest {
+    @Test
+    public void testDate() {
+        assertTrue(true);
+    }
+}
