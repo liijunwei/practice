659eceede9bb933b3fd3c282c28c190e970f769e
liijunwei <ljw532344863@sina.com>
2023-03-24 22:01:54 +0800

    Add Graph static methods

--- algorithms/algs4/Graph.java
+++ algorithms/algs4/Graph.java
@@ -34,6 +34,43 @@ public class Graph {
         return E;
     }
 
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v)) {
+            degree++;
+        }
+
+        return degree;
+    }
+
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.V(); v++) {
+            if (degree(G, v) > max) {
+                max = degree(G, v);
+            }
+        }
+
+        return max;
+    }
+
+    public static double avgDegree(Graph G) {
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++) {
+            for (int w : G.adj(v)) {
+                if (v == w) {
+                    count++;
+                }
+            }
+        }
+
+        return count / 2; // 每条边被记两次
+    }
+
     private void addEdge(int v, int w) {
         this.adj[v].add(w);
         this.adj[w].add(v);
--- algorithms/algs4/GraphTest.java
+++ algorithms/algs4/GraphTest.java
@@ -1,10 +1,44 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GraphTest {
     @Test
-    public void testDate() {
+    public void testTinyG() {
+        In in = new In("data/tinyG.txt");
+        Graph G = new Graph(in);
+        StdOut.println(G);
+
         assertTrue(true);
+    }
+
+    @Test
+    public void testMediumG() {
+        In in = new In("data/mediumG.txt");
+        Graph G = new Graph(in);
+        StdOut.println(G);
+
+        assertTrue(true);
+    }
+
+
+    @Test
+    public void testStaticMethods() {
+        In in = new In("data/tinyG.txt");
+        Graph G = new Graph(in);
+
+        assertEquals(4, Graph.degree(G, 0));
+        assertEquals(1, Graph.degree(G, 1));
+        assertEquals(1, Graph.degree(G, 2));
+        assertEquals(2, Graph.degree(G, 3));
+        assertEquals(3, Graph.degree(G, 4));
+
+        assertEquals(4, Graph.maxDegree(G));
+        assertEquals(2.0, Graph.avgDegree(G), 0.001);
+
+        assertEquals(0, Graph.numberOfSelfLoops(G));
     }
 }
