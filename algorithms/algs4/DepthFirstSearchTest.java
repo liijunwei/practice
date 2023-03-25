d6065f8a845ae9231e0b9d00fe8f3b010c599949
liijunwei <ljw532344863@sina.com>
2023-03-25 08:00:04 +0800

    Add DFS algorithm

---
+++ algorithms/algs4/DepthFirstSearchTest.java
@@ -0,0 +1,36 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DepthFirstSearchTest {
+    private DepthFirstSearch searcherFor(String inputFile, int source) {
+        In in = new In(inputFile);
+        Graph G = new Graph(in);
+
+        DepthFirstSearch search = new DepthFirstSearch(G, source);
+
+        for (int v = 0; v < G.V(); v++) {
+            if (search.marked(v)) {
+                StdOut.print(v + " ");
+            }
+        }
+        StdOut.println();
+
+        if (search.count() != G.V()) {
+            StdOut.print("NOT ");
+        }
+
+        StdOut.println("connected");
+
+        return search;
+    }
+
+    @Test
+    public void testSearch() {
+        assertEquals(7, searcherFor("data/tinyG.txt", 0).count());
+        assertEquals(4, searcherFor("data/tinyG.txt", 9).count());
+        assertEquals(2, searcherFor("data/tinyG.txt", 7).count());
+    }
+}
