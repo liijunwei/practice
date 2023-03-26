4ac63e7859f280d2361b478de32ab456256117ea
lijunwei <ljw532344863@sina.com>
2023-03-26 22:25:35 +0800

    Add test cases

--- algorithms/algs4/ruby-lab/depth_first_search.rb
+++ algorithms/algs4/ruby-lab/depth_first_search.rb
@@ -16,6 +16,10 @@ class DepthFirstSearch
     @count
   end
 
+  def connected?
+    count == @graph.V
+  end
+
   private
 
   def dfs(graph, v)
@@ -32,21 +36,75 @@ class DepthFirstSearch
 end
 
 require_relative './bit_matrix'
+require 'rspec'
 
 # The order of a graph is its number of vertices |V|
 # The size of a graph is its number of edges |E|
 
-v, e, *edges = File.readlines("data/tinyG.txt")
+# v, e, *edges = File.readlines("data/tinyG.txt")
 
-V = v.to_i
-E = e.to_i
-graph = BitMatrix.new(V)
-edges.map {|e| e.strip.split(" ")}.each {|from, to| graph.set(from.to_i, to.to_i) }
+# V = v.to_i
+# E = e.to_i
+# graph = BitMatrix.new(V)
+# edges.map {|e| e.strip.split(" ")}.each {|from, to| graph.set(from.to_i, to.to_i) }
 
-source = ARGV[0].to_i
-search = DepthFirstSearch.new(graph, source)
+# source = ARGV[0].to_i
+# search = DepthFirstSearch.new(graph, source)
 
-graph.vertices.select {|v| search.marked(v)}.each {|v| print("#{v} ")}
-puts
-print("NOT ") if search.count != graph.V
-puts "connected"
+# graph.vertices.select {|v| search.marked(v)}.each {|v| print("#{v} ")}
+# puts
+# print("NOT ") if search.count != graph.V
+# puts "connected"
+
+# rspec ruby-lab/depth_first_search.rb
+RSpec.describe DepthFirstSearch do
+  v, _e, *edges = File.readlines("data/tinyG.txt")
+
+  let(:graph) { BitMatrix.new(v.to_i) }
+  let(:search) { DepthFirstSearch.new(graph, source) }
+
+  before do
+    edges.map {|e| e.strip.split(" ")}.each {|from, to| graph.set(from.to_i, to.to_i) }
+  end
+
+  context 'when source is 0' do
+    let(:source) { 0 }
+
+    specify do
+      expect(graph.vertices.select {|v| search.marked(v)}.sort).to eq([0,1,2,3,4,5,6])
+      expect(search.connected?).to eq(false)
+    end
+  end
+
+  context 'when source is 7' do
+    let(:source) { 7 }
+
+    specify do
+      expect(graph.vertices.select {|v| search.marked(v)}.sort).to eq([7,8])
+      expect(search.connected?).to eq(false)
+    end
+  end
+
+  context 'when source is 9' do
+    let(:source) { 9 }
+
+    specify do
+      expect(graph.vertices.select {|v| search.marked(v)}.sort).to eq([9,10,11,12])
+      expect(search.connected?).to eq(false)
+    end
+  end
+
+  context 'when new edges added' do
+    before do
+      graph.set(6, 7)
+      graph.set(8, 9)
+    end
+
+    let(:source) { 0 }
+
+    specify do
+      expect(graph.vertices.select {|v| search.marked(v)}.sort).to eq([0,1,2,3,4,5,6,7,8,9,10,11,12])
+      expect(search.connected?).to eq(true)
+    end
+  end
+end
