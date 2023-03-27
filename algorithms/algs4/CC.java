93ca3a544d4b85ccbd3e7b0cd9598516f4f8a8bf
liijunwei <ljw532344863@sina.com>
2023-03-27 21:37:13 +0800

    Add CC

---
+++ algorithms/algs4/CC.java
@@ -0,0 +1,38 @@
+public class CC {
+    private final boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public CC(Graph g) {
+        this.marked = new boolean[g.V()];
+        this.id = new int[g.V()];
+
+        for (int s = 0; s < g.V(); s++) {
+            dfs(g, s);
+            count++;
+        }
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        id[v] = count;
+
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                dfs(g, w);
+            }
+        }
+    }
+
+    public boolean connected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+
+    public int count() {
+        return count;
+    }
+}
