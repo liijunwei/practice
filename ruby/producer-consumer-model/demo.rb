56b01ec06e33ed67d7f380bcc5f6c832a9415f4f
liijunwei <ljw532344863@sina.com>
2023-03-29 15:05:08 +0800

    try another way(why it's sooooooo slow)

--- ruby/producer-consumer-model/demo.rb
+++ ruby/producer-consumer-model/demo.rb
@@ -13,35 +13,33 @@ end
 
 def t_producer
   loop do
-    LOCK.synchronize do
-      if can_produce?
-        @count += 1
-        print('(')
-      end
+    LOCK.lock
+    if can_produce?
+      @count += 1
+      print('(')
     end
+    LOCK.unlock
   end
 end
 
 def t_consumer
   loop do
-    LOCK.synchronize do
-      if can_consume?
-        @count -= 1
-        print(')')
-      end
+    LOCK.lock
+    if can_consume?
+      @count -= 1
+      print(')')
     end
+    LOCK.unlock
   end
 end
 
 @count = 0
 @num_of_nest = ARGV[0].to_i
-@num_of_thread = ARGV[1].to_i
-
-# require "pry"; binding.pry
+num_of_thread = ARGV[1].to_i
 
 threads = []
 
-@num_of_thread.times do
+num_of_thread.times do
   threads << Thread.new { t_producer }
   threads << Thread.new { t_consumer }
 end
