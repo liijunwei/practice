446af8430d6b2d12d537b763d42a5f2af741065f
lijunwei <ljw532344863@sina.com>
2023-04-19 23:34:52 +0800

    Add WIP

--- algorithms/algs4/longest_repeat_string.rb
+++ algorithms/algs4/longest_repeat_string.rb
@@ -1,9 +1,65 @@
-def get_suffix_array_of(string)
-  upper_limit = string.size - 1
-  string.chars.map.with_index {|_char, index| {k: index, v: string[index..upper_limit]}}.sort_by {|e| e[:v]}
+
+# TODO make this right
+
+class SuffixArray
+  attr_reader :suffixes
+
+  def initialize(text)
+    @suffixes = get_suffixes_of(text)
+  end
+
+  def index(i)
+    suffixes[i][:k]
+  end
+
+  def lcp(i)
+    lcp_suffix(suffixes[i][:v], suffixes[i - 1][:v])
+  end
+
+  def lcp_suffix(suffix_s, suffix_t)
+    max = [suffix_s.size, suffix_t.size].max
+
+    max.times do |i|
+      if suffix_s[i] != suffix_t[i]
+        return i
+      end
+    end
+
+    max
+  end
+
+  def select(i)
+    suffixes[i][:v]
+  end
+
+  private
+
+  def get_suffixes_of(text)
+    upper_limit = text.size - 1
+    text.chars.map.with_index {|_char, index| {k: index, v: text[index..upper_limit]}}.sort_by {|e| e[:v]}
+  end
 end
 
-get_suffix_array_of('HORSE') # => ["E", "HORSE", "ORSE", "RSE", "SE"]
-get_suffix_array_of('camel') # => ["amel", "camel", "el", "l", "mel"]
-get_suffix_array_of('to be or not to be')
-get_suffix_array_of('aacaagtttacaagc')
+class LRS
+  def execute
+    text = ARGF.read
+
+    n = text.size
+    sa = SuffixArray.new(text)
+    lrs = ""
+
+    n.times do |i|
+      length = sa.lcp(i)
+
+      if(length > lrs.size)
+        lrs = sa.select(i)[0..length]
+      end
+    end
+
+    puts "text:\n#{text}"
+    puts "lrs:\n#{lrs}"
+    puts
+  end
+end
+
+LRS.new.execute
