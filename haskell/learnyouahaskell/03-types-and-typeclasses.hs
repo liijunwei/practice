43069fdeb2599696f083ef2bdd48bbf62279a6b5
lijunwei <ljw532344863@sina.com>
2023-04-22 16:20:58 +0800

    Add notes for learnyouahaskell ch03

--- haskell/learnyouahaskell/03-types-and-typeclasses.hs
+++ haskell/learnyouahaskell/03-types-and-typeclasses.hs
@@ -24,3 +24,52 @@
 -- Functions also have types
 let tripleMe x = x * 3
 :t tripleMe
+
+-- how do we write out the type of a function that takes several parameters?
+
+-- The parameters are separated with -> and there's no special distinction between the parameters and the return type.
+-- **The return type is the last item in the declaration** and the parameters are the first three.
+-- Later on we'll see why they're all just separated with -> instead of having some more explicit distinction between the return types and the parameters like Int, Int, Int -> Int or something.
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+-- a can be of any type.
+:t head
+-- head :: [a] -> a
+
+-- Functions that have type variables are called polymorphic functions. 多态函数
+-- The type declaration of head states that it takes a list of any type and returns one element of that type.
+
+-- ### Typeclasses 101
+
+-- A typeclass is a sort of interface that defines some behavior.
+-- If a type is a part of a typeclass, that means that it supports and implements the behavior the typeclass describes.
+-- typeclass 类似于java里的interface，但是更好(better) (不明白...)
+
+:t (==)
+-- (==) :: Eq a => a -> a -> Bool
+-- Everything before the => symbol is called a class constraint.
+-- the equality function takes any two values that are of the same type and returns a Bool.
+-- The type of those two values must be a member of the Eq class (this was the class constraint).
+
+:t elem
+-- elem :: (Foldable t, Eq a) => a -> t a -> Bool
+
+-- Some basic typeclasses:
+    -- `Eq` is used for types that support equality testing.
+    -- `Ord` is for types that have an ordering.
+    -- Members of `Show` can be presented as strings.
+    -- `Read` is sort of the opposite typeclass of Show
+    -- `Enum` members are sequentially ordered types — they can be enumerated
+    -- `Bounded` members have an upper and a lower bound.
+    -- `Num` is a numeric typeclass.
+
+:t compare
+
+read "1.4" :: Float
+read "True" :: Bool
+
+-- successors and predecesors
+succ 'B'
+pred 'B'
+
