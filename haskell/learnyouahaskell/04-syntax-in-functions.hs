aea80a256331b069384a2d9d54874bed238ff4b7
lijunwei <ljw532344863@sina.com>
2023-04-24 21:30:34 +0800

    Add notes for 04-syntax-in-functions

---
+++ haskell/learnyouahaskell/04-syntax-in-functions.hs
@@ -0,0 +1,33 @@
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+
+-- bmiTell :: (RealFloat a) => a -> String
+-- bmiTell bmi
+--     | bmi <= 18.5 = "You're underweight, you emo, you!"
+--     | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+--     | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+--     | otherwise   = "You're a whale, congratulations!"
+
+-- bmiTell :: (RealFloat a) => a -> a -> String
+-- bmiTell weight height
+--     | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+--     | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+--     | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+--     | otherwise                 = "You're a whale, congratulations!"
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
