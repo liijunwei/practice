38ce39adfeeb58b59974ed1bd36a865add7d1eff
liijunwei <ljw532344863@sina.com>
2023-04-29 23:23:06 +0800

    Add notes for 07-modules

--- haskell/learnyouahaskell/07-modules.hs
+++ haskell/learnyouahaskell/07-modules.hs
@@ -15,4 +15,5 @@ numUniques = length . nub
 -- selectively import
 import Data.List (nub, sort)
 
-
+findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+phoneBook = [("betty","555-2938"),("bonnie","452-2928"),("patsy","493-2928"),("lucille","205-2928"),("wendy","939-8282"),("penny","853-2492")]
