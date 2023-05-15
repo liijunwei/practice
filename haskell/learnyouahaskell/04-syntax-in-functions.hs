af1cd1ec258be23ce2f1d4454c87d7d157980604
lijunwei <ljw532344863@sina.com>
2023-05-15 22:54:10 +0800

    Commit manually

--- haskell/learnyouahaskell/04-syntax-in-functions.hs
+++ haskell/learnyouahaskell/04-syntax-in-functions.hs
@@ -2,10 +2,23 @@ lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"
 
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "one"
+sayMe 2 = "two"
+sayMe 3 = "three"
+sayMe 4 = "four"
+sayMe 5 = "five"
+sayMe x = "not between 1 and 5" -- "Pattern match is redundant" if this line is at top
+
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
+addVectors1 :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors1 a b = (fst a + fst b, snd a + snd b)
+
+addVectors2 :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors2 (x1, y1) (x2, y2) = (x1+x2, y1+y2)
 
 -- bmiTell :: (RealFloat a) => a -> String
 -- bmiTell bmi
