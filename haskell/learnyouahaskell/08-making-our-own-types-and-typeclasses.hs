75cb6e3e4f348cced708e0cf0d0eef55910728d9
junwei <ljw532344863@sina.com>
2023-05-25 23:34:09 +0800

    Add hashkell examples

--- haskell/learnyouahaskell/08-making-our-own-types-and-typeclasses.hs
+++ haskell/learnyouahaskell/08-making-our-own-types-and-typeclasses.hs
@@ -267,3 +267,35 @@ lockers = Map.fromList
 -- lockerLookup 105 lockers
 -- lockerLookup 109 lockers
 -- lockerLookup 110 lockers
+
+data List a = Empty | MyCon { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)
+
+-- Empty
+-- 3 `Cons` (4 `Cons` (5 `Cons` Empty))
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+
+-- let nums = [8,6,4,1,7,3,5]
+-- let numsTree = foldr treeInsert EmptyTree nums
+-- numsTree
+-- 8 `treeElem` numsTree
+-- 888 `treeElem` numsTree
+
