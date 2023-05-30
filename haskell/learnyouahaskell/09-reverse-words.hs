7ed4fa415314e69c42a6a3b288e48916b6ab8268
junwei <ljw532344863@sina.com>
2023-05-30 08:55:20 +0800

    Add todo

--- haskell/learnyouahaskell/09-reverse-words.hs
+++ haskell/learnyouahaskell/09-reverse-words.hs
@@ -1,7 +1,8 @@
 -- ghc --make 09-reverse-words.hs -o a.out
 
 reverseWords :: String -> String
-reverseWords = unwords . map reverse . words
+reverseWords = unwords . map reverse . words -- TODO I forgot what does this mean, roughly function composition
+                                             -- http://learnyouahaskell.com/higher-order-functions#composition
 
 main = do
   line <- getLine
