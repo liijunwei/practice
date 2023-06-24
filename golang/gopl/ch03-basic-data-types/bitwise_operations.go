ba5655afcb707e767807e3f70edd29fe4ee5a402
junwei <ljw532344863@sina.com>
2023-06-24 17:12:26 +0800

    Add examples in book

---
+++ golang/gopl/ch03-basic-data-types/bitwise_operations.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\n", y)
+	fmt.Printf("%08b\n", x&y)
+	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x&^y) // bit clear ???
+}
---
+++ golang/gopl/ch03-basic-data-types/note.md
@@ -0,0 +1,1 @@
++ Go’s types fall into four categories: basic types, aggregate types, reference types, and interface types.
