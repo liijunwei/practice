457a07c08e3c8c68d5dfd2ddeb408ff3e0b8470b
junwei <ljw532344863@sina.com>
2023-08-04 07:06:34 +0800

    Add demo for go example

---
+++ golang/mess/example/example_demo_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// https://pkg.go.dev/testing#hdr-Examples
+func ExampleHello() {
+	fmt.Println("hello")
+	// Output: hello
+}
+
+func ExampleSalutations() {
+	fmt.Println("hello, and")
+	fmt.Println("goodbye")
+	// Output:
+	// hello, and
+	// goodbye
+}
