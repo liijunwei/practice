0b27b5006ad3a01defb426d75493757c66b55556
junwei <ljw532344863@sina.com>
2023-08-12 22:29:15 +0800

    Simplify

--- golang/mess/cmd/graph_based_scheduling/main.go
+++ golang/mess/cmd/graph_based_scheduling/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
-
-	"github.com/dominikbraun/graph"
 )
 
 const (
@@ -16,96 +13,17 @@ const (
 )
 
 func main() {
-	nodeHash := func(c WorkNode) string {
-		return c.Name
+	var taskGraph = map[string][]string{
+		"t2": {"t1"},
+		"t3": {"t1"},
+		"t4": {"t3"},
 	}
 
-	g1 := graph.New(nodeHash, graph.Directed(), graph.PreventCycles())
-	n1 := WorkNode{
-		Name:     "task1",
-		Duration: 10 * time.Millisecond,
-		Status:   "hold",
-	}
-
-	n2 := WorkNode{
-		Name:     "task2",
-		Duration: 20 * time.Millisecond,
-		Status:   "hold",
-	}
-
-	n3 := WorkNode{
-		Name:     "task3",
-		Duration: 30 * time.Millisecond,
-		Status:   "hold",
-	}
-
-	n4 := WorkNode{
-		Name:     "task4",
-		Duration: 40 * time.Millisecond,
-		Status:   "hold",
-	}
-
-	_ = g1.AddVertex(n1)
-	_ = g1.AddVertex(n2)
-	_ = g1.AddVertex(n3)
-	_ = g1.AddVertex(n4)
-
-	_ = g1.AddEdge(n1.Name, n2.Name)
-	_ = g1.AddEdge(n1.Name, n3.Name)
-	_ = g1.AddEdge(n3.Name, n4.Name)
-
-	order, _ := graph.TopologicalSort(g1)
-	fmt.Println(order)
-
-	// adjMap, err := g1.AdjacencyMap()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-	queue := make(chan string)
-
-	go func() {
-		produce(queue, g1)
-		wg.Done()
-	}()
-
-	go func() {
-		consume(queue, g1)
-		wg.Done()
-	}()
-
-	wg.Wait()
+	fmt.Println(taskGraph)
 }
 
 type WorkNode struct {
 	Name     string
 	Duration time.Duration
 	Status   string
-}
-
-func produce(queue chan<- string, _graph graph.Graph[string, WorkNode]) {
-	for i := 0; i < 10; i++ {
-		queue <- fmt.Sprintf("task#%d", i)
-	}
-
-	close(queue)
-}
-
-func consume(queue <-chan string, _graph graph.Graph[string, WorkNode]) {
-	for {
-		select {
-		case task, ok := <-queue:
-			if ok {
-				fmt.Printf("working on %s\n", task)
-			} else {
-				fmt.Println("queue closed")
-				return
-			}
-		default:
-			fmt.Println("idle...")
-			time.Sleep(100 * time.Millisecond)
-		}
-	}
 }
