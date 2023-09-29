7ab40b0690971f3f00dac3066782565476abb483
junwei <ljw532344863@sina.com>
2023-09-29 08:31:47 +0800

    Commit manually

--- golang/recursion/decimal2binary/d2b.go
+++ golang/recursion/decimal2binary/d2b.go
@@ -1,5 +1,17 @@
 package decimal2binary
 
-func d2b(input string) string {
-	return input
+import "fmt"
+
+func d2b(input int, result string) string {
+	if input == 0 && result == "" {
+		return "0"
+	}
+
+	if input == 0 && result != "" {
+		return result
+	}
+
+	result = fmt.Sprintf("%d", input%2) + result
+
+	return d2b(input/2, result)
 }
--- golang/recursion/decimal2binary/d2b_test.go
+++ golang/recursion/decimal2binary/d2b_test.go
@@ -1,29 +1,47 @@
 package decimal2binary
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_d2b(t *testing.T) {
 	type test struct {
-		input string
+		input int
 		want  string
 	}
 
 	tests := []test{
 		{
-			input: "0",
+			input: 0,
 			want:  "0",
 		},
 		{
-			input: "1",
+			input: 1,
 			want:  "1",
+		},
+		{
+			input: 2,
+			want:  "10",
+		},
+		{
+			input: 3,
+			want:  "11",
+		},
+		{
+			input: 100,
+			want:  "1100100",
+		},
+		{
+			input: 99999,
+			want:  "11000011010011111",
 		},
 	}
 
 	for _, tt := range tests {
 		tt := tt
 
-		if got := d2b(tt.input); tt.want != got {
-			t.Fatalf("input: %s, want: %v, got: %v", tt.input, tt.want, got)
+		if got := d2b(tt.input, ""); got != tt.want {
+			t.Fatalf("input: %v, want: %v, got: %v", tt.input, tt.want, got)
 		}
 	}
 }
