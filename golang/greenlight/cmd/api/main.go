ac6cae2633fc2fcdc76f678575ec5a2fb322a705
liijunwei <ljw532344863@sina.com>
2024-09-15 23:42:34 +0800

    feat: Bootstrap health check api

--- golang/greenlight/cmd/api/main.go
+++ golang/greenlight/cmd/api/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
 
 func main() {
-	fmt.Println("first line")
+	var cfg config
+	flag.StringVar(&cfg.env, "env", "development", "environment(development|staging|production)")
+	flag.StringVar(&cfg.ip, "ip", "localhost", "the server ip address")
+	flag.IntVar(&cfg.port, "port", 4000, "api server port")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	app := &application{
+		config: cfg,
+		logger: *logger,
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf("%s:%d", cfg.ip, cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, server.Addr)
+	err := server.ListenAndServe()
+	logger.Fatal(err)
+}
+
+type config struct {
+	ip   string
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger log.Logger
+}
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "status: available\n")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
 }
