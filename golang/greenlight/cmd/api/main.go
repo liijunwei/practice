4655c211a8b2430e9887d5b226dd4fc0878ec146
liijunwei <ljw532344863@sina.com>
2024-09-15 23:57:00 +0800

    chore: Add app api routers

--- golang/greenlight/cmd/api/main.go
+++ golang/greenlight/cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -23,8 +24,8 @@ func main() {
 		config: cfg,
 		logger: *logger,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	mux := app.routes()
 
 	server := &http.Server{
 		Addr:         fmt.Sprintf("%s:%d", cfg.ip, cfg.port),
@@ -54,4 +55,85 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintf(w, "status: available\n")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
+}
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// borrowed from: https://github.com/benhoyt/go-routing/blob/master/stdlib/route.go
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /contact", contact)
+	mux.HandleFunc("GET /api/widgets", apiGetWidgets)
+	mux.HandleFunc("POST /api/widgets", apiCreateWidget)
+	mux.HandleFunc("POST /api/widgets/{slug}", apiUpdateWidget)
+	mux.HandleFunc("POST /api/widgets/{slug}/parts", apiCreateWidgetPart)
+	mux.HandleFunc("POST /api/widgets/{slug}/parts/{id}/update", apiUpdateWidgetPart)
+	mux.HandleFunc("POST /api/widgets/{slug}/parts/{id}/delete", apiDeleteWidgetPart)
+	mux.HandleFunc("GET /{slug}", widgetGet)
+	mux.HandleFunc("GET /{slug}/admin", widgetAdmin)
+	mux.HandleFunc("POST /{slug}/image", widgetImage)
+
+	return mux
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "home\n")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "contact\n")
+}
+
+func apiGetWidgets(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "apiGetWidgets\n")
+}
+
+func apiCreateWidget(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "apiCreateWidget\n")
+}
+
+func apiUpdateWidget(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	fmt.Fprintf(w, "apiUpdateWidget %s\n", slug)
+}
+
+func apiCreateWidgetPart(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	fmt.Fprintf(w, "apiCreateWidgetPart %s\n", slug)
+}
+
+func apiUpdateWidgetPart(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "apiUpdateWidgetPart %s %d\n", slug, id)
+}
+
+func apiDeleteWidgetPart(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "apiDeleteWidgetPart %s %d\n", slug, id)
+}
+
+func widgetGet(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	fmt.Fprintf(w, "widget %s\n", slug)
+}
+
+func widgetAdmin(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	fmt.Fprintf(w, "widgetAdmin %s\n", slug)
+}
+
+func widgetImage(w http.ResponseWriter, r *http.Request) {
+	slug := r.PathValue("slug")
+	fmt.Fprintf(w, "widgetImage %s\n", slug)
 }
