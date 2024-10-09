3c9cc198927b2e879fa40c479971da74d995966d
junwei <ljw532344863@sina.com>
2024-10-09 22:44:37 +0800

    Add learning notes

--- postgresql/practices2.sql
+++ postgresql/practices2.sql
@@ -293,3 +293,267 @@ SELECT date_trunc('year', TIMESTAMP '2001-02-16 20:38:40');
 SELECT date_trunc('day', TIMESTAMP WITH TIME ZONE '2001-02-16 20:38:40+00');
 SELECT date_trunc('day', TIMESTAMP WITH TIME ZONE '2001-02-16 20:38:40+00', 'Australia/Sydney');
 SELECT date_trunc('hour', INTERVAL '3 days 02:47:33');
+
+-- chapter9
+CREATE TABLE meat_poultry_egg_inspect (
+    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+    company varchar(100),
+    street varchar(100),
+    city varchar(30),
+    st varchar(2),
+    zip varchar(5),
+    phone varchar(14),
+    grant_date date,
+    activities text,
+    dbas text
+);
+
+COPY meat_poultry_egg_inspect
+FROM '/Users/lijunwei/OuterGitRepo/practical-sql/Chapter_09/MPI_Directory_by_Establishment_Name.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+CREATE INDEX company_idx ON meat_poultry_egg_inspect (company);
+
+select * from meat_poultry_egg_inspect
+SELECT count(*) FROM meat_poultry_egg_inspect;
+
+SELECT company,
+       street,
+       city,
+       st as state,
+       count(*) AS address_count
+FROM meat_poultry_egg_inspect
+GROUP BY company, street, city, st
+HAVING count(*) > 1
+ORDER BY company, street, city, st;
+
+SELECT company,
+       street,
+       city,
+       st as state,
+       count(*) AS address_count
+FROM meat_poultry_egg_inspect
+GROUP BY 1,2,3,4
+HAVING count(*) > 1
+ORDER BY 2,3,4,1
+
+SELECT company,
+       street,
+       city,
+       st as state
+FROM meat_poultry_egg_inspect
+ORDER BY street, city, st, company;
+
+
+SELECT st,
+       count(*) AS st_count
+FROM meat_poultry_egg_inspect
+GROUP BY st
+ORDER BY st;
+
+SELECT est_number,
+       company,
+       city,
+       st,
+       zip
+FROM meat_poultry_egg_inspect
+WHERE st IS NULL;
+
+SELECT company,
+       count(*) AS company_count
+FROM meat_poultry_egg_inspect
+GROUP BY company
+ORDER BY company ASC;
+
+SELECT length(zip),
+       count(*) AS length_count
+FROM meat_poultry_egg_inspect
+GROUP BY length(zip)
+ORDER BY length(zip) ASC;
+
+SELECT st,
+       count(*) AS st_count
+FROM meat_poultry_egg_inspect
+WHERE length(zip) < 5
+GROUP BY st
+ORDER BY st ASC;
+
+-- Listing 9-8: Backing up a table
+
+CREATE TABLE meat_poultry_egg_inspect_backup AS
+SELECT * FROM meat_poultry_egg_inspect;
+drop table if exists m1; create table m1 as select * from meat_poultry_egg_inspect; select count(*) from m1;
+drop table if exists m2; create table m2 as select * from meat_poultry_egg_inspect; select count(*) from m2;
+drop table if exists m3; create table m3 as select * from new_york_addresses; select count(*) from new_york_addresses;
+drop table if exists m4; create table m4 as select * from new_york_addresses; select count(*) from new_york_addresses;
+
+SELECT 
+    (SELECT count(*) FROM meat_poultry_egg_inspect) AS original,
+    (SELECT count(*) FROM meat_poultry_egg_inspect_backup) AS backup;
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN st_copy varchar(2);
+alter table meat_poultry_egg_inspect drop column st_copy;
+alter table meat_poultry_egg_inspect add column st_copy varchar(2)
+
+select count(st_copy) from meat_poultry_egg_inspect
+UPDATE meat_poultry_egg_inspect SET st_copy = st;
+
+SELECT st,
+       st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st;
+
+SELECT st,
+       st_copy
+FROM meat_poultry_egg_inspect
+group by 1,2
+ORDER BY st;
+
+SELECT distinct st,
+       st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st;
+
+SELECT distinct st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st_copy;
+
+select distinct st from meat_poultry_egg_inspect where est_number = 'V18677A'
+select distinct st from meat_poultry_egg_inspect where est_number = 'M45319+P45319'
+select distinct st from meat_poultry_egg_inspect where est_number = 'M263A+P263A+V263A'
+
+update meat_poultry_egg_inspect set st = 'MN' where est_number = 'V18677A'
+UPDATE meat_poultry_egg_inspect SET st = 'AL' WHERE est_number = 'M45319+P45319';
+UPDATE meat_poultry_egg_inspect SET st = 'WI' WHERE est_number = 'M263A+P263A+V263A';
+
+-- Restoring original st column values
+UPDATE meat_poultry_egg_inspect SET st = st_copy;
+
+-- Restoring from the column backup
+UPDATE meat_poultry_egg_inspect original
+SET st = backup.st
+FROM meat_poultry_egg_inspect_backup backup
+WHERE original.est_number = backup.est_number;
+
+select distinct company_standard from meat_poultry_egg_inspect where company like 'Armour%'
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN company_standard varchar(100);
+
+UPDATE meat_poultry_egg_inspect SET company_standard = company;
+
+UPDATE meat_poultry_egg_inspect
+SET company_standard = 'Armour-Eckrich Meats'
+WHERE company LIKE 'Armour%';
+
+SELECT company, company_standard
+FROM meat_poultry_egg_inspect
+WHERE company LIKE 'Armour%';
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN zip_copy varchar(5);
+UPDATE meat_poultry_egg_inspect
+SET zip_copy = zip;
+
+SELECT zip, zip_copy
+FROM meat_poultry_egg_inspect
+WHERE st IN('PR','VI') AND length(zip) = 3;
+
+UPDATE meat_poultry_egg_inspect
+SET zip = '00' || zip
+WHERE st IN('PR','VI') AND length(zip) = 3;
+
+SELECT zip, zip_copy
+FROM meat_poultry_egg_inspect
+WHERE st IN('CT','MA','ME','NH','NJ','RI','VT') AND length(zip) = 4;
+
+UPDATE meat_poultry_egg_inspect
+SET zip = '0' || zip
+WHERE st IN('CT','MA','ME','NH','NJ','RI','VT') AND length(zip) = 4;
+
+------------------
+
+CREATE TABLE state_regions (
+    st varchar(2) CONSTRAINT st_key PRIMARY KEY,
+    region varchar(20) NOT NULL
+);
+
+COPY state_regions
+FROM '/Users/lijunwei/OuterGitRepo/practical-sql/Chapter_09/state_regions.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+select * from state_regions;
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN inspection_date date;
+ALTER TABLE meat_poultry_egg_inspect drop COLUMN inspection_date
+
+SELECT inspection_date, count(*) from meat_poultry_egg_inspect
+group by inspection_date
+
+SELECT inspection_date
+FROM meat_poultry_egg_inspect inspect
+WHERE EXISTS (SELECT state_regions.region
+              FROM state_regions
+              WHERE inspect.st = state_regions.st 
+              AND state_regions.region = 'New England');
+
+UPDATE meat_poultry_egg_inspect inspect
+SET inspection_date = '2019-12-01'
+WHERE EXISTS (SELECT state_regions.region
+              FROM state_regions
+              WHERE inspect.st = state_regions.st 
+              AND state_regions.region = 'New England');
+
+SELECT st, inspection_date
+FROM meat_poultry_egg_inspect
+GROUP BY st, inspection_date
+ORDER BY st;
+
+select * FROM meat_poultry_egg_inspect
+WHERE st IN('PR','VI');
+
+DELETE FROM meat_poultry_egg_inspect
+WHERE st IN('PR','VI');
+
+ALTER TABLE meat_poultry_egg_inspect DROP COLUMN zip_copy;
+DROP TABLE meat_poultry_egg_inspect_backup;
+
+-- Demonstrating a transaction block
+START TRANSACTION; COMMIT;
+START TRANSACTION;
+
+UPDATE meat_poultry_egg_inspect
+SET company = 'AGRO Merchantss Oakland LLC'
+WHERE company = 'AGRO Merchants Oakland, LLC';
+
+-- view changes
+SELECT company
+FROM meat_poultry_egg_inspect
+WHERE company LIKE 'AGRO%'
+ORDER BY company;
+
+ROLLBACK;
+
+SELECT company
+FROM meat_poultry_egg_inspect
+WHERE company LIKE 'AGRO%'
+ORDER BY company;
+
+START TRANSACTION;
+
+UPDATE meat_poultry_egg_inspect
+SET company = 'AGRO Merchants Oakland LLC'
+WHERE company = 'AGRO Merchants Oakland, LLC';
+
+COMMIT
+
+CREATE TABLE meat_poultry_egg_inspect_backup AS
+SELECT *,
+       '2018-02-07'::date AS reviewed_date
+FROM meat_poultry_egg_inspect;
+
+select * from meat_poultry_egg_inspect_backup;
+
+-- Listing 9-26: Swapping table names using ALTER TABLE
+ALTER TABLE meat_poultry_egg_inspect RENAME TO meat_poultry_egg_inspect_temp;
+ALTER TABLE meat_poultry_egg_inspect_backup RENAME TO meat_poultry_egg_inspect;
+ALTER TABLE meat_poultry_egg_inspect_temp RENAME TO meat_poultry_egg_inspect_backup;
+
