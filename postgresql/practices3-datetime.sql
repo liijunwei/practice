c83a97ea0a422aa79ba6a9f7a2458b2a4b7f990a
junwei <ljw532344863@sina.com>
2024-10-12 18:00:00 +0800

    Add notes for SQL subquery/CTE/crosstab

--- postgresql/practices3-datetime.sql
+++ postgresql/practices3-datetime.sql
@@ -282,6 +282,10 @@ SELECT segment,
 FROM train_rides;
 
 -- chapter12
+-- A subquery is nested inside another query.
+
+-- Say you wanted to write a query to show which U.S. counties are at or above the 90th percentile, or top 10 percent, for population.
+-- You can convert the CTE to a scalar subquery if it makes the query more straightforward
 SELECT geo_name,
        state_us_abbreviation,
        p0010001
@@ -290,6 +294,38 @@ WHERE p0010001 >= (
 	    SELECT percentile_cont(.9) WITHIN GROUP (ORDER BY p0010001)
 	    FROM us_counties_2010
     )
+ORDER BY p0010001 DESC;
+
+-- unclear about the difference yet
+with t(percentile_value) as (
+	    SELECT percentile_cont(.9) WITHIN GROUP (ORDER BY p0010001)
+	    FROM us_counties_2010
+    )
+SELECT geo_name,
+       state_us_abbreviation,
+       p0010001
+FROM us_counties_2010
+WHERE p0010001 >= (select percentile_value from t)
+ORDER BY p0010001 DESC;
+
+-- join the CTE table(NICE)
+WITH t AS (
+    SELECT percentile_cont(.9) WITHIN GROUP (ORDER BY p0010001) AS percentile_value
+    FROM us_counties_2010
+)
+SELECT geo_name, state_us_abbreviation, p0010001
+FROM us_counties_2010
+JOIN t ON p0010001 >= t.percentile_value
+ORDER BY p0010001 DESC;
+
+-- join CTE without any condition
+WITH t AS (
+    SELECT percentile_cont(.9) WITHIN GROUP (ORDER BY p0010001) AS percentile_value
+    FROM us_counties_2010
+)
+SELECT geo_name, state_us_abbreviation, p0010001, t.percentile_value
+FROM us_counties_2010
+JOIN t ON true
 ORDER BY p0010001 DESC;
 
 -- TODO Q: when to use subquery and when to use CTE?
@@ -308,9 +344,12 @@ WHERE p0010001 < (
 
 SELECT count(*) FROM us_counties_2010_top10;
 
+-- If your subquery returns rows and columns of data, you can convert that data to a table by placing it in a FROM clause, the result of which is known as a derived table.
+-- A derived table behaves just like any other table, so you can query it or join it to other tables, even other derived tables.
 SELECT round(calcs.average, 0) as average,
        calcs.median,
-       round(calcs.average - calcs.median, 0) AS median_average_diff
+	   round(calcs.average - calcs.median, 2) AS average_median_diff,
+	   round(calcs.average / calcs.median, 2) AS average_median_ratio
 FROM (
      SELECT avg(p0010001) AS average,
             percentile_cont(.5)
@@ -318,3 +357,314 @@ FROM (
      FROM us_counties_2010
      )
 AS calcs;
+
+-- Joining Derived Tables
+-- Because derived tables behave like regular tables, you can join them.
+SELECT census.st AS st,
+       census.st_population,
+       plants.plant_count,
+       round((plants.plant_count/census.st_population::numeric(10,1)) * 1000000, 1)
+           AS plants_per_million
+FROM
+    (
+         SELECT st,
+                count(*) AS plant_count
+         FROM meat_poultry_egg_inspect
+         GROUP BY st
+    )
+    AS plants
+JOIN
+    (
+        SELECT state_us_abbreviation st,
+               sum(p0010001) AS st_population
+        FROM us_counties_2010
+        GROUP BY st
+    )
+    AS census
+ON plants.st = census.st
+ORDER BY plants_per_million DESC;
+
+-- rewrite with CTE
+with
+plants as (
+	SELECT st, count(*) AS plant_count
+	FROM meat_poultry_egg_inspect
+	GROUP BY st
+),
+census as (
+	SELECT state_us_abbreviation as st, sum(p0010001) AS st_population
+	FROM us_counties_2010
+	GROUP BY st
+)
+SELECT census.st AS st,
+       census.st_population,
+       plants.plant_count,
+       round((plants.plant_count/census.st_population::numeric(10,1)) * 1000000, 1)
+           AS plants_per_million
+FROM plants JOIN census
+ON plants.st = census.st
+ORDER BY plants_per_million DESC;
+
+-- generating columns with subqueires
+SELECT geo_name,
+       state_us_abbreviation AS st,
+       p0010001 AS total_pop,
+       (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY p0010001)
+        FROM us_counties_2010) AS us_median
+FROM us_counties_2010;
+
+with t(us_median) as (
+	SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY p0010001)
+	FROM us_counties_2010
+)
+SELECT geo_name,
+       state_us_abbreviation AS st,
+       p0010001 AS total_pop,
+       t.us_median
+FROM us_counties_2010
+join t on true;
+
+ -- looks like cross join is another option, but not sure which one is easier to understand
+with t(us_median) as (
+	SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY p0010001)
+	FROM us_counties_2010
+)
+SELECT geo_name,
+       state_us_abbreviation AS st,
+       p0010001 AS total_pop,
+       t.us_median
+FROM us_counties_2010
+cross join t;
+
+-- calculate the diff between total and mdeian
+SELECT geo_name,
+       state_us_abbreviation AS st,
+       p0010001 AS total_pop,
+       (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY p0010001)
+        FROM us_counties_2010) AS us_median,
+       p0010001 - (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY p0010001)
+                   FROM us_counties_2010) AS diff_from_median
+FROM us_counties_2010
+WHERE (p0010001 - (SELECT percentile_cont(.5) WITHIN GROUP (ORDER BY p0010001)
+                   FROM us_counties_2010))
+       BETWEEN -1000 AND 1000;
+
+CREATE TABLE retirees (
+    id int,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+
+truncate table retirees;
+drop table retirees;
+select * from retirees;
+
+INSERT INTO retirees 
+VALUES (2, 'Lee', 'Smith'),
+       (4, 'Janet', 'King');
+
+-- Generating values for the IN operator
+SELECT first_name, last_name
+FROM employees
+WHERE emp_id IN (
+    SELECT id
+    FROM retirees);
+
+SELECT first_name, last_name
+FROM employees
+WHERE EXISTS ( -- EXISTS check the boolean result
+    SELECT id
+    FROM retirees);
+
+-- mimic the behavior of IN query above
+-- This approach is particularly helpful if you need to join on more than one column, which you can’t do with the IN expression.
+SELECT first_name, last_name
+FROM employees
+WHERE EXISTS (
+    SELECT id
+    FROM retirees
+    WHERE id = employees.emp_id);
+
+SELECT first_name, last_name
+FROM employees
+WHERE NOT EXISTS (
+    SELECT id
+    FROM retirees
+    WHERE id = employees.emp_id);
+
+-- CTE: Common Table Expression
+-- Also, the column list is optional if you’re not renaming columns, although including the list is still a good idea for clarity even if you don’t rename columns.
+WITH large_counties (geo_name, st, p0010001) AS (
+	SELECT geo_name, state_us_abbreviation, p0010001
+	FROM us_counties_2010
+	WHERE p0010001 >= 100000
+)
+SELECT st, count(*)
+FROM large_counties
+GROUP BY st
+ORDER BY count(*) DESC;
+
+SELECT state_us_abbreviation, count(*)
+FROM us_counties_2010
+WHERE p0010001 >= 100000
+GROUP BY state_us_abbreviation
+ORDER BY count(*) DESC;
+
+-- why use CTE?
+-- 1. pre-stage subsets of data to feed into larger query for more complex analysis, can reuse the table defined by CTE
+-- 2. improve readability
+-- 3. use CTE to simplify queries with redundant code
+WITH
+    counties (st, population) AS
+    (SELECT state_us_abbreviation, sum(population_count_100_percent)
+     FROM us_counties_2010
+     GROUP BY state_us_abbreviation),
+    plants (st, plants) AS
+    (SELECT st, count(*) AS plants
+     FROM meat_poultry_egg_inspect
+     GROUP BY st)
+SELECT counties.st,
+       population,
+       plants,
+       round((plants/population::numeric(10,1))*1000000, 1) AS per_million
+FROM counties JOIN plants
+ON counties.st = plants.st
+ORDER BY per_million DESC;
+
+WITH us_median AS 
+    (SELECT percentile_cont(.5)
+     WITHIN GROUP (ORDER BY p0010001) AS us_median_pop
+     FROM us_counties_2010)
+SELECT geo_name,
+       state_us_abbreviation AS st,
+       p0010001 AS total_pop,
+       us_median_pop,
+       p0010001 - us_median_pop AS diff_from_median 
+FROM us_counties_2010 CROSS JOIN us_median -- make sure "cross join" here is well understood
+WHERE (p0010001 - us_median_pop)
+       BETWEEN -1000 AND 1000;
+
+-- Cross Tabulations
+-- https://www.postgresql.org/docs/current/tablefunc.html
+create extension tablefunc;
+
+CREATE TABLE ice_cream_survey (
+    response_id integer PRIMARY KEY,
+    office varchar(20),
+    flavor varchar(20)
+);
+
+copy ice_cream_survey
+from '/Users/lijunwei/OuterGitRepo/practical-sql/Chapter_12/ice_cream_survey.csv'
+with (FORMAT CSV, HEADER)
+
+select * from ice_cream_survey
+
+-- Generating the ice cream survey crosstab
+-- a different/summarized view of the first query...
+-- 数据透视表 in excel, steps:
+-- 1. download query result
+-- 2. open csv with wps/excel
+-- 3. menu -> insert -> pivot table
+-- 4. select office as row
+-- 5. select flavor as column
+-- 6. select count as value
+-- 7. you'll get the same result
+SELECT *
+FROM crosstab('SELECT office,
+                      flavor,
+                      count(*)
+               FROM ice_cream_survey
+               GROUP BY office, flavor
+               ORDER BY office',
+
+              'SELECT distinct flavor
+               FROM ice_cream_survey
+               ORDER BY flavor')
+
+AS (office varchar(20),
+    chocolate bigint,
+    strawberry bigint,
+    vanilla bigint);
+
+CREATE TABLE temperature_readings (
+    reading_id bigserial PRIMARY KEY,
+    station_name varchar(50),
+    observation_date date,
+    max_temp integer,
+    min_temp integer
+);
+
+COPY temperature_readings 
+     (station_name, observation_date, max_temp, min_temp)
+FROM '/Users/lijunwei/OuterGitRepo/practical-sql/Chapter_12/temperature_readings.csv'
+WITH (FORMAT CSV, HEADER);
+
+select * from temperature_readings;
+
+-- data source
+SELECT
+	station_name, date_part('month', observation_date),
+	percentile_cont(.5)
+	WITHIN GROUP (ORDER BY max_temp)
+FROM temperature_readings
+GROUP BY station_name,
+date_part('month', observation_date)
+ORDER BY station_name
+
+-- Crosstabs do take time to set up, but viewing data sets in a matrix often makes comparisons easier than viewing the same data in a vertical list.
+-- Keep in mind that the crosstab() function is CPU-intensive, so tread carefully when querying sets that have millions or billions of rows.
+SELECT *
+FROM crosstab('SELECT
+                  station_name,
+                  date_part(''month'', observation_date),
+                  percentile_cont(.5)
+                      WITHIN GROUP (ORDER BY max_temp)
+               FROM temperature_readings
+               GROUP BY station_name,
+                        date_part(''month'', observation_date)
+               ORDER BY station_name',
+
+              'SELECT month
+               FROM generate_series(1,12) month')
+
+AS (station varchar(50),
+    jan numeric(3,0),
+    feb numeric(3,0),
+    mar numeric(3,0),
+    apr numeric(3,0),
+    may numeric(3,0),
+    jun numeric(3,0),
+    jul numeric(3,0),
+    aug numeric(3,0),
+    sep numeric(3,0),
+    oct numeric(3,0),
+    nov numeric(3,0),
+    dec numeric(3,0)
+);
+
+SELECT max_temp,
+       CASE WHEN max_temp >= 90 THEN 'Hot'
+            WHEN max_temp BETWEEN 70 AND 89 THEN 'Warm'
+            WHEN max_temp BETWEEN 50 AND 69 THEN 'Pleasant'
+            WHEN max_temp BETWEEN 33 AND 49 THEN 'Cold'
+            WHEN max_temp BETWEEN 20 AND 32 THEN 'Freezing'
+            ELSE 'Inhumane'
+        END AS temperature_group
+FROM temperature_readings;
+
+WITH temps_collapsed (station_name, max_temperature_group) AS
+    (SELECT station_name,
+           CASE WHEN max_temp >= 90 THEN 'Hot'
+                WHEN max_temp BETWEEN 70 AND 89 THEN 'Warm'
+                WHEN max_temp BETWEEN 50 AND 69 THEN 'Pleasant'
+                WHEN max_temp BETWEEN 33 AND 49 THEN 'Cold'
+                WHEN max_temp BETWEEN 20 AND 32 THEN 'Freezing'
+                ELSE 'Inhumane'
+            END
+    FROM temperature_readings)
+
+SELECT station_name, max_temperature_group, count(*)
+FROM temps_collapsed
+GROUP BY station_name, max_temperature_group
+ORDER BY station_name, count(*) DESC;
