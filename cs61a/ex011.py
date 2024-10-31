962c11cd75edd077a06a98c5d943750b72f05042
junwei <ljw532344863@sina.com>
2024-10-31 09:47:04 +0800

    Add cs61a learning examples

--- cs61a/ex011.py
+++ cs61a/ex011.py
@@ -20,3 +20,23 @@ def count_partitions(n, m):
     with_m = count_partitions(n-m, m)
     withoug_m = count_partitions(n, m-1)
     return with_m+withoug_m
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def partition(number):
+  answer = set()
+  answer.add((number, ))
+  for x in range(1, number):
+      for y in partition(number - x):
+          answer.add(tuple(sorted((x, ) + y)))
+  return answer
+
+@lru_cache(maxsize=None)
+def partition_with_max(number, max):
+  answer = set()
+  answer.add((number, ))
+  for x in range(max, number):
+      for y in partition_with_max(number - x, number-max):
+          answer.add(tuple(sorted((x, ) + y)))
+  return answer
