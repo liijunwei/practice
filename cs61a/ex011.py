b38bef99af44ae5540e20dedb17ebe058d894f4d
junwei <ljw532344863@sina.com>
2024-11-07 09:31:21 +0800

    Add cs61a learning examples

--- cs61a/ex011.py
+++ cs61a/ex011.py
@@ -40,3 +40,33 @@ def partition_with_max(number, max):
       for y in partition_with_max(number - x, number-max):
           answer.add(tuple(sorted((x, ) + y)))
   return answer
+
+
+def part(n, k):
+  """
+  >>> part(5, 1)
+  [[5]]
+  >>> part(5, 2)
+  [[4, 1], [3, 2]]
+  >>> part(5, 3)
+  [[3, 1, 1], [2, 2, 1]]
+  >>> part(5, 4)
+  [[2, 1, 1, 1]]
+  >>> part(5, 5)
+  [[1, 1, 1, 1, 1]]
+  >>> part(6, 3)
+  [[4, 1, 1], [3, 2, 1], [2, 2, 2]]
+  """
+  def _part(n, k, pre):
+    if n <= 0:
+        return []
+    if k == 1:
+        if n <= pre:
+            return [[n]]
+        return []
+    ret = []
+    for i in range(min(pre, n), 0, -1):
+        ret += [[i] + sub for sub in _part(n-i, k-1, i)]
+    return ret
+  return _part(n, k, n)
+
