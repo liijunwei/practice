ff3cbd021c04da2bf36e7af592298793cc0652dd
junwei <ljw532344863@sina.com>
2024-11-11 22:19:27 +0800

    Add database seeds

---
+++ golang/greenlight/db/seeds/movies.sql
@@ -0,0 +1,6 @@
+insert into movies(title, year, runtime, genres) values
+('fun movie1', '1992', 123, ARRAY['t1','t2']),
+('fun movie2', '2003', 10, ARRAY['t2','t4']),
+('fun movie3', '2018', 209, ARRAY['t1','t2']),
+('boaring movie1', '2019', 49, ARRAY['t7','t9']),
+('boaring movie1', '1992', 20, ARRAY['t11','t29']);
