bd91f24be30270f804ce81e84b44b15f25abc9b6
junwei <ljw532344863@sina.com>
2024-11-15 17:15:53 +0800

    Setup mailer

--- golang/greenlight/go.mod
+++ golang/greenlight/go.mod
@@ -6,4 +6,8 @@ require github.com/lib/pq v1.10.9
 
 require golang.org/x/time v0.8.0
 
-require golang.org/x/crypto v0.29.0 // indirect
+require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
---
+++ golang/greenlight/internal/mailer/mailer.go
@@ -0,0 +1,67 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"text/template"
+	"time"
+
+	"github.com/go-mail/mail/v2"
+)
+
+// indicate to Go that we want to store the contents of the ./templates directory in the templateFS embeded file system variable
+//
+//go:embed "templates"
+var templateFS embed.FS
+
+type Mailer struct {
+	dialer *mail.Dialer
+	sender string
+}
+
+func New(host string, port int, username, password, sender string) Mailer {
+	dialer := mail.NewDialer(host, port, username, password)
+	dialer.Timeout = 5 * time.Second
+
+	return Mailer{
+		dialer: dialer,
+		sender: sender,
+	}
+}
+
+func (m Mailer) Send(recipient, templateFile string, data any) error {
+	tmpl, err := template.New("email").ParseFS(templateFS, "templates/"+templateFile)
+	if err != nil {
+		return err
+	}
+
+	subject := new(bytes.Buffer)
+	if err := tmpl.ExecuteTemplate(subject, "subject", data); err != nil {
+		return err
+	}
+
+	plainBody := new(bytes.Buffer)
+	if err := tmpl.ExecuteTemplate(plainBody, "plainBody", data); err != nil {
+		return err
+	}
+
+	htmlBody := new(bytes.Buffer)
+	if err := tmpl.ExecuteTemplate(htmlBody, "htmlBody", data); err != nil {
+		return err
+	}
+
+	msg := mail.NewMessage()
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("From", m.sender)
+	msg.SetHeader("Subject", subject.String())
+	msg.SetBody("text/plain", plainBody.String())
+	msg.AddAlternative("text/html", htmlBody.String())
+
+	// open a connection to the SMTP server -> send the message -> close connection
+	// maybe timeout
+	if err := m.dialer.DialAndSend(msg); err != nil {
+		return err
+	}
+
+	return nil
+}
