c1cb677beb4f11c6372fb0ccb633dc17d10a8e7c
junwei <ljw532344863@sina.com>
2024-11-28 09:19:39 +0800

    chore: [skip ci] WIP

--- golang/greenlight/internal/eventsourcing/example/main.go
+++ golang/greenlight/internal/eventsourcing/example/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"greenlight/internal/ericlagergren"
 	"net/http"
 	"os"
 
+	"github.com/ericlagergren/decimal"
+	"github.com/gofrs/uuid"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -55,4 +59,95 @@ func run() error {
 	}
 
 	return nil
+}
+
+func createAccountHandler(accountRepo *AccountRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		ctx := r.Context()
+
+		initialBalance := decimal.New(10000, 0) //nolint: mnd // example
+		account, err := NewAccount(initialBalance)
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msg("NewAccount error")
+
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			w.Write([]byte("\n"))
+
+			return
+		}
+
+		// save it to database
+		if err := accountRepo.Save(ctx, account); err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msg("save account error")
+
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			w.Write([]byte("\n"))
+
+			return
+		}
+
+		// write response
+		w.WriteHeader(http.StatusCreated)
+
+		if err := json.NewEncoder(w).Encode(account); err != nil {
+			log.Warn().Err(err).
+				Str("account_id", account.ID.String()).
+				Msg("failed to write response")
+		}
+	}
+}
+
+func getAccountHandler(accountRepo *AccountRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		ctx := r.Context()
+
+		idString := r.URL.Query().Get("id")
+		if idString == "" {
+			zerolog.Ctx(ctx).Error().Msg("no id provided")
+			w.WriteHeader(http.StatusBadRequest)
+
+			return
+		}
+
+		accountID, err := uuid.FromString(idString)
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msg("not valid id")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			w.Write([]byte("\n"))
+
+			return
+		}
+
+		account, err := accountRepo.Load(ctx, accountID)
+		zerolog.Ctx(ctx).Error().Err(err).Msg("account not found")
+
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(err.Error()))
+			w.Write([]byte("\n"))
+
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+
+		data, err := json.Marshal(account)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			w.Write([]byte("\n"))
+
+			return
+		}
+
+		w.Write(data)
+		w.Write([]byte("\n"))
+	}
 }
