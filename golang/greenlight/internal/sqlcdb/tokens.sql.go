f08035ffd81a9a48d60b6128c247c625d85e60ef
junwei <ljw532344863@sina.com>
2024-11-28 22:42:01 +0800

    chore: [skip ci] WIP

--- golang/greenlight/internal/sqlcdb/tokens.sql.go
+++ golang/greenlight/internal/sqlcdb/tokens.sql.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-const createScopedUserToken = `-- name: CreateScopedUserToken :exec
+const CreateScopedUserToken = `-- name: CreateScopedUserToken :exec
 insert into tokens(hash, user_id, expire_at, scope)
 values($1,$2,$3,$4)
 `
@@ -22,8 +22,8 @@ type CreateScopedUserTokenParams struct {
 	Scope    string
 }
 
-func (q *Queries) CreateScopedUserToken(ctx context.Context, arg CreateScopedUserTokenParams) error {
-	_, err := q.db.ExecContext(ctx, createScopedUserToken,
+func (q *Queries) CreateScopedUserToken(ctx context.Context, arg *CreateScopedUserTokenParams) error {
+	_, err := q.db.ExecContext(ctx, CreateScopedUserToken,
 		arg.Hash,
 		arg.UserID,
 		arg.ExpireAt,
@@ -32,7 +32,7 @@ func (q *Queries) CreateScopedUserToken(ctx context.Context, arg CreateScopedUse
 	return err
 }
 
-const deleteScopedUserTokens = `-- name: DeleteScopedUserTokens :exec
+const DeleteScopedUserTokens = `-- name: DeleteScopedUserTokens :exec
 delete from tokens where scope = $1 and user_id = $2
 `
 
@@ -41,7 +41,7 @@ type DeleteScopedUserTokensParams struct {
 	UserID int64
 }
 
-func (q *Queries) DeleteScopedUserTokens(ctx context.Context, arg DeleteScopedUserTokensParams) error {
-	_, err := q.db.ExecContext(ctx, deleteScopedUserTokens, arg.Scope, arg.UserID)
+func (q *Queries) DeleteScopedUserTokens(ctx context.Context, arg *DeleteScopedUserTokensParams) error {
+	_, err := q.db.ExecContext(ctx, DeleteScopedUserTokens, arg.Scope, arg.UserID)
 	return err
 }
