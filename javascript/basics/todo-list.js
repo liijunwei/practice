21026b2922bd8db38e2dc7328d0726285eeca5c1
junwei <ljw532344863@sina.com>
2024-12-01 11:36:06 +0800

    watch commit

--- javascript/basics/todo-list.js
+++ javascript/basics/todo-list.js
@@ -3,10 +3,10 @@ const todoList = [];
 function addTodo() {
   const inputElememt = document.querySelector('.js-name-input');
   const name = inputElememt.value;
-  todoList.push(name);
-
+  if (name !== '' || name.trim() !== '') {
+    todoList.push(name);
+  }
   console.log(todoList);
-
   inputElememt.value = ''
 }
 
