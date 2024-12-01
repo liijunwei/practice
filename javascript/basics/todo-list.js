c8ae04f04bebbd557ba2774ea90c1f0fa1b49f5c
junwei <ljw532344863@sina.com>
2024-12-01 12:01:30 +0800

    watch commit

--- javascript/basics/todo-list.js
+++ javascript/basics/todo-list.js
@@ -1,17 +1,26 @@
-const todoList = ['task1', 'task2'];
+const todoList = [
+  {name: 'task1', dueDate: '2024-11-30'},
+  {name: 'task2', dueDate: '2024-12-1'},
+];
+
 renderTodoList()
 
 // 1. save the data
 // 2. generate the HTML
 // 3. make it interactive
 function addTodo() {
-  const inputElememt = document.querySelector('.js-name-input');
-  const name = inputElememt.value;
+  const nameElememt = document.querySelector('.js-name-input');
+  const name = nameElememt.value;
+
+  const dateElememt = document.querySelector('.js-due-date-input');
+  const date = dateElememt.value;
+
   if (name !== '' && name.trim() !== '') {
-    todoList.push(name);
+    todoList.push({name: name, dueDate: date});
   }
+
   console.log(todoList);
-  inputElememt.value = ''
+  nameElememt.value = ''
   renderTodoList()
 }
 
@@ -26,7 +35,7 @@ function renderTodoList() {
   todoList.forEach((item, index) => {
     const html = `
     <p class="css-item">
-      ${item}
+      ${item.name} ${item.dueDate}
       <button class="js-delete-button" onclick="handleDelete(${index})">Delete</button>
     </p>`
     todoHTML += html
@@ -38,4 +47,8 @@ function renderTodoList() {
 function handleDelete(index) {
   todoList.splice(index, 1);
   renderTodoList();
+}
+
+function getDefaultDueDate() {
+
 }
