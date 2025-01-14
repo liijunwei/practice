e6c7a8bc7a01af7816d0a4f4defff1879cfadb56
junwei <ljw532344863@sina.com>
2025-01-14 23:38:50 +0800

    chore: Commit manually

---
+++ golang/play/p8/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// this is so inconvenient in go
+func main() {
+	var count int
+	_, err := fmt.Scan(&count)
+	if err != nil {
+		panic(err)
+	}
+
+	arr := make([]string, 0, count)
+
+	for i := 0; i < count; i++ {
+		var s string
+		_, err := fmt.Scan(&s)
+		boom(err)
+
+		arr = append(arr, s)
+	}
+
+	sort.Strings(arr)
+	fmt.Println(strings.Join(arr, " "))
+}
+
+func boom(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
