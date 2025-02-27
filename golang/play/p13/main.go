f2fde12f00b52a4b90498cce99c7bb3f19d6f33c
junwei <ljw532344863@sina.com>
2025-02-27 09:12:38 +0800

    Add note

--- golang/play/p13/main.go
+++ golang/play/p13/main.go
@@ -6,10 +6,29 @@ import (
 	"math/big"
 )
 
+func toBase62(s string) string {
+	var i big.Int
+	i.SetBytes([]byte(s))
+	return i.Text(62)
+}
+
+func toBase62_1(s []byte) string {
+	var i big.Int
+	i.SetBytes(s)
+	return i.Text(62)
+}
+
 func main() {
 	h := sha256.New()
 	h.Write([]byte("foo"))
 	r := h.Sum(nil)
 	num := new(big.Int).SetBytes(r)
 	fmt.Println(num.String())
+
+	fmt.Println([]byte("61"))
+	fmt.Println([]byte{61})
+	fmt.Println(toBase62("61"))
+	fmt.Println(toBase62_1([]byte{0}))
+	fmt.Println(toBase62_1([]byte{1}))
+	fmt.Println(toBase62_1([]byte{61}))
 }
