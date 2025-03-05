51a4299ce564319497da86933bfed1eecf81d0ec
junwei <ljw532344863@sina.com>
2025-03-05 09:50:00 +0800

    Add TestConcurrentAccess

--- golang/ttlcache1/cache_test.go
+++ golang/ttlcache1/cache_test.go
@@ -1,7 +1,11 @@
 package ttlcache1_test
 
 import (
+	"fmt"
 	"golang-practices/ttlcache1"
+	"math/rand/v2"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -59,4 +63,46 @@ func TestTTLRenewal(t *testing.T) {
 	<-time.After(10 * time.Millisecond)
 	_, ok = c.Get("a")
 	require.True(t, ok)
+}
+
+func TestConcurrentAccess(t *testing.T) {
+	c := ttlcache1.New(ttlcache1.WithTTL(3*time.Millisecond), ttlcache1.WithInterval(1*time.Millisecond))
+
+	var wg sync.WaitGroup
+	numWorkers := 100000
+	keys := []string{"k1", "k2", "k3"}
+
+	wg.Add(numWorkers)
+
+	var hitcount atomic.Int64
+	var misscount atomic.Int64
+
+	for id := range numWorkers {
+		go func() {
+			defer wg.Done()
+
+			key := keys[id%len(keys)]
+
+			if rand.IntN(100) > 95 {
+				c.Set(key, fmt.Sprintf("value-%d", id))
+			}
+
+			if _, ok := c.Get(key); ok {
+				hitcount.Add(1)
+				return
+			}
+
+			misscount.Add(1)
+		}()
+	}
+
+	wg.Wait()
+
+	// TODO add metrics to the cache struct
+	t.Log("hitcount", hitcount.Load(), "misscount", misscount.Load(), "cache hit rate", float64(hitcount.Load())/float64((hitcount.Load()+misscount.Load())))
+
+	for _, k := range keys {
+		_, ok := c.Get(k)
+		require.True(t, ok)
+	}
 }
