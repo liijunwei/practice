b9289f2bb93e04a40cd849a9aa7d3b6fb55f0b57
junwei <ljw532344863@sina.com>
2025-03-09 19:56:08 +0800

    Make boundary obvious

--- golang/mess/cmd/presum/main.go
+++ golang/mess/cmd/presum/main.go
@@ -11,6 +11,34 @@ func main() {
 	print([]int{1, 1, 1, 1, 1})
 	print([]int{1, 2, 3, 4, 5})
 	print(parse(os.Args[1]))
+
+	left := mustParseInt(os.Args[2])
+	right := mustParseInt(os.Args[3])
+	fmt.Println(rangesum(parse(os.Args[1]), left, right))
+}
+
+// range sum: sum(L, R) = prefix[R] - prefix[L-1]
+// e.g. rangesum([]int{1, 1, 1, 1, 1}, 2, 3) => 1+1 = presum[3]-presum[1])
+// e.g. rangesum([]int{1, 2, 3, 4, 5}, 2, 3) => 3+4 = presum[3]-presum[1])
+func rangesum(input []int, left, right int) int {
+	assert(left >= 0)
+	assert(right >= 0)
+	assert(right >= left)
+	assert(right < len(input))
+
+	presum := presum(input)
+
+	if left == 0 {
+		return presum[right]
+	}
+
+	return presum[right] - presum[left-1]
+}
+
+func assert(ok bool) {
+	if !ok {
+		panic("assertion failed")
+	}
 }
 
 func print(input []int) {
@@ -19,6 +47,7 @@ func print(input []int) {
 	fmt.Println(presum(input))
 }
 
+// time complexity: O(n)
 func presum(input []int) []int {
 	prefix := make([]int, len(input)+1)
 
@@ -34,9 +63,17 @@ func parse(input string) []int {
 	nums := make([]int, 0, len(strs))
 
 	for _, str := range strs {
-		num, _ := strconv.Atoi(str)
-		nums = append(nums, num)
+		nums = append(nums, mustParseInt(str))
 	}
 
 	return nums
+}
+
+func mustParseInt(input string) int {
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
+
+	return num
 }
