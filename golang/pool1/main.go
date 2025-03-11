457cb3ce872cba214622ced50901bf6c01d2d0cd
junwei <ljw532344863@sina.com>
2025-03-11 11:51:12 +0800

    chore: Commit manually

--- golang/pool1/main.go
+++ golang/pool1/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -32,4 +35,91 @@ func main() {
 	pool.Run()
 
 	log.Println("all done, elapsed: ", time.Since(startTime))
+}
+
+type Task interface {
+	Run()
+}
+
+type SendEmail struct {
+	Email       string
+	Subject     string
+	MessageBody string
+}
+
+func NewSendEmailTask(id int) *SendEmail {
+	return &SendEmail{
+		Email:       fmt.Sprintf("me-%d@foo.com", id),
+		Subject:     fmt.Sprintf("fiz-%d", id),
+		MessageBody: fmt.Sprintf("buz-%d", id),
+	}
+}
+
+func (t *SendEmail) Run() {
+	content, err := json.Marshal(t)
+	boom(err)
+
+	log.Printf("processing... %s\n", content)
+	time.Sleep(2 * time.Second)
+}
+
+func NewProcessImageTask(id int) *ProcessImage {
+	return &ProcessImage{
+		ImageUrl: fmt.Sprintf("image@%d", id),
+	}
+}
+
+type ProcessImage struct {
+	ImageUrl string
+}
+
+func (t *ProcessImage) Run() {
+	content, err := json.Marshal(t)
+	boom(err)
+
+	log.Printf("processing... %s\n", content)
+	time.Sleep(5 * time.Second)
+}
+
+type Pool struct {
+	Tasks       []Task
+	concurrency int
+	taskChannel chan Task
+	wg          sync.WaitGroup
+}
+
+func NewPool(tasks []Task, concurrency int) Pool {
+	return Pool{
+		Tasks:       tasks,
+		concurrency: concurrency,
+		taskChannel: make(chan Task),
+	}
+}
+
+func (p *Pool) startWorker() {
+	for task := range p.taskChannel {
+		p.wg.Add(1)
+		task.Run()
+		p.wg.Done()
+	}
+}
+
+func (p *Pool) Run() {
+	for i := 0; i < p.concurrency; i++ {
+		go p.startWorker()
+	}
+
+	for _, task := range p.Tasks {
+		p.taskChannel <- task
+	}
+
+	close(p.taskChannel)
+
+	p.wg.Wait()
+}
+
+func boom(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
