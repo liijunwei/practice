5a6ff89a565ad04f9c230b15fc0a7f8df700c28f
junwei <ljw532344863@sina.com>
2025-04-08 09:23:54 +0800

    Practice C function

--- clang/struct/demo-01.c
+++ clang/struct/demo-01.c
@@ -8,11 +8,31 @@ typedef struct {
   int book_id;
 } Book;
 
+void printf_book1(Book book);
+void printf_book2(Book *book);
+
 int main() {
   Book book = {"C 语言", "RUNOOB", "编程语言", 123456};
 
+  printf_book1(book);
+  printf_book2(&book);
+}
+
+void printf_book1(Book book) {
+  printf("printf_book1 sizeof(book): %zu\n", sizeof(book));
+  printf("sizeof(book): %zu\n", sizeof(book));
   printf("title:   %s\n", book.title);
   printf("author:  %s\n", book.author);
   printf("subject: %s\n", book.subject);
   printf("book_id: %d\n", book.book_id);
+  printf("\n");
+}
+
+void printf_book2(Book *book) {
+  printf("printf_book2 sizeof(book): %zu\n", sizeof(book));
+  printf("title:   %s\n", book->title);
+  printf("author:  %s\n", book->author);
+  printf("subject: %s\n", book->subject);
+  printf("book_id: %d\n", book->book_id);
+  printf("\n");
 }
