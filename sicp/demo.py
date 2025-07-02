5b24bd2bd0ff619ffbb21d4c15609620632c9870
junwei <ljw532344863@sina.com>
2025-07-03 00:15:49 +0800

    Practice sicp

--- sicp/demo.py
+++ sicp/demo.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 import sys
-from math import pi,sqrt
+from math import pi,sqrt,pow
 
 def f(x, y):
   return g(x)
@@ -51,3 +51,25 @@ def are_hexagon(r):
 print("area_square", area_square(1))
 print("are_circle", are_circle(1))
 print("are_hexagon", are_hexagon(1))
+
+# python3 -m doctest demo.py
+# generalizing over computational processes
+def sum_naturals(n):
+  """sum of the first N natural numbers
+  >>> sum_naturals(5)
+  15
+  """
+  total, k = 0, 1
+  while k<=n:
+    total, k = total+k, k+1
+  return total
+
+def sum_cubes(n):
+  """
+  >>> sum_cubes(5)
+  225.0
+  """
+  total, k = 0, 1
+  while k<=n:
+    total, k = total+pow(k,3), k+1
+  return total
