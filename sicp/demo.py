2238eb2420c32fa2db9a4213b35e7a31b975e820
junwei <ljw532344863@sina.com>
2025-07-03 00:08:30 +0800

    Practice sicp

--- sicp/demo.py
+++ sicp/demo.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 import sys
+from math import pi,sqrt
 
 def f(x, y):
   return g(x)
@@ -27,3 +28,25 @@ print(a)
 print(a(10))
 print(b)
 print(b(10))
+
+# a guide to designing function
+# 1. give each function exactly one job, but make it apply to many related situations
+# DRY
+
+# shift+enter
+
+def area_square(r):
+  assert r>0
+  return r*r
+
+def are_circle(r):
+  assert r>0
+  return r*r*pi
+
+def are_hexagon(r):
+  assert r>0
+  return r*r*3*sqrt(3)/2
+
+print("area_square", area_square(1))
+print("are_circle", are_circle(1))
+print("are_hexagon", are_hexagon(1))
