c2a110a95cb538b1f06c44dd8a02ac13e839d83a
junwei <ljw532344863@sina.com>
2025-07-03 08:43:39 +0800

    Practice sicp

--- sicp/demo.py
+++ sicp/demo.py
@@ -49,8 +49,8 @@ def are_hexagon(r):
   return area(r, 3*sqrt(3)/2)
 
 print("area_square", area_square(1))
-print("are_circle", are_circle(1))
-print("are_hexagon", are_hexagon(1))
+print("area_circle", are_circle(1))
+print("area_hexagon", are_hexagon(1))
 
 # python3 -m doctest -v demo.py
 # generalizing over computational processes
@@ -74,3 +74,35 @@ def sum_cubes(n):
   225.0
   """
   return summation(n, lambda x: pow(x, 3))
+
+def make_adder(n):
+  def adder(k):
+    return k+n
+  return adder
+
+add_two = make_adder(2)
+add_two(3)
+
+"""
+functions defined within other function bodies are bound to names in a local frame
+"""
+
+def compose1(f, g):
+  def h(x):
+    return f(g(x))
+  return h
+
+# make_adder(2)(3) => 5
+# square(5) => 25
+print(compose1(square1, make_adder(2))(3))
+
+# returning a function using its own name
+def print_sums(n):
+  print(n)
+  def next_sum(k):
+    return print_sums(n+k)
+  return next_sum
+
+print(print_sums(1)(3)(5))
+
+# TODO p06
