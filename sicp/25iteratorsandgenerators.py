0b182c74b452a28f7daba8e2d1e620b3cf7e7633
junwei <ljw532344863@sina.com>
2025-07-31 23:53:48 +0800

    Practice sicp: iterators and generators

--- sicp/25iteratorsandgenerators.py
+++ sicp/25iteratorsandgenerators.py
@@ -1,1 +1,18 @@
 # https://www.bilibili.com/video/BV1nJ41157p6?t=2.0&p=25
+
+def dict_iter():
+  d = {1: "one", 2: "two"}
+  for i in d:
+    print(i, d[i])
+
+# 1. build the whole list
+# 2. turn the list into an iterator
+def fib_iter(n):
+  prev, curr = 0, 1
+  lst = [prev, curr]
+  index = 2
+  while index < n:
+    prev, curr = curr, prev+curr
+    lst += [curr]
+    index += 1
+  return iter(lst)
