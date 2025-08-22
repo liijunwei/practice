47f1bd1ad89181ba34ff1dee21f1cc421d7af642
junwei <junwei@mtmbp>
2025-08-22 10:24:51 +0800

    Practice heap with container/heap

---
+++ golang/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+var _ heap.Interface = &IntHeap{}
+
+// An IntHeap is a min-heap of ints.
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] >= h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func main() {
+	h := &IntHeap{2, 1, 5, 38, 99}
+	heap.Init(h)
+	heap.Push(h, 3)
+	fmt.Printf("minimum: %d\n", (*h)[0])
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+}
