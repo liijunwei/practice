51618bb0c65b7c1d8928fce2f75f1116e3ca9a6b
junwei <junwei@mtmbp>
2025-08-26 19:36:51 +0800

    Add fuzzy test

--- golang/heap2/main_test.go
+++ golang/heap2/main_test.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"math"
-	"math/rand"
 	"testing"
-	"time"
 )
 
-func TestHeapWithRandomData(t *testing.T) {
-	h := NewMaxHeap()
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	data := make([]int, 1000000)
-	for i := 0; i < 1000000; i++ {
-		data[i] = r.Intn(10000)
-		h.Insert(data[i])
-	}
+// go test -fuzz=FuzzHeap .
+// go test -fuzz=FuzzHeap -fuzztime=30s .
+// go test -fuzz=FuzzHeap -fuzztime=10000000x .
+func FuzzHeap(f *testing.F) {
+	// 添加一些种子测试用例
+	f.Add([]byte{1, 2, 3, 4, 5})
+	f.Add([]byte{100, 50, 75, 25, 90})
+	f.Add([]byte{})
 
-	// 验证删除时是否始终弹出最大值
-	prev := math.MaxInt32
-	for !h.Empty() {
-		current := h.DelMax()
-		if current > prev {
-			t.Errorf("堆序错误：%d > %d", current, prev)
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) == 0 {
+			return
 		}
-		prev = current
-	}
+
+		h := NewMaxHeap()
+
+		// 将字节数据转换为整数并插入堆
+		for _, b := range data {
+			h.Insert(int(b))
+		}
+
+		// 验证删除时是否始终弹出最大值
+		prev := math.MaxInt32
+		for !h.Empty() {
+			current := h.DelMax()
+			if current > prev {
+				t.Errorf("堆序错误：%d > %d", current, prev)
+			}
+			prev = current
+		}
+	})
 }
